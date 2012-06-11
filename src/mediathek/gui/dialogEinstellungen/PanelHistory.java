cb4b4ae3e2064bee25f8ab33eb47df923357e36e
W.Xaver <w.xaver@googlemail.com>
2012-06-11 20:22:23 +0200

    Former-commit-id: d8a74fe58573a5a41d7dad0395b457375a26dfee

--- src/mediathek/gui/dialogEinstellungen/PanelHistory.java
+++ src/mediathek/gui/dialogEinstellungen/PanelHistory.java
@@ -20,7 +20,6 @@
 package mediathek.gui.dialogEinstellungen;
 
 import java.awt.Point;
-import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
