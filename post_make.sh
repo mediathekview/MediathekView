dbbc8653cdbbfd1bc46ef39a08ccecbd86ee6dec
W.Xaver <w.xaver@googlemail.com>
2012-08-12 17:04:00 +0200

    HR 	new file:   dist/Anleitung.pdf 	new file:   dist/Copyright/commons-compress-LICENSE.txt 	new file:   dist/Copyright/commons-lang-LICENSE.txt 	new file:   dist/Copyright/flvstreamer-LICENSE.txt 	new file:   dist/Copyright/launch4j-LICENSE.txt 	new file:   dist/Copyright/medithekView-LICENSE.txt 	new file:   dist/MediathekView.exe 	new file:   dist/MediathekView.ico 	new file:   dist/MediathekView.jar 	new file:   dist/MediathekView.sh 	new file:   dist/info/make.txt 	new file:   dist/info/releases.txt 	new file:   dist/lib/commons-compress-1.4.jar 	new file:   dist/lib/commons-lang3-3.1.jar 	modified:   post_make.sh 	modified:   src/mediathek/controller/filme/filmeSuchen/sender/MediathekHr.java 	deleted:    src/mediathek/controller/filme/filmeSuchen/sender/MediathekOrf_1.java 	modified:   src/mediathek/controller/filme/filmeSuchenSender/FilmeSuchenSender.java 	modified:   src/version.properties
    Former-commit-id: eeb9ca5f91059c231e4107005f35b92cfc118d98

--- post_make.sh
+++ post_make.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Dateien ins dist-Verzeichnis kopieren
 cp -r /mnt/daten/software/java/Mediathek_3/bin/* /mnt/daten/software/java/Mediathek_3/dist
 cp /mnt/daten/software/java/Mediathek_3/bin/Anleitung/Anleitung.pdf /mnt/daten/software/java/Mediathek_3/dist
 
@@ -10,13 +11,14 @@ cp /mnt/daten/software/java/Mediathek_3/bin/Anleitung/Anleitung.pdf /mnt/daten/s
 # Aufräumen
 rm /mnt/daten/software/java/Mediathek_3/dist/README.TXT
 rm -r /mnt/daten/software/java/Mediathek_3/dist/Anleitung
+#rm -r /mnt/daten/software/java/Mediathek_3/dist/Anleitung/jpg
+#rm -r /mnt/daten/software/java/Mediathek_3/dist/Anleitung/Anleitung.odg
 
+# zur Vorsicht: Programmsets ins www-verzeichnis kopieren
 cp /mnt/daten/software/java/Mediathek_3/src/mediathek/file/*.xml /mnt/daten/www/online/ZDFMediathekView/programmgruppen/
 
-cd /mnt/daten/software/java/Mediathek_3/dist/
-
 # zip erstellen
-
+cd /mnt/daten/software/java/Mediathek_3/dist/
 datum=$(date +%Y.%m.%d )
 zip -r MediathekView_3.0.0_$datum.zip .
  
