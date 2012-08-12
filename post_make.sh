7e9dc4e89fad1bda9ae9373fb45a606ad9d11e37
W.Xaver <w.xaver@googlemail.com>
2012-08-12 20:50:58 +0200

    dist 	new file:   dist/Anleitung/Anleitung.odg 	new file:   dist/Anleitung/Anleitung.pdf 	new file:   dist/Anleitung/jpg/Abos.jpg 	new file:   dist/Anleitung/jpg/Downloads.jpg 	new file:   dist/Anleitung/jpg/Downloads_Maus.jpg 	new file:   dist/Anleitung/jpg/Filme.jpg 	new file:   dist/Anleitung/jpg/Filme_Maus.jpg 	modified:   dist/MediathekView.jar 	new file:   dist/bin/flv.bat 	new file:   dist/bin/flv.sh 	new file:   dist/bin/flvstreamer_win32_latest.exe 	modified:   post_make.sh 	modified:   src/version.properties

--- post_make.sh
+++ post_make.sh
@@ -10,7 +10,7 @@ cp /mnt/daten/software/java/Mediathek_3/bin/Anleitung/Anleitung.pdf /mnt/daten/s
 
 # Aufräumen
 rm /mnt/daten/software/java/Mediathek_3/dist/README.TXT
-rm -r /mnt/daten/software/java/Mediathek_3/dist/Anleitung
+#rm -r /mnt/daten/software/java/Mediathek_3/dist/Anleitung
 
 # Programmsets ins www-verzeichnis kopieren
 cp /mnt/daten/software/java/Mediathek_3/src/mediathek/file/*.xml /mnt/daten/www/online/ZDFMediathekView/programmgruppen/
