b547ada4a9f225ab448c60c931c1321521b56625
W.Xaver <w.xaver@googlemail.com>
2012-08-24 19:16:13 +0200

    Aufräumen

--- src/mediathek/controller/filmeLaden/importieren/FilmUpdateServerSuchen.java
+++ src/mediathek/controller/filmeLaden/importieren/FilmUpdateServerSuchen.java
@@ -17,7 +17,7 @@
  *    You should have received a copy of the GNU General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package mediathek.controller.filmeLaden.filmUpdateServer;
+package mediathek.controller.filmeLaden.importieren;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
