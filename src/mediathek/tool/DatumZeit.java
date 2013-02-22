b7c588aaaa3c77655c5fe4c430bedf2a0596bc02
W.Xaver <w.xaver@googlemail.com>
2013-02-22 20:52:23 +0100

    Zieldateiname direkte Downloads

--- src/mediathek/tool/DatumZeit.java
+++ src/mediathek/tool/DatumZeit.java
@@ -23,8 +23,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import mediathek.daten.DatenDownload;
 import mediathek.daten.DatenFilm;
 
