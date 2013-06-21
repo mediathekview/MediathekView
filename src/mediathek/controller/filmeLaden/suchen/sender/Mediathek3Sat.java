2729699cfeadfb3064e36015595668efb16a5a9f
Xaver <w.xaver@googlemail.com>
2013-06-21 21:20:51 +0200

    3sat 	geändert:   dist/MediathekView.jar 	geändert:   src/mediathek/controller/filmeLaden/suchen/sender/Mediathek3Sat.java 	geändert:   src/version.properties

--- src/mediathek/controller/filmeLaden/suchen/sender/Mediathek3Sat.java
+++ src/mediathek/controller/filmeLaden/suchen/sender/Mediathek3Sat.java
@@ -106,6 +106,8 @@ public class Mediathek3Sat extends MediathekReader implements Runnable {
         void laden(String url_rss, String thema_rss) {
             // <title>3sat.schweizweit: Mediathek-Beiträge</title>
             final String MUSTER_URL = "type=\"video/x-ms-asf\" url=\"";
+            final String MUSTER_URL_QUICKTIME_1 = "type=\"video/quicktime\" url=\"http://hstreaming.zdf.de/3sat/veryhigh";
+            final String MUSTER_URL_QUICKTIME_2 = "type=\"video/quicktime\" url=\"http://hstreaming.zdf.de/3sat/300";
             final String MUSTER_TITEL = "<title>";
             final String MUSTER_DESCRIPTION = "<media:description>";
             final String MUSTER_IMAGE = "<media:thumbnail url=\"";
@@ -117,6 +119,7 @@ public class Mediathek3Sat extends MediathekReader implements Runnable {
             int pos = 0;
             int pos1;
             int pos2;
+            int ende = 0;
             String url;
             String thema = "3sat";
             String link;
@@ -145,6 +148,7 @@ public class Mediathek3Sat extends MediathekReader implements Runnable {
             }
             while (!Daten.filmeLaden.getStop() && (pos = seite1.indexOf(MUSTER_TITEL, pos)) != -1) {
                 pos += MUSTER_TITEL.length();
+                ende = seite1.indexOf(MUSTER_TITEL, pos); // beginnt der nächste Film
                 url = "";
                 link = "";
                 datum = "";
@@ -219,25 +223,55 @@ public class Mediathek3Sat extends MediathekReader implements Runnable {
                             link = seite1.substring(pos1, pos2);
                         }
                     }
-                    if ((pos1 = seite1.indexOf(MUSTER_URL, pos)) != -1) {
+                    pos1 = seite1.indexOf(MUSTER_URL, pos);
+                    if (pos1 == -1 || (ende != -1 && pos1 > ende)) {
+                        // dann nach Quicktime-Link suchen
+                        if ((pos1 = seite1.indexOf(MUSTER_URL_QUICKTIME_1, pos)) != -1) {
+                            pos1 += MUSTER_URL_QUICKTIME_1.length();
+                            if ((pos2 = seite1.indexOf("\"", pos1)) != -1) {
+                                url = seite1.substring(pos1, pos2);
+                            }
+//                            } else {
+//                                // 2. Möglichkeit von Quicktime
+//                                if ((pos1 = seite1.indexOf(MUSTER_URL_QUICKTIME_2, pos)) != -1) {
+//                                    pos1 += MUSTER_URL_QUICKTIME_1.length();
+//                                    if ((pos2 = seite1.indexOf("\"", pos1)) != -1) {
+//                                        url = seite1.substring(pos1, pos2);
+//                                    }
+//                                }
+                        }
+                        if (url.equals("")) {
+                            Log.fehlerMeldung(-398745632, Log.FEHLER_ART_MREADER, "Mediathek3Sat.addToList-quicktime", "keine URL" + url_rss);
+                        } else {
+                            url = "http://hstreaming.zdf.de/3sat/veryhigh" + url;
+                            if (!url.endsWith("mov")) {
+                                Log.fehlerMeldung(-798965430, Log.FEHLER_ART_MREADER, "Mediathek3sat.filmHolen", "keine URL: " + url_rss);
+                            } else {
+                                quicktimeHolen(thema, titel, link, url, datum, zeit, duration, description, imageUrl);
+                            }
+                        }
+                    } else if (pos1 != -1 && ((ende != -1 && pos1 < ende) || ende == -1)) {
+                        // asx
                         pos1 += MUSTER_URL.length();
                         if ((pos2 = seite1.indexOf("\"", pos1)) != -1) {
                             url = seite1.substring(pos1, pos2);
                         }
-                    }
-                    if (url.equals("")) {
-                        Log.fehlerMeldung(-532169764, Log.FEHLER_ART_MREADER, "Mediathek3Sat.addToList", "keine URL");
-                    } else {
-                        url = url.replace("/300/", "/veryhigh/");
-                        //    public DatenFilm(Daten ddaten, String ssender, String tthema, String urlThema, String ttitel, String uurl, String datum) {
-                        //    public DatenFilm(String ssender, String tthema, String urlThema, String ttitel, String uurl, String datum, String zeit) {
-                        //addFilm(new DatenFilm(nameSenderMReader, (thema_rss.equals("") ? thema : thema_rss), link, titel, url, datum, zeit));
-                        if (!url.endsWith("asx")) {
-                            Log.fehlerMeldung(-896325047, Log.FEHLER_ART_MREADER, "Mediathek3sat.filmHolen-2", "keine URL: " + url);
+                        if (url.equals("")) {
+                            Log.fehlerMeldung(-532169764, Log.FEHLER_ART_MREADER, "Mediathek3Sat.addToList-asx", "keine URL");
                         } else {
-                            //flashHolen(thema, titel, link, url, datum, zeit);
-                            flashHolen(thema, titel, link, url, datum, zeit, duration, description, imageUrl);
+                            url = url.replace("/300/", "/veryhigh/");
+                            //    public DatenFilm(Daten ddaten, String ssender, String tthema, String urlThema, String ttitel, String uurl, String datum) {
+                            //    public DatenFilm(String ssender, String tthema, String urlThema, String ttitel, String uurl, String datum, String zeit) {
+                            //addFilm(new DatenFilm(nameSenderMReader, (thema_rss.equals("") ? thema : thema_rss), link, titel, url, datum, zeit));
+                            if (!url.endsWith("asx")) {
+                                Log.fehlerMeldung(-896325047, Log.FEHLER_ART_MREADER, "Mediathek3sat.filmHolen-2", "keine URL: " + url_rss);
+                            } else {
+                                //flashHolen(thema, titel, link, url, datum, zeit);
+                                flashHolen(thema, titel, link, url, datum, zeit, duration, description, imageUrl);
+                            }
                         }
+                    } else {
+                        Log.fehlerMeldung(-978741398, Log.FEHLER_ART_MREADER, "Mediathek3sat.filmHolen", "kann nicht sein, URL: " + url_rss);
                     }
                 } catch (Exception ex) {
                     Log.fehlerMeldung(-823694892, Log.FEHLER_ART_MREADER, "Mediathek3Sat.laden", ex);
@@ -253,6 +287,57 @@ public class Mediathek3Sat extends MediathekReader implements Runnable {
             if (f != null) {
                 addFilm(f);
             }
+        }
+
+        private void quicktimeHolen(String thema, String titel, String urlThema, String urlFilm, String datum, String zeit, long durationInSeconds, String description, String imageUrl) {
+            meldung(urlFilm);
+            //DatenFilm f = MediathekZdf.flash(getUrl, seite2, nameSenderMReader, thema, titel, urlThema, urlFilm, datum, zeit);
+            DatenFilm f = quicktime(getUrl, seite2, nameSenderMReader, thema, titel, urlThema, urlFilm, datum, zeit, durationInSeconds, description, "", imageUrl, new String[]{});
+            if (f != null) {
+                addFilm(f);
+            }
+        }
+
+        private DatenFilm quicktime(GetUrl getUrl, StringBuffer seiteQuicktime, String senderName, String thema, String titel, String filmWebsite, String urlFilm, String datum, String zeit, long durationInSeconds, String description, String thumbnailUrl, String imageUrl, String[] keywords) {
+            // RTSPtext
+            // rtsp://a1966.v1252936.c125293.g.vq.akamaistream.net/7/1966/125293/v0001/mp4.od.origin.zdf.de.gl-systemhaus.de/none/3sat/13/06/130614_meyer_kuz_1596k_p13v9.mp4
+            // 
+            // daraus wird:
+            // http://rodl.zdf.de / none/3sat/13/06/130621_news_kuz_436k_p9v11.mp4
+            DatenFilm ret = null;
+            final String MUSTER_URL = "rtsp://";
+            final String MUSTER_TAUSCH = "gl-systemhaus.de";
+            final String MUSTER_ENDE = ".mp4";
+            String orgUrl = urlFilm;
+            String url, tmpUrl = "";
+            int pos1;
+            int pos2;
+            try {
+                seiteQuicktime = getUrl.getUri_Utf(senderName, orgUrl, seiteQuicktime, "filmWebsite: " + filmWebsite);
+                String strSeiteQuicktime = seiteQuicktime.toString();
+                if ((pos1 = strSeiteQuicktime.indexOf(MUSTER_URL, 0)) != -1) {
+                    pos1 += MUSTER_URL.length();
+                    if ((pos2 = strSeiteQuicktime.indexOf(MUSTER_ENDE, pos1)) != -1) {
+                        tmpUrl = strSeiteQuicktime.substring(pos1, pos2);
+                    }
+                }
+                if (tmpUrl.equals("")) {
+                    Log.fehlerMeldung(-679893014, Log.FEHLER_ART_MREADER, "Mediathek3sat.quicktime", "!gefunden: " + urlFilm);
+                } else {
+                    tmpUrl = MUSTER_URL + tmpUrl + MUSTER_ENDE;
+                    if ((pos1 = tmpUrl.indexOf(MUSTER_TAUSCH)) != -1) {
+                        pos1 += MUSTER_TAUSCH.length();
+                        url = "http://rodl.zdf.de" + tmpUrl.substring(pos1);
+                        //ret = new DatenFilm(senderName, thema, urlThema, titel, url, ""/* urlRtmp */, datum, zeit);
+                        ret = new DatenFilm(senderName, thema, filmWebsite, titel, url, ""/* urlRtmp */, datum, zeit, durationInSeconds, description, thumbnailUrl, imageUrl, keywords);
+                    } else {
+                        Log.fehlerMeldung(-918596307, Log.FEHLER_ART_MREADER, "Mediathek3sat.quicktime", "url passt nicht: " + urlFilm);
+                    }
+                }
+            } catch (Exception ex) {
+                Log.fehlerMeldung(-265847128, Log.FEHLER_ART_MREADER, "MediathekZdf.flash" + senderName, ex, urlFilm);
+            }
+            return ret;
         }
 
         private synchronized String[] getListeThemen() {
