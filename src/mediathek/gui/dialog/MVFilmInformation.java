e8e4f67384655b03b35eacd7f2fce43335db8563
W.Xaver <w.xaver@googlemail.com>
2013-06-26 14:27:49 +0200

    Textarea Zeilenumbruch

--- src/mediathek/gui/dialog/MVFilmInformation.java
+++ src/mediathek/gui/dialog/MVFilmInformation.java
@@ -42,7 +42,7 @@ import mediathek.tool.UrlHyperlinkAction;
  * Display the current film information in a Apple-style HUD window.
  */
 public class MVFilmInformation implements ChangeListener {
-
+    
     private HudWindow hud = null;
     private JDialog dialog = null;
     private JXHyperlink lblUrlThemaField;
@@ -55,7 +55,7 @@ public class MVFilmInformation implements ChangeListener {
     private JButton buttonBild = new JButton("Bild laden");
     private Color foreground, background;
     private DatenFilm aktFilm = new DatenFilm();
-
+    
     public MVFilmInformation(Frame owner, JTabbedPane tabbedPane, DDaten ddaten) {
         this.ddaten = ddaten;
         if (Funktionen.getOs() == Funktionen.OS_LINUX) {
@@ -63,14 +63,14 @@ public class MVFilmInformation implements ChangeListener {
             background = Color.BLACK;
             dialog = new JDialog(ddaten.mediathekGui);
             dialog.setTitle("Filminformation");
-
+            
             for (int i = 0; i < DatenFilm.FILME_MAX_ELEM; ++i) {
                 labelArrNames[i] = new JLabel(DatenFilm.FILME_COLUMN_NAMES[i] + ":");
                 labelArrNames[i].setHorizontalAlignment(SwingConstants.RIGHT);
                 labelArrNames[i].setDoubleBuffered(true);
                 labelArrNames[i].setForeground(foreground);
                 labelArrNames[i].setBackground(background);
-
+                
                 txtArrCont[i] = new JTextField("");
                 txtArrCont[i].setEditable(false);
                 txtArrCont[i].setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
@@ -120,7 +120,7 @@ public class MVFilmInformation implements ChangeListener {
         size.height = 600;
         dialog.setSize(size);
         calculateHudPosition();
-
+        
         tabbedPane.addChangeListener(this);
         new EscBeenden(dialog) {
             @Override
@@ -129,7 +129,7 @@ public class MVFilmInformation implements ChangeListener {
             }
         };
     }
-
+    
     private JComponent setLable() {
         JPanel panel = new JPanel();
         GridBagLayout gridbag = new GridBagLayout();
@@ -153,6 +153,7 @@ public class MVFilmInformation implements ChangeListener {
         textAreaBeschreibung = new JTextArea();
         textAreaBeschreibung.setDoubleBuffered(true);
         textAreaBeschreibung.setLineWrap(true);
+        textAreaBeschreibung.setWrapStyleWord(true);
         textAreaBeschreibung.setBackground(background);
         textAreaBeschreibung.setForeground(foreground);
         textAreaBeschreibung.setOpaque(false);
@@ -191,7 +192,7 @@ public class MVFilmInformation implements ChangeListener {
         panel.add(label);
         return panel;
     }
-
+    
     private void addLable(int i, GridBagLayout gridbag, GridBagConstraints c, JPanel panel) {
         c.gridx = 0;
         c.weightx = 0;
@@ -213,27 +214,27 @@ public class MVFilmInformation implements ChangeListener {
             panel.add(txtArrCont[i]);
         }
     }
-
+    
     private void calculateHudPosition() {
         //FIXME calculate the HUD position
     }
-
+    
     public void show() {
         setAktFilm(false);
         dialog.setVisible(true);
     }
-
+    
     public boolean isVisible() {
         return dialog.isVisible();
     }
-
+    
     public void updateCurrentFilm(DatenFilm film) {
         aktFilm = film;
         if (this.isVisible()) {
             setAktFilm(false);
         }
     }
-
+    
     private void setAktFilm(boolean bild) {
         for (int i = 0; i < txtArrCont.length; ++i) {
             txtArrCont[i].setText(aktFilm.arr[i]);
@@ -271,18 +272,18 @@ public class MVFilmInformation implements ChangeListener {
         }
         dialog.repaint();
     }
-
+    
     @Override
     public void stateChanged(ChangeEvent changeEvent) {
         //Whenever there is a change event, reset HUD info to nothing
         DatenFilm emptyFilm = new DatenFilm();
         updateCurrentFilm(emptyFilm);
     }
-
+    
     private class ViewImage extends JComponent {
-
+        
         private BufferedImage image = null;
-
+        
         public void setImage(String urlStr) {
             if (urlStr.equals("")) {
                 image = null;
@@ -293,15 +294,15 @@ public class MVFilmInformation implements ChangeListener {
             try {
                 image = ImageIO.read(new URL(urlStr));
                 image = scale(image, new Dimension(250, 250));
-                this.setMinimumSize(new Dimension(image.getWidth(), image.getHeight() ));
+                this.setMinimumSize(new Dimension(image.getWidth(), image.getHeight()));
                 this.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
-                this.setSize(new Dimension(image.getWidth() , image.getHeight() ));
+                this.setSize(new Dimension(image.getWidth(), image.getHeight()));
             } catch (Exception ex) {
                 Log.fehlerMeldung(919302497, Log.FEHLER_ART_PROG, MVFilmInformation.class.getName(), ex);
             }
             repaint();
         }
-
+        
         @Override
         protected void paintComponent(Graphics g) {
             if (image != null) {
@@ -311,21 +312,21 @@ public class MVFilmInformation implements ChangeListener {
                 super.paintComponent(g);
             }
         }
-
+        
         private BufferedImage scale(BufferedImage img, Dimension d) {
             float factor = getFactor(img.getWidth(), img.getHeight(), d);
             // create the image
             int w = (int) (img.getWidth() * factor);
             int h = (int) (img.getHeight() * factor);
             BufferedImage scaled = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-
+            
             Graphics2D g = scaled.createGraphics();
             g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
             g.drawImage(img, 0, 0, w, h, null);
             g.dispose();
             return scaled;
         }
-
+        
         float getFactor(int width, int height, Dimension dim) {
             float sx = dim.width / (float) width;
             float sy = dim.height / (float) height;
