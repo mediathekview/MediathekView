13aaaedf4e3ae1f5622cbc0867b7382330593fac
Xaver <w.xaver@googlemail.com>
2013-12-02 20:20:23 +0100

    build.xml 	neue Datei:   build.xml 	geändert:   dist/MediathekView.jar 	neue Datei:   dist/info/6.343.build 	geändert:   dist/lib/MSearch.jar 	geändert:   libs/MSearch.jar 	neue Datei:   nbproject/build-impl.xml 	neue Datei:   nbproject/genfiles.properties 	neue Datei:   nbproject/project.properties 	neue Datei:   nbproject/project.xml 	neue Datei:   post_make.sh 	geändert:   src/version.properties

---
+++ post_make.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+if [ $(hostname) = "beta" ]
+then
+# nur für den Entwicklungsrechner sinnvoll
+
+dir=`dirname "$0"`
+cd "$dir"
+
+# Dateien ins dist-Verzeichnis kopieren
+cp -r res/* dist
+cp -r dist/lib/* libs
+
+# für Netbeans nochmal
+cp -r res/* build
+
+# Aufräumen
+rm dist/README.TXT
+
+# Programmsets ins www-verzeichnis kopieren
+cp src/mediathek/file/*.xml /mnt/daten/www/online/ZDFMediathekView/programmgruppen/
+
+# release
+relNr=$(cat src/version.properties | grep BUILD | sed 's#BUILD=##g')
+datum=$(date +%d.%m.%Y )
+echo Datum: $datum >> dist/info/$relNr.build
+echo MediathekView Buildnummer: $relNr >> dist/info/$relNr.build
+
+# zip erstellen
+cd dist/
+datum=$(date +%Y.%m.%d )
+zip -r MediathekView_3.3.0_$datum.zip .
+cd ..
+
+# Dateien ins share-Verzeichnis von VmWare kopieren
+cp -r dist/* /mnt/daten/virtualbox/share/aktMed
+
+cd $OLDPWD
+
+fi
