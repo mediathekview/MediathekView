58d7ad5658d3c71f8ba7664f742a43c8cf6af3fc
Xaver <w.xaver@googlemail.com>
2014-03-21 17:50:37 +0100

    Fehlermeldung
    Former-commit-id: d63743a19dc451a1510a807f78a8f8c4372adc64

--- src/mediathek/controller/starter/StarterClass.java
+++ src/mediathek/controller/starter/StarterClass.java
@@ -323,6 +323,7 @@ public class StarterClass {
                 BufferedInputStream srcBuffer = null;
                 BufferedOutputStream destBuffer = null;
                 HttpURLConnection conn = null;
+                String responseCode = "";
                 try {
                     URL url = new URL(datenDownload.arr[DatenDownload.DOWNLOAD_URL_NR]);
                     conn = (HttpURLConnection) url.openConnection();
@@ -341,7 +342,8 @@ public class StarterClass {
                     conn.setDoOutput(true);
                     conn.connect();
                     if (conn.getResponseCode() >= 400) {
-                        Log.fehlerMeldung(915236798, Log.FEHLER_ART_PROG, "StartetClass", "HTTP-Fehler: " + conn.getResponseCode() + " " + conn.getResponseMessage());
+                        responseCode = "Responsecode: "+conn.getResponseCode() + ", " + conn.getResponseMessage();
+                        Log.fehlerMeldung(915236798, Log.FEHLER_ART_PROG, "StartetClass.StartenDownload", "HTTP-Fehler: " + conn.getResponseCode() + " " + conn.getResponseMessage());
                     }
                     input = new MVInputStream(conn.getInputStream());
                     start.mVInputStream = input;
@@ -383,8 +385,8 @@ public class StarterClass {
                     }
                     Log.systemMeldung(input.toString());
                 } catch (Exception ex) {
-                    Log.fehlerMeldung(316598941, Log.FEHLER_ART_PROG, "StartetClass.leeresFileLoeschen", ex, "Fehler");
-                    new DialogDownloadfehler(null, ex.getLocalizedMessage(), datenDownload).setVisible(true);
+                    Log.fehlerMeldung(316598941, Log.FEHLER_ART_PROG, "StartetClass.StartenDownload", ex, "Fehler");
+                    new DialogDownloadfehler(null, ex.getLocalizedMessage() + (responseCode.isEmpty() ? "" : "\n" + responseCode), datenDownload).setVisible(true);
                 } finally {
                     try {
                         if (srcBuffer != null) {
@@ -400,7 +402,7 @@ public class StarterClass {
                     }
                 }
             } catch (Exception ex) {
-                Log.fehlerMeldung(502039078, Log.FEHLER_ART_PROG, "StarterClass.StartenDownload-1", ex);
+                Log.fehlerMeldung(502039078, Log.FEHLER_ART_PROG, "StarterClass.StartenDownload", ex);
             }
             if (!start.stoppen) {
                 if (datenDownload.getQuelle() == Start.QUELLE_BUTTON) {
@@ -422,100 +424,6 @@ public class StarterClass {
             notifyStartEvent(datenDownload);
         }
 
-        public void run__() {
-            startmeldung(datenDownload, start);
-            MVInputStream input;
-            try {
-                int len;
-                new File(datenDownload.arr[DatenDownload.DOWNLOAD_ZIEL_PFAD_NR]).mkdirs();
-                datenDownload.mVFilmSize.setSize(laenge(datenDownload.arr[DatenDownload.DOWNLOAD_URL_NR]));
-                datenDownload.mVFilmSize.setAktSize(0);
-                BufferedInputStream srcBuffer = null;
-                BufferedOutputStream destBuffer = null;
-                HttpURLConnection conn = null;
-                try {
-                    URL url = new URL(datenDownload.arr[DatenDownload.DOWNLOAD_URL_NR]);
-                    conn = (HttpURLConnection) url.openConnection();
-                    File file = new File(datenDownload.arr[DatenDownload.DOWNLOAD_ZIEL_PFAD_DATEINAME_NR]);
-                    conn.setRequestProperty("User-Agent", Daten.getUserAgent());
-                    conn.connect();
-                    if (conn.getResponseCode() >= 400) {
-                        Log.fehlerMeldung(915236798, Log.FEHLER_ART_PROG, "StartetClass", "HTTP-Fehler: " + conn.getResponseCode() + " " + conn.getResponseMessage());
-                    }
-                    input = new MVInputStream(conn.getInputStream());
-                    start.mVInputStream = input;
-                    srcBuffer = new BufferedInputStream(input);
-                    destBuffer = new BufferedOutputStream(new FileOutputStream(file), 1024);
-                    byte[] buffer = new byte[1024];
-                    long p, pp = 0;
-                    while ((len = srcBuffer.read(buffer)) != -1 && !start.stoppen) {
-                        destBuffer.write(buffer, 0, len);
-                        datenDownload.mVFilmSize.addAktSize(len);
-                        if (datenDownload.mVFilmSize.getSize() > 0) {
-                            p = (datenDownload.mVFilmSize.getAktSize() * (long) 1000) / datenDownload.mVFilmSize.getSize();
-                            // p muss zwischen 1 und 999 liegen
-                            if (p == 0) {
-                                p = Start.PROGRESS_GESTARTET;
-                            } else if (p >= 1000) {
-                                p = 999;
-                            }
-                            start.percent = (int) p;
-                            if (p != pp) {
-                                pp = p;
-                                // Restzeit ermitteln
-                                if (p > 2) {
-                                    // sonst macht es noch keinen Sinn
-                                    start.bandbreite = input.getBandbreite();
-                                    int diffZeit = start.startZeit.diffInSekunden();
-                                    int restProzent = 1000 - (int) p;
-                                    start.restSekunden = (diffZeit * restProzent / p);
-                                    // anfangen zum Schauen kann man, wenn die Restzeit kürzer ist
-                                    // als die bereits geladene Speilzeit des Films
-                                    bereitsAnschauen(datenDownload);
-                                }
-                                ListenerMediathekView.notify(ListenerMediathekView.EREIGNIS_ART_DOWNLOAD_PROZENT, StarterClass.class.getName());
-                            }
-                        }
-                    }
-                    Log.systemMeldung(input.toString());
-                } catch (Exception ex) {
-                    Log.fehlerMeldung(316598941, Log.FEHLER_ART_PROG, "StartetClass.leeresFileLoeschen", ex, "Fehler");
-                } finally {
-                    try {
-                        if (srcBuffer != null) {
-                            srcBuffer.close();
-                        }
-                        if (destBuffer != null) {
-                            destBuffer.close();
-                        }
-                        if (conn != null) {
-                            conn.disconnect();
-                        }
-                    } catch (Exception e) {
-                    }
-                }
-            } catch (Exception ex) {
-                Log.fehlerMeldung(502039078, Log.FEHLER_ART_PROG, "StarterClass.StartenDownload-1", ex);
-            }
-            if (!start.stoppen) {
-                if (datenDownload.getQuelle() == Start.QUELLE_BUTTON) {
-                    // direkter Start mit dem Button
-                    start.status = Start.STATUS_FERTIG;
-                } else if (pruefen(datenDownload, start)) {
-                    //Anzeige ändern - fertig
-                    start.status = Start.STATUS_FERTIG;
-                } else {
-                    //Anzeige ändern - bei Fehler fehlt der Eintrag
-                    start.status = Start.STATUS_ERR;
-                }
-            }
-            leeresFileLoeschen(new File(datenDownload.arr[DatenDownload.DOWNLOAD_ZIEL_PFAD_DATEINAME_NR]));
-            fertigmeldung(datenDownload, start);
-            start.restSekunden = -1;
-            start.percent = Start.PROGRESS_FERTIG;
-            datenDownload.mVFilmSize.setAktSize(-1);
-            notifyStartEvent(datenDownload);
-        }
     }
 
     private void bereitsAnschauen(DatenDownload datenDownload) {
@@ -533,11 +441,6 @@ public class StarterClass {
         }
     }
 
-//    private void programmBeenden() {
-//        if (ddaten.nachDownloadShutDown) {
-//            // Sicherheitsabfrage, dann beenden
-//        }
-//    }
     private boolean pruefen(DatenDownload datenDownload, Start start) {
         //prüfen ob der Downoad geklappt hat und die Datei existiert und eine min. Grüße hat
         boolean ret = false;
