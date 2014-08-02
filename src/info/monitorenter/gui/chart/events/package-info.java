dc9a2ad9b5391e7913eda0acd505b6222d9b8693
Xaver <w.xaver@googlemail.com>
2014-08-02 18:49:14 +0200

    chart2

--- src/info/monitorenter/gui/chart/events/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Package for {@link java.awt.event.ActionEvent} implementations.
- * <p>
- * 
- * @author <a href="mailto:Achim.Westermann@gmx.de">Achim Westermann </a>
- **/
-package info.monitorenter.gui.chart.events;
