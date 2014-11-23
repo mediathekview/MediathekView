07eb698574c5564719b4c8675eeccc4a10f83cb2
Xaver <w.xaver@googlemail.com>
2014-11-23 15:29:32 +0100

    KIKA: Downloaderror: not complete
    Former-commit-id: 6d4997817ced7dd478927b5012964624602363f1

--- src/mediathek/controller/starter/RuntimeExec.java
+++ src/mediathek/controller/starter/RuntimeExec.java
@@ -33,16 +33,17 @@ public class RuntimeExec {
 
     private static final int INPUT = 1;
     private static final int ERROR = 2;
-    private String prog;
+    private final String prog;
     Thread clearIn;
     Thread clearOut;
     private Process process = null;
     Start start;
     private static int procnr = 0; //TH
-    private Pattern patternFlvstreamer = Pattern.compile("([0-9]*.[0-9]{1}%)");
-    private Pattern patternFfmpeg = Pattern.compile("(?<=  Duration: )[^,]*"); // Duration: 00:00:30.28, start: 0.000000, bitrate: N/A
-    private Pattern patternZeit = Pattern.compile("(?<=time=)[^ ]*");  // frame=  147 fps= 17 q=-1.0 size=    1588kB time=00:00:05.84 bitrate=2226.0kbits/s   
-    private Pattern patternSize = Pattern.compile("(?<=size=)[^k]*");  // frame=  147 fps= 17 q=-1.0 size=    1588kB time=00:00:05.84 bitrate=2226.0kbits/s   
+    private final Pattern patternFlvstreamer = Pattern.compile("([0-9]*.[0-9]{1}%)");
+    private final Pattern patternFlvstreamerComplete = Pattern.compile("Download complete");
+    private final Pattern patternFfmpeg = Pattern.compile("(?<=  Duration: )[^,]*"); // Duration: 00:00:30.28, start: 0.000000, bitrate: N/A
+    private final Pattern patternZeit = Pattern.compile("(?<=time=)[^ ]*");  // frame=  147 fps= 17 q=-1.0 size=    1588kB time=00:00:05.84 bitrate=2226.0kbits/s   
+    private final Pattern patternSize = Pattern.compile("(?<=size=)[^k]*");  // frame=  147 fps= 17 q=-1.0 size=    1588kB time=00:00:05.84 bitrate=2226.0kbits/s   
 
     private double totalSecs = 0;
     private long aktSize = 0, oldSize = 0, oldSecs = 0;
@@ -71,7 +72,7 @@ public class RuntimeExec {
             clearIn.start();
             clearOut.start();
         } catch (Exception ex) {
-            Log.fehlerMeldung(450028932,  "RuntimeExec.exec", ex, "Fehler beim Starten");
+            Log.fehlerMeldung(450028932, "RuntimeExec.exec", ex, "Fehler beim Starten");
         }
         return process;
     }
@@ -126,9 +127,9 @@ public class RuntimeExec {
         }
 
         private void GetPercentageFromErrorStream(String input) {
-            // by: siedlerchr
-            // für den flvstreamer und rtmpdump
-            Matcher matcher = patternFlvstreamer.matcher(input);
+            // by: siedlerchr für den flvstreamer und rtmpdump
+            Matcher matcher;
+            matcher = patternFlvstreamer.matcher(input);
             if (matcher.find()) {
                 try {
                     String prozent = matcher.group();
@@ -137,66 +138,73 @@ public class RuntimeExec {
                     meldenDouble(d);
                 } catch (Exception ex) {
                     ListenerMediathekView.notify(ListenerMediathekView.EREIGNIS_ART_DOWNLOAD_PROZENT, RuntimeExec.class.getName());
-                    Log.fehlerMeldung(912036780,  "RuntimeExec.GetPercentageFromErrorStream-1", input);
+                    Log.fehlerMeldung(912036780, "RuntimeExec.GetPercentageFromErrorStream-1", input);
                 }
-            } else {
-                // für ffmpeg
-                // ffmpeg muss dazu mit dem Parameter -i gestartet werden:
-                // -i %f -acodec copy -vcodec copy -y **
-                try {
-                    // Gesamtzeit
-                    matcher = patternFfmpeg.matcher(input);
-                    if (matcher.find()) {
-                        // Find duration
-                        String dauer = matcher.group().trim();
-                        String[] hms = dauer.split(":");
-                        totalSecs = Integer.parseInt(hms[0]) * 3600
-                                + Integer.parseInt(hms[1]) * 60
-                                + Double.parseDouble(hms[2]);
-                    }
-                    // Bandbreite
-                    matcher = patternSize.matcher(input);
-                    if (matcher.find()) {
-                        String s = matcher.group().trim();
-                        if (!s.isEmpty()) {
-                            try {
-                                aktSize = Integer.parseInt(s.replace("kB", ""));
-                                datenDownload.mVFilmSize.setAktSize(aktSize * 1_000);
-                                long akt = start.startZeit.diffInSekunden();
-                                if (oldSecs < akt - 5) {
-                                    start.bandbreite = (aktSize - oldSize) * 1_000 / (akt - oldSecs);
+                return;
+            }
+            matcher = patternFlvstreamerComplete.matcher(input);
+            if (matcher.find()) {
+                // dann ist der Download fertig, zur sicheren Erkennung von 100%
+                meldenDouble(100);
+                return;
+            }
+
+            // für ffmpeg
+            // ffmpeg muss dazu mit dem Parameter -i gestartet werden:
+            // -i %f -acodec copy -vcodec copy -y **
+            try {
+                // Gesamtzeit
+                matcher = patternFfmpeg.matcher(input);
+                if (matcher.find()) {
+                    // Find duration
+                    String dauer = matcher.group().trim();
+                    String[] hms = dauer.split(":");
+                    totalSecs = Integer.parseInt(hms[0]) * 3600
+                            + Integer.parseInt(hms[1]) * 60
+                            + Double.parseDouble(hms[2]);
+                }
+                // Bandbreite
+                matcher = patternSize.matcher(input);
+                if (matcher.find()) {
+                    String s = matcher.group().trim();
+                    if (!s.isEmpty()) {
+                        try {
+                            aktSize = Integer.parseInt(s.replace("kB", ""));
+                            datenDownload.mVFilmSize.setAktSize(aktSize * 1_000);
+                            long akt = start.startZeit.diffInSekunden();
+                            if (oldSecs < akt - 5) {
+                                start.bandbreite = (aktSize - oldSize) * 1_000 / (akt - oldSecs);
 //                                    long si = aktSize - oldSize;
 //                                    long ti = (akt - oldSecs);
-                                    oldSecs = akt;
-                                    oldSize = aktSize;
-                                }
-                            } catch (NumberFormatException ignored) {
+                                oldSecs = akt;
+                                oldSize = aktSize;
                             }
+                        } catch (NumberFormatException ignored) {
                         }
                     }
-                    // Fortschritt
-                    matcher = patternZeit.matcher(input);
-                    if (totalSecs > 0 && matcher.find()) {
-                        // ffmpeg    1611kB time=00:00:06.73 bitrate=1959.7kbits/s   
-                        // avconv    size=   26182kB time=100.96 bitrate=2124.5kbits/s 
-                        String zeit = matcher.group();
-                        if (zeit.contains(":")) {
-                            String[] hms = zeit.split(":");
-                            aktSecs = Integer.parseInt(hms[0]) * 3600
-                                    + Integer.parseInt(hms[1]) * 60
-                                    + Double.parseDouble(hms[2]);
-                            double d = aktSecs / totalSecs * 100;
-                            meldenDouble(d);
-                        } else {
-                            double aktSecs = Double.parseDouble(zeit);
-                            double d = aktSecs / totalSecs * 100;
-                            meldenDouble(d);
-                        }
-                    }
-                } catch (Exception ex) {
-                    ListenerMediathekView.notify(ListenerMediathekView.EREIGNIS_ART_DOWNLOAD_PROZENT, RuntimeExec.class.getName());
-                    Log.fehlerMeldung(912036780,  "RuntimeExec.GetPercentageFromErrorStream-2", input);
                 }
+                // Fortschritt
+                matcher = patternZeit.matcher(input);
+                if (totalSecs > 0 && matcher.find()) {
+                    // ffmpeg    1611kB time=00:00:06.73 bitrate=1959.7kbits/s   
+                    // avconv    size=   26182kB time=100.96 bitrate=2124.5kbits/s 
+                    String zeit = matcher.group();
+                    if (zeit.contains(":")) {
+                        String[] hms = zeit.split(":");
+                        aktSecs = Integer.parseInt(hms[0]) * 3600
+                                + Integer.parseInt(hms[1]) * 60
+                                + Double.parseDouble(hms[2]);
+                        double d = aktSecs / totalSecs * 100;
+                        meldenDouble(d);
+                    } else {
+                        double aktSecs = Double.parseDouble(zeit);
+                        double d = aktSecs / totalSecs * 100;
+                        meldenDouble(d);
+                    }
+                }
+            } catch (Exception ex) {
+                ListenerMediathekView.notify(ListenerMediathekView.EREIGNIS_ART_DOWNLOAD_PROZENT, RuntimeExec.class.getName());
+                Log.fehlerMeldung(912036780, "RuntimeExec.GetPercentageFromErrorStream-2", input);
             }
         }
 
