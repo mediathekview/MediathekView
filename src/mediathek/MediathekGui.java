dadadfdc3881211330292e2ece4d351a81284d60
Christian F <crystalpalace1977@googlemail.com>
2015-03-07 23:10:03 +0100

    refactor OSX support for native Apple class use

--- src/mediathek/MediathekGui.java
+++ src/mediathek/MediathekGui.java
@@ -33,7 +33,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.LinkedList;
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
 import javax.swing.InputMap;
@@ -208,6 +207,7 @@ public final class MediathekGui extends JFrame {
     }
 
     public MediathekGui(String[] ar, final boolean maximized) {
+        super();
         initializeSplashScreen();
 
         String pfad = "";
@@ -799,94 +799,6 @@ public final class MediathekGui extends JFrame {
                 }
             }
         });
-    }
-
-    /**
-     * This thread will update the percentage drawn on the dock icon on OS X.
-     */
-    private class OsxIndicatorThread extends Thread {
-
-        /**
-         * The BufferedImage of the OS X application icon.
-         */
-        private Image OsxApplicationIconImage = null;
-        /**
-         * Stores the application image with the progress drawn on it
-         */
-        private BufferedImage newApplicationIcon = null;
-        private final int appIconWidth;
-        private final int appIconHeight;
-        private int numOfDownloadsActive;
-        private double accumPercentage;
-        private double oldPercentage;
-        private boolean bFirstUpdate = true;
-
-        public OsxIndicatorThread() {
-            setName("OSX dock icon update thread");
-
-            OsxApplicationIconImage = Application.getApplication().getDockIconImage();
-            appIconWidth = OsxApplicationIconImage.getWidth(null);
-            appIconHeight = OsxApplicationIconImage.getHeight(null);
-            newApplicationIcon = new BufferedImage(appIconWidth, appIconHeight, BufferedImage.TYPE_INT_ARGB);
-        }
-
-        /**
-         * Draw the progress bar into the application icon and set dock icon.
-         *
-         * @param progressBarWidth width of the bar.
-         */
-        private void drawAndSetApplicationIconWithProgress(int progressBarWidth) {
-            Graphics g = newApplicationIcon.getGraphics();
-            g.drawImage(OsxApplicationIconImage, 0, 0, null);
-            g.setColor(Color.RED);
-            g.fillRect(0, appIconHeight - 20, appIconWidth, 20);
-            g.setColor(Color.GREEN);
-            g.fillRect(0, appIconHeight - 20, progressBarWidth, 20);
-            g.dispose();
-            Application.getApplication().setDockIconImage(newApplicationIcon);
-        }
-
-        @Override
-        public void run() {
-            try {
-                while (!isInterrupted()) {
-                    numOfDownloadsActive = 0;
-                    accumPercentage = 0.0;
-
-                    //only count running/active downloads and calc accumulated progress..
-                    LinkedList<DatenDownload> activeDownloadList = Daten.listeDownloads.getListOfStartsNotFinished(Start.QUELLE_ALLE);
-                    for (DatenDownload download : activeDownloadList) {
-                        if (download.start != null && download.start.status == Start.STATUS_RUN) {
-                            numOfDownloadsActive++;
-                            accumPercentage += download.start.percent / 10.0;
-                        }
-                    }
-
-                    final double percentage = accumPercentage / numOfDownloadsActive;
-                    final int progressBarWidth = (int) ((appIconWidth / 100.0) * percentage);
-
-                    if (bFirstUpdate) {
-                        drawAndSetApplicationIconWithProgress(progressBarWidth);
-                        bFirstUpdate = false;
-                    }
-
-                    //update in 1pct steps...
-                    if (percentage % 1 == 0) {
-                        //if icon was already drawn, don´ do it again
-                        if (oldPercentage != percentage) {
-                            drawAndSetApplicationIconWithProgress(progressBarWidth);
-                        }
-
-                        oldPercentage = percentage;
-                    }
-                    sleep(500);
-                }
-            } catch (Exception ignored) {
-            } finally {
-                //reset the application dock icon
-                Application.getApplication().setDockIconImage(OsxApplicationIconImage);
-            }
-        }
     }
 
     /**
