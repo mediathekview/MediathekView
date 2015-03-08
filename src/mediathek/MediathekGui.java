c5437dadb25952afac9136ea6c28786e80557108
Christian F <crystalpalace1977@googlemail.com>
2015-03-08 15:21:39 +0100

    OSX updates
    Former-commit-id: be1bf4dbe09dd11e546584b685d79f6f695fe644

--- src/mediathek/MediathekGui.java
+++ src/mediathek/MediathekGui.java
@@ -1171,34 +1171,12 @@ public class MediathekGui extends JFrame {
     /**
      * Shutdown the computer depending on Operating System.
      */
-    private void shutdownComputer() {
+    protected void shutdownComputer() {
         String strShutdownCommand = "";
 
         switch (Funktionen.getOs()) {
             case LINUX:
                 strShutdownCommand = "shutdown -h now";
-                break;
-
-            case MAC:
-                //we cannot shutdown the system while we are running...
-                //MV (or java) will prevent OS X shutdown process and there seems to be no way around it.
-                //NASTY WORKAROUND:
-                //use applescript to execute a scriptlet application which will wait 5 seconds until it
-                //asks the system to shut down
-                //meanwhile we MUST terminate MV WITHIN 5 seconds in order not to interrupt the
-                //shutdown process :(
-                //AND this whole shit works ONLY with osascript, not with the java script engine...
-                //Scriptlet(executable) content:
-                //delay 5
-                //tell application "system events" to shut down
-                //EOF
-                //The OSX_Shutdown scriptlet application is provided in the official MV app bundle.
-                try {
-                    final ProcessBuilder builder = new ProcessBuilder("/usr/bin/osascript", "-e");
-                    builder.command().add("tell application \"OSX_Shutdown\" to activate");
-                    builder.start();
-                } catch (Exception ignored) {
-                }
                 break;
 
             case WIN32:
--- src/mediathek/mac/MediathekGuiMac.java
+++ src/mediathek/mac/MediathekGuiMac.java
@@ -133,4 +133,27 @@ public class MediathekGuiMac extends MediathekGui {
         setupOsxDockIconBadge();
     }
 
+    @Override
+    protected void shutdownComputer() {
+        //we cannot shutdown the system while we are running...
+        //MV (or java) will prevent OS X shutdown process and there seems to be no way around it.
+        //NASTY WORKAROUND:
+        //use applescript to execute a scriptlet application which will wait 5 seconds until it
+        //asks the system to shut down
+        //meanwhile we MUST terminate MV WITHIN 5 seconds in order not to interrupt the
+        //shutdown process :(
+        //AND this whole shit works ONLY with osascript, not with the java script engine...
+        //Scriptlet(executable) content:
+        //delay 5
+        //tell application "system events" to shut down
+        //EOF
+        //The OSX_Shutdown scriptlet application is provided in the official MV app bundle.
+        try {
+            final ProcessBuilder builder = new ProcessBuilder("/usr/bin/osascript", "-e");
+            builder.command().add("tell application \"OSX_Shutdown\" to activate");
+            builder.start();
+        } catch (Exception ignored) {
+        }
+    }
+
 }
