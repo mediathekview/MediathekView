64b4ddb6bb90a374c2aa051382393cb269f841c1
Christian F <crystalpalace1977@googlemail.com>
2015-03-09 18:32:33 +0100

    -use new logging functions
    Former-commit-id: a6d684f1431147527b9aada7233dd2d22bfa50e3

--- src/mediathek/controller/Log.java
+++ src/mediathek/controller/Log.java
@@ -117,8 +117,7 @@ public class Log {
         fehlermeldung_(fehlerNummer, null, new String[]{text});
     }
 
-    @Deprecated
-    public static synchronized void fehlerMeldung(int fehlerNummer, String klasse, String[] text) {
+    public static synchronized void fehlerMeldung(int fehlerNummer, String[] text) {
         fehlermeldung_(fehlerNummer, null, text);
     }
 
