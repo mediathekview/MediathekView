ab99146ac5c7592c9e350a1db1984f499804c752
Xaver <w.xaver@googlemail.com>
2015-05-23 16:06:16 +0200

    search local media

---
+++ src/mediathek/tool/CellRendererMediaDB.java
@@ -0,0 +1,55 @@
+/*    
+ *    MediathekView
+ *    Copyright (C) 2008   W. Xaver
+ *    W.Xaver[at]googlemail.com
+ *    http://zdfmediathk.sourceforge.net/
+ *    
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mediathek.tool;
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import mediathek.controller.Log;
+
+public class CellRendererMediaDB extends DefaultTableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(
+            JTable table,
+            Object value,
+            boolean isSelected,
+            boolean hasFocus,
+            int row,
+            int column) {
+        setBackground(null);
+        setForeground(null);
+        setFont(null);
+        setIcon(null);
+        setHorizontalAlignment(SwingConstants.LEADING);
+        super.getTableCellRendererComponent(
+                table, value, isSelected, hasFocus, row, column);
+        try {
+            int c = table.convertColumnIndexToModel(column);
+            if (c == MVMediaDB.MEDIA_DB_SIZE_NR) {
+                setHorizontalAlignment(SwingConstants.CENTER);
+            }
+        } catch (Exception ex) {
+            Log.fehlerMeldung(976432158, ex);
+        }
+        return this;
+    }
+}
