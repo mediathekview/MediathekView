30e047ef18b109fae1514db99a40d21124bbf235
Xaver <w.xaver@googlemail.com>
2015-07-23 19:25:15 +0200

    SRF, PHOENIX

--- src/mediathek/gui/dialog/DialogEditDownload.java
+++ src/mediathek/gui/dialog/DialogEditDownload.java
@@ -32,7 +32,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
