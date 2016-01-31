d4ea0a44cbba3bf9989b5761b46bea8a7374dc43
Xaver <w.xaver@googlemail.com>
2016-01-31 18:02:33 +0100

    crawler
    Former-commit-id: 8f4d134e041fab9c5dedb043b20ecf88300a1170

--- src/mediathek/MediathekGui.java
+++ src/mediathek/MediathekGui.java
@@ -33,7 +33,6 @@ import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
