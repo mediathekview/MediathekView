5da29e82c4bb078bbc8d5ff6055b6d3624ca40c3
Xaver <w.xaver@googlemail.com>
2016-07-03 20:00:06 +0200

    Win Startdateien
    Former-commit-id: d191a15ec473eb87335bd89cd7ddcfe010aaac86

--- dist/Windows/MediathekView__Start.bat
+++ dist/Windows/MediathekView__Start.bat
@@ -11,14 +11,14 @@ start /min javaw -Xms128m -Xmx1024m -jar "C:\Users\PFAD\MediathekView.jar"
 :: Start mit mehr Speicher für das Programm
 :: java -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
 
+:: Start mit noch mehr Speicher, falls neue Filmliste trotzdem nicht voll geladen werden kann
+:: java -Xms128M -Xmx1.5G -jar "C:\Users\PFAD\MediathekView.jar
 
 :: Start mit Pfad zu Java
 :: "%path-to-32-Bit-java%\javaw.exe"  -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
 
-
 :: Es wird ein Proxyserver verwendet.
 :: java -jar -Dhttp.proxyHost=proxyserver -Dhttp.proxyPort=8080 "C:\Users\PFAD\MediathekView.jar"
-
 
 :: Der Parameter "-Djava.net.preferIPv4Stack=true", "-Djava.net.preferIPv6Addresses=true" ermöglicht eine 
 :: Verbindung zum Internet, wenn der verwendete Netzwerk-Stack von Java nicht automatisch 
