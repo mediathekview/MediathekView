4cf66d69086b0159f6457da21703799a3a1fde74
Xaver <w.xaver@googlemail.com>
2016-07-14 18:46:14 +0200

    Win startdatei.bat

--- dist/Windows/MediathekView__Start.bat
+++ dist/Windows/MediathekView__Start.bat
@@ -10,12 +10,12 @@
 
 
 
-
-:: =============================================
-
+:: ================================================
 :: Das sind verschiedene Möglichkeiten das Programm
 :: zu starten, die anderen Aufrufe sind auskommentiert
 :: der Pfad zum Programm "PFAD" muss angepasst werden
+:: Durch Entfernen des "::" vor einer Zeile wird die Zeile als Befehl interpretiert.
+:: Durch Schreiben eines "::" zu Beginn einer Zeile wird diese nicht mehr als Befehl interpretiert.
 
 
 :: Start in einer extra Dos-Box die minimiert startet
@@ -26,14 +26,27 @@ start /min javaw -Xms128m -Xmx1024m -jar "C:\Users\PFAD\MediathekView.jar"
 :: Start mit mehr Speicher für das Programm
 :: java -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
 
+
 :: Start mit noch mehr Speicher, falls neue Filmliste trotzdem nicht voll geladen werden kann
 :: java -Xms128M -Xmx1.5G -jar "C:\Users\PFAD\MediathekView.jar
+
 
 :: Start mit Pfad zu Java
 :: "%path-to-32-Bit-java%\javaw.exe"  -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
 
+
+:: "Einstellungen/.mediathek3" legt den Ort relativ zur Datei "MediathekView.jar"
+:: im MV-Programmordner fest. Wer keine portablen Einstellungen verwenden will,
+:: löscht diese Zeichenfolge. Wer die Programm-Einstellungen zum Beispiel auf das
+:: Laufwerk D: legen will, kann alternativ einen entsprechenden Pfad angeben,
+:: z.B. "D:\MediathekView\Einstellungen\.mediathek3". Die Ordner "Einstellungen" bzw.
+:: "MediathekView" müssen vorhanden sein bzw. zuerst erstellt werden.
+:: start javaw -Xms128M -Xmx1G -jar MediathekView.jar "Einstellungen\.mediathek3" exit
+
+
 :: Es wird ein Proxyserver verwendet.
 :: java -jar -Dhttp.proxyHost=proxyserver -Dhttp.proxyPort=8080 "C:\Users\PFAD\MediathekView.jar"
+
 
 :: Der Parameter "-Djava.net.preferIPv4Stack=true", "-Djava.net.preferIPv6Addresses=true" ermöglicht eine 
 :: Verbindung zum Internet, wenn der verwendete Netzwerk-Stack von Java nicht automatisch 
