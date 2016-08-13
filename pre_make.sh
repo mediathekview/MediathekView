3d523d13c4be8eed1ac1a352d81fee4fd559fa00
Xaver <w.xaver@googlemail.com>
2016-08-13 19:56:13 +0200

    buildNr
    Former-commit-id: 19adbf0a58ee8d777c53fb28619a1a59815a22a5

--- pre_make.sh
+++ pre_make.sh
@@ -4,24 +4,27 @@
 #BUILD=003
 #DATE=13.08.2016 / 18\:34\:42
 
-
+echo ========================
 ver=13
-echo $ver
+echo Version: $ver
 
-nr=$(cat src/version.properties | grep BUILD | sed 's#BUILD=##g')
-echo $nr
+nr=$(cat src/version.properties | grep NR | sed 's#NR=##g')
+echo Nr-vor:  $nr
 
 nr=$((nr + 1))
-echo $nr
+echo Nr-nach: $nr
 
 buildDate=$(date +%d.%m.%y\ %H:%M:%S)
-echo $buildDate
+echo Date:    $buildDate
+echo ========================
 
 echo VERSION=$ver > src/version.properties
-echo BUILD=$nr >> src/version.properties
+echo NR=$nr >> src/version.properties
+echo BUILD=$ver-$nr >> src/version.properties
 echo DATE=$buildDate >> src/version.properties
 
 echo VERSION=$ver > /home/emil/daten/software/mediathek/mSearch/src/version.properties
-echo BUILD=$nr >> /home/emil/daten/software/mediathek/mSearch/src/version.properties
+echo NR=$nr >> /home/emil/daten/software/mediathek/mSearch/src/version.properties
+echo BUILD=$ver-$nr >> /home/emil/daten/software/mediathek/mSearch/src/version.properties
 echo DATE=$buildDate >> /home/emil/daten/software/mediathek/mSearch/src/version.properties
 
