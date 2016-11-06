f143547fda9b3af614cc24d9598e79fefe7581f7
Christian F <crystalpalace1977@googlemail.com>
2016-11-06 12:57:13 +0100

    separate spotlight comment writing, OSX specific-function

---
+++ src/mediathek/mac/SpotlightCommentWriter.java
@@ -0,0 +1,96 @@
+package mediathek.mac;
+
+import mSearch.daten.DatenFilm;
+import mSearch.tool.Log;
+import mediathek.config.Daten;
+import mediathek.daten.DatenDownload;
+import org.jdesktop.swingx.JXErrorPane;
+import org.jdesktop.swingx.error.ErrorInfo;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.swing.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+/**
+ * Writes spotlight comments to the downloaded file on OS X.
+ */
+public class SpotlightCommentWriter {
+    public SpotlightCommentWriter() {
+    }
+
+    /**
+     * Log that MV wasn´t used via the official mac app.
+     * This is relevant to know for bug reports.
+     */
+    private void logUnofficialMacAppUse() {
+        Log.errorLog(915263987, "MV wird NICHT über die offizielle Mac App genutzt.");
+    }
+
+    /**
+     * This will write the content of the film description into the OS X Finder Info Comment Field.
+     * This enables Spotlight to search for these tags.
+     *
+     * @param datenDownload The download information object
+     */
+    public void writeComment(final DatenDownload datenDownload) {
+        if (datenDownload.film == null) {
+            // kann bei EinmalDownloads nach einem Neuladen der Filmliste/Programmneustart der Fall sein
+            return;
+        }
+        final Path filmPath = Paths.get(datenDownload.arr[DatenDownload.DOWNLOAD_ZIEL_PFAD_DATEINAME]);
+        if (Files.exists(filmPath)) {
+            final String strFilePath = filmPath.toString();
+            String strComment = datenDownload.film.arr[DatenFilm.FILM_BESCHREIBUNG];
+            if (strComment != null) {
+                //no need to write spotlight data when there is no description...
+                if (strComment.isEmpty()) {
+                    return;
+                }
+
+                //replace quotation marks...
+                strComment = strComment.replace("\"", "\\\"");
+
+                final String script = "tell application \"Finder\"\n"
+                        + "set my_file to POSIX file \"" + strFilePath + "\" as alias\n"
+                        + "set comment of my_file to \"" + strComment + "\"\n"
+                        + "end tell\n";
+                try {
+                    //FIXME AppleScript support not available when Java6 Apple ist not installed
+                    //solution -> use osascript process
+                    final ScriptEngineManager mgr = new ScriptEngineManager();
+                    final ScriptEngine engine = mgr.getEngineByName("AppleScript");
+                    engine.eval(script);
+                } catch (Exception ex) {
+                    if (Daten.mediathekGui != null) {
+                        SwingUtilities.invokeLater(() -> {
+                            final ErrorInfo info = new ErrorInfo(null,
+                                    "<html>Es trat ein Fehler beim Schreiben des Spotlight-Kommentars auf.<br>" +
+                                            "Sollte dieser häufiger auftreten kontaktieren Sie bitte " +
+                                            "das Entwicklerteam.</html>",
+                                    null,
+                                    "category",
+                                    ex,
+                                    Level.SEVERE,
+                                    null);
+                            JXErrorPane.showDialog(Daten.mediathekGui, info);
+                        });
+                    }
+                    Log.errorLog(915263987, "Fehler beim Spotlight schreiben" + filmPath.toString());
+                    //AppleScript may not be available if user does not use the official MacApp.
+                    //We need to log that as well if there are error reports.
+                    try {
+                        if (!System.getProperty("OSX_OFFICIAL_APP").equalsIgnoreCase("true")) {
+                            logUnofficialMacAppUse();
+                        }
+                    } catch (NullPointerException ignored) {
+                        logUnofficialMacAppUse();
+                    }
+                }
+            }
+        }
+    }
+}
