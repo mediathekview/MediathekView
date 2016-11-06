c0efeb63833e6920a414152920e1f69ed7c7eb3c
Nicklas Wiegandt <Nicklas2751@users.noreply.github.com>
2016-11-06 16:59:15 +0100

    Replaced Ant with gradle (#132)
    * Finnished the most gradle work
    
    Missing: .exe generation for windows
    
    * Removed ant.
    
    * Fixed broken libs
    
    * Added travis publish
    
    * Fixed travis config

--- start-noGui.sh
+++ start-noGui.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 #
-java -jar /mnt/daten/software/mediathek/mediathek/dist/MediathekView.jar /tmp -nogui -i "http://178.77.79.81/mediathek3/Mediathek_02.zip"
+java -jar build/libs/MediathekView-13.jar /tmp -nogui -i "http://178.77.79.81/mediathek3/Mediathek_02.zip"
