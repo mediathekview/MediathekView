b52009804f778c6cb3590d50d5ded467db92a20f
Nicklas Wiegandt <nicklas@wiegandt.net>
2016-11-23 19:53:40 +0100

    Setting the jar file name dynamically
    Former-commit-id: 2e8f67c6fb0bc74677b2f330e154e8b2aa0f2e65

--- res/Portable/MediathekView__Mac_Portable.command
+++ res/Portable/MediathekView__Mac_Portable.command
@@ -1,19 +1,19 @@
 #!/bin/sh
 #
 # Wenn der Arbeitsspeicher knapp ist, kann das helfen:
-# java -Xdock:name="MediathekView" -Xms128M -Xmx1G -jar ./MediathekView.jar $*
+# java -Xdock:name="MediathekView" -Xms128M -Xmx1G -jar ./@JARNAME@ $*
 
 dir=`dirname "$0"`
 cd "$dir"
 
-if [ -f ../MediathekView.jar ]; then
+if [ -f ../@JARNAME@ ]; then
    cd ..
 fi
 
 if [ -n "$JAVA_HOME" ]; then
-   $JAVA_HOME/bin/java -Xdock:name="MediathekView" -jar ./MediathekView.jar Einstellungen/.mediathek3 $*
+   $JAVA_HOME/bin/java -Xdock:name="MediathekView" -jar ./@JARNAME@ Einstellungen/.mediathek3 $*
 else
-   java -Xdock:name="MediathekView" -jar ./MediathekView.jar Einstellungen/.mediathek3 $*
+   java -Xdock:name="MediathekView" -jar ./@JARNAME@ Einstellungen/.mediathek3 $*
 fi
 cd $OLDPWD
 
--- res/Windows/MediathekView__Start.bat
+++ res/Windows/MediathekView__Start.bat
@@ -6,7 +6,7 @@
 :: Nachdem die Einstellungen geändert wurden, und
 :: MV neu gestartet wird, versuch es hiermit:
  
-:: java -Xms128M -Xmx256M -jar ./MediathekView.jar
+:: java -Xms128M -Xmx256M -jar ./@JARNAME@
 
 
 
@@ -21,39 +21,39 @@
 :: Start in einer extra Dos-Box die minimiert startet
 :: Die Parameter "-Xms128M -Xmx1G" helfen bei geringem Arbeitsspeicher. 
 
-start /min javaw -Xms128m -Xmx1024m -jar "C:\Users\PFAD\MediathekView.jar"
+start /min javaw -Xms128m -Xmx1024m -jar "C:\Users\PFAD\@JARNAME@"
 
 
 :: Start mit mehr Speicher für das Programm
-:: java -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
+:: java -Xms128M -Xmx1G -jar "C:\Users\PFAD\@JARNAME@"
 
 
 :: Start mit noch mehr Speicher, falls neue Filmliste trotzdem nicht voll geladen werden kann
-:: java -Xms128M -Xmx1.5G -jar "C:\Users\PFAD\MediathekView.jar
+:: java -Xms128M -Xmx1.5G -jar "C:\Users\PFAD\@JARNAME@
 
 
 :: Start mit Pfad zu Java
-:: "%path-to-32-Bit-java%\javaw.exe"  -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
+:: "%path-to-32-Bit-java%\javaw.exe"  -Xms128M -Xmx1G -jar "C:\Users\PFAD\@JARNAME@"
 
 
-:: "Einstellungen/.mediathek3" legt den Ort relativ zur Datei "MediathekView.jar"
+:: "Einstellungen/.mediathek3" legt den Ort relativ zur Datei "@JARNAME@"
 :: im MV-Programmordner fest. Wer keine portablen Einstellungen verwenden will,
 :: löscht diese Zeichenfolge. Wer die Programm-Einstellungen zum Beispiel auf das
 :: Laufwerk D: legen will, kann alternativ einen entsprechenden Pfad angeben,
 :: z.B. "D:\MediathekView\Einstellungen\.mediathek3". Die Ordner "Einstellungen" bzw.
 :: "MediathekView" müssen vorhanden sein bzw. zuerst erstellt werden.
-:: start javaw -Xms128M -Xmx1G -jar MediathekView.jar "Einstellungen\.mediathek3" exit
+:: start javaw -Xms128M -Xmx1G -jar @JARNAME@ "Einstellungen\.mediathek3" exit
 
 
 :: Es wird ein Proxyserver verwendet.
-:: java -jar -Dhttp.proxyHost=proxyserver -Dhttp.proxyPort=8080 "C:\Users\PFAD\MediathekView.jar"
+:: java -jar -Dhttp.proxyHost=proxyserver -Dhttp.proxyPort=8080 "C:\Users\PFAD\@JARNAME@"
 
 
 :: Der Parameter "-Djava.net.preferIPv4Stack=true", "-Djava.net.preferIPv6Addresses=true" ermöglicht eine 
 :: Verbindung zum Internet, wenn der verwendete Netzwerk-Stack von Java nicht automatisch 
 :: richtig erkannt wird, wodurch die Filmliste nicht geladen werden könnte.
-:: java -Djava.net.preferIPv4Stack=true -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
-:: java -Djava.net.preferIPv6Addresses=true -Xms128M -Xmx1G -jar "C:\Users\PFAD\MediathekView.jar"
+:: java -Djava.net.preferIPv4Stack=true -Xms128M -Xmx1G -jar "C:\Users\PFAD\@JARNAME@"
+:: java -Djava.net.preferIPv6Addresses=true -Xms128M -Xmx1G -jar "C:\Users\PFAD\@JARNAME@"
 
 
 :: -Dhttp.proxyHost=proxyserver
