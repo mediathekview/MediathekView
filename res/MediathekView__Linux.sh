5a7dabbedf8bd55e532697721522d2fefd1ff8ba
Nicklas Wiegandt <Nicklas2751@users.noreply.github.com>
2016-11-30 03:01:50 +0100

    Fixing #156 (#157)
    * Setting the jar file name dynamically
    
    * Added gradle tasks to debug and run
    
    * Fixed travis jar name for deploy
    
    * Moved import to other imports.
    
    * Probleme mit Icons kopieren und copyright Ordner entfernt. gradle-version gedumpt auf 3.0 für eclipse gradle import problem.
    
    
    Former-commit-id: d592589721326aec083208e04bb16d3985b6dc35

--- res/MediathekView__Linux.sh
+++ res/MediathekView__Linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Wenn der Arbeitsspeicher knapp ist, kann das helfen:
-# java -Xms128M -Xmx1G -jar ./MediathekView.jar "$@"
+# java -Xms128M -Xmx1G -jar ./@JARNAME@ "$@"
 
 
 #dir=`dirname "$0"`
@@ -9,8 +9,8 @@ dir=$(dirname $(readlink -f "$0"))
 cd "$dir"
 
 if [ -n "$JAVA_HOME" ]; then
-  $JAVA_HOME/bin/java -jar ./MediathekView.jar "$@"
+  $JAVA_HOME/bin/java -jar ./@JARNAME@ "$@"
 else
-  java -jar ./MediathekView.jar "$@"
+  java -jar ./@JARNAME@ "$@"
 fi
 cd $OLDPWD
