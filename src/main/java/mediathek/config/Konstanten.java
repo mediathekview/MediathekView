b0d56dbdee8a29fd7341f237a41bf4da05fad0ab
Alex <af@kreativstrecke.de>
2016-12-20 10:31:35 +0100

    Programmgruppen Link abgeändert. Downloadlink geändert.
    Former-commit-id: 0ec554fb39071e9eb6dc3d54fd79de3af3ad4319

--- src/main/java/mediathek/config/Konstanten.java
+++ src/main/java/mediathek/config/Konstanten.java
@@ -34,10 +34,10 @@ public class Konstanten {
     // MediathekView URLs
 //    public static final String ADRESSE_FILMLISTEN_SERVER = "http://zdfmediathk.sourceforge.net/update.xml";
     public static final String ADRESSE_PROGRAMM_VERSION = "https://res.mediathekview.de/prog-info-12.xml";
-    public static final String ADRESSE_DOWNLAD = "http://sourceforge.net/projects/zdfmediathk/";
+    public static final String ADRESSE_DOWNLAD = "https://mediathekview.de/download/";
     public static final String ADRESSE_ANLEITUNG = "https://github.com/mediathekview/MediathekView/wiki";
     public static final String ADRESSE_ONLINE_HELP = "https://sourceforge.net/p/zdfmediathk/wiki/Anleitung/";
-    public static final String ADRESSE_VORLAGE_PROGRAMMGRUPPEN = "http://zdfmediathk.sourceforge.net/programmgruppen11/programmgruppen.xml";
+    public static final String ADRESSE_VORLAGE_PROGRAMMGRUPPEN = "https://res.mediathekview.de/programmgruppen11/programmgruppen.xml";
     public static final String ADRESSE_WEBSITE = "https://mediathekview.de/";
     public static final String ADRESSE_FORUM = "https://forum.mediathekview.de/";
     public static final String ADRESSE_DONATION = "https://mediathekview.de/spenden/";
