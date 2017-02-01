404c9a22ccf5692c54061033aaa5866c1beb4f99
Alex <af@kreativstrecke.de>
2017-02-01 09:09:24 +0100

    Travis nightly build deploy hinzugefügt.

--- scripte/deploy/deploy.sh
+++ scripte/deploy/deploy.sh
@@ -10,17 +10,24 @@ PORT="22"
 ADRESSE="deploy@mediathekview.de"
 KEYFILE="scripte/deploy/deploy.key"
 
-# Deploy zum Nexus Repo
-echo "Deploy zum Nexus Repo repo.mediathekview.de"
-./gradlew release
+if [ "$1" != "nightly" ]; then
+    # Deploy zum Nexus Repo
+    echo "Deploy zum Nexus Repo repo.mediathekview.de"
+    ./gradlew release
+    
+    # Status auf fertig setzen für release
+    echo 1 > $STATUSDATEI
+else
+    # Status auf fertig setzen für nightly
+    echo 2 > $STATUSDATEI
+fi
+
 rm ${LOCAL}/MediathekView-*.tar;
 rm ${LOCAL}/MediathekView-*.tar.SHA-1;
 
 echo "Deploy zu Hauptserver";
 # Rechte am Key nur dem Benutzer geben, ansonsten meckert ssh
 chmod 600 $KEYFILE
-
-echo 1 > $STATUSDATEI
 
 # Ins Verzeichnis wechseln Befehl
 echo "cd $REMOTE" >> $BATCHDATEI
@@ -41,4 +48,5 @@ sftp -b $BATCHDATEI -o PubkeyAuthentication=yes -o IdentityFile=$KEYFILE -o Port
 
 # Aufräumen
 rm $BATCHDATEI $STATUSDATEI
+
 
