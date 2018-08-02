8fd0ab7830c89a2b51656df0eebab87d8bd29dc5
Christian F <crystalpalace1977@googlemail.com>
2018-08-02 22:29:07 +0200

    use weak reference for all film related future ops

--- src/main/java/mSearch/daten/DatenFilm.java
+++ src/main/java/mSearch/daten/DatenFilm.java
@@ -27,6 +27,7 @@ import org.h2.jdbc.JdbcSQLException;
 import org.jetbrains.annotations.NotNull;
 import sun.misc.Cleaner;
 
+import java.lang.ref.WeakReference;
 import java.sql.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -135,12 +136,13 @@ public class DatenFilm implements AutoCloseable, Comparable<DatenFilm> {
      * Future used for writing description into database.
      * Will be checked before each read if finished
      */
-    private CompletableFuture<Void> descriptionFuture;
+    private WeakReference<CompletableFuture<Void>> descriptionFutureReference = null;
+
     /**
      * Future used for writing website link into database.
      * Will be checked before each read if finished
      */
-    private CompletableFuture<Void> websiteFuture;
+    private WeakReference<CompletableFuture<Void>> websiteFutureReference = null;
 
     /**
      * Get the film number.
@@ -239,12 +241,16 @@ public class DatenFilm implements AutoCloseable, Comparable<DatenFilm> {
              PreparedStatement statement = connection.prepareStatement("SELECT desc FROM mediathekview.description WHERE id = ?")) {
             statement.setInt(1, databaseFilmNumber);
 
-            if (descriptionFuture != null) {
-                if (!descriptionFuture.isDone()) {
-                    descriptionFuture.get();
+            if (descriptionFutureReference != null) {
+                final CompletableFuture<Void> descriptionFuture = descriptionFutureReference.get();
+                if (descriptionFuture != null) {
+                    if (!descriptionFuture.isDone()) {
+                        descriptionFuture.get();
+                    }
+                    //set to null when finished as we don´t need it anymore...
+                    descriptionFutureReference.clear();
+                    descriptionFutureReference = null;
                 }
-                //set to null when finished as we don´t need it anymore...
-                descriptionFuture = null;
             }
 
             try (ResultSet rs = statement.executeQuery()) {
@@ -271,8 +277,10 @@ public class DatenFilm implements AutoCloseable, Comparable<DatenFilm> {
         if (desc != null && !desc.isEmpty()) {
             if (MemoryUtils.isLowMemoryEnvironment())
                 writeDescriptionToDatabase(desc);
-            else
-                descriptionFuture = CompletableFuture.runAsync(() -> writeDescriptionToDatabase(desc), PooledDatabaseConnection.getInstance().getDatabaseExecutor());
+            else {
+                final CompletableFuture<Void> descriptionFuture = CompletableFuture.runAsync(() -> writeDescriptionToDatabase(desc), PooledDatabaseConnection.getInstance().getDatabaseExecutor());
+                descriptionFutureReference = new WeakReference<>(descriptionFuture);
+            }
         }
     }
 
@@ -283,14 +291,18 @@ public class DatenFilm implements AutoCloseable, Comparable<DatenFilm> {
              PreparedStatement statement = connection.prepareStatement("SELECT link FROM mediathekview.website_links WHERE id = ?")) {
             statement.setInt(1, databaseFilmNumber);
 
-            if (websiteFuture != null) {
-                if (!websiteFuture.isDone()) {
-                    //make sure async op has finished before...
-                    websiteFuture.get();
-                }
+            if (websiteFutureReference != null) {
+                final CompletableFuture<Void> websiteFuture = websiteFutureReference.get();
+                if (websiteFuture != null) {
+                    if (!websiteFuture.isDone()) {
+                        //make sure async op has finished before...
+                        websiteFuture.get();
+                    }
 
-                //we don´t need it anymore...
-                websiteFuture = null;
+                    //we don´t need it anymore...
+                    websiteFutureReference.clear();
+                    websiteFutureReference = null;
+                }
             }
 
             try (ResultSet rs = statement.executeQuery()) {
@@ -311,8 +323,10 @@ public class DatenFilm implements AutoCloseable, Comparable<DatenFilm> {
         if (link != null && !link.isEmpty()) {
             if (MemoryUtils.isLowMemoryEnvironment())
                 writeWebsiteLinkToDatabase(link);
-            else
-                websiteFuture = CompletableFuture.runAsync(() -> writeWebsiteLinkToDatabase(link), PooledDatabaseConnection.getInstance().getDatabaseExecutor());
+            else {
+                CompletableFuture<Void> websiteFuture = CompletableFuture.runAsync(() -> writeWebsiteLinkToDatabase(link), PooledDatabaseConnection.getInstance().getDatabaseExecutor());
+                websiteFutureReference = new WeakReference<>(websiteFuture);
+            }
         }
     }
 
