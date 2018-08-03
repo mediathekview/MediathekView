af532b4bf4f054aec5f7b25082b38a52b85d165c
Christian F <crystalpalace1977@googlemail.com>
2018-08-03 14:57:14 +0200

    - remove SwingX legacy library from source base, keeping only 4 classes still used for layout management - increase versionto 13.1.2
    Former-commit-id: 67e76bd40e20371fd5d32e9ddb7622828fb05cb7

---
+++ src/main/java/org/jdesktop/swingx/AbstractLayoutManager.java
@@ -0,0 +1,44 @@
+package org.jdesktop.swingx;
+
+import java.awt.*;
+import java.io.Serializable;
+
+/**
+ * A simple abstract class to handle common layout implementations. Package-private as we do NOT
+ * want to export this as part of the public API.
+ *
+ * @author kschaefer
+ */
+abstract class AbstractLayoutManager implements LayoutManager, Serializable {
+    private static final long serialVersionUID = 1446292747820044161L;
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation does nothing.
+     */
+    @Override
+    public void addLayoutComponent(String name, Component comp) {
+        //does nothing
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation does nothing.
+     */
+    @Override
+    public void removeLayoutComponent(Component comp) {
+        // does nothing
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation defers to {@link #preferredLayoutSize(Container)}.
+     */
+    @Override
+    public Dimension minimumLayoutSize(Container parent) {
+        return preferredLayoutSize(parent);
+    }
+}
