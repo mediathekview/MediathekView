2bd6cedeef3d214f3f935ca6b9c559f86f9071b8
Christian F <crystalpalace1977@googlemail.com>
2018-09-16 13:34:23 +0200

    remove zstd compression
    Former-commit-id: 7a4a762a8df8ec00f8fc0869c67d9eca2ac3571a

--- src/main/java/mediathek/config/Const.java
+++ src/main/java/mediathek/config/Const.java
@@ -26,7 +26,6 @@ public class Const {
     // Dateien/Verzeichnisse
     public static final String FORMAT_ZIP = ".zip";
     public static final String FORMAT_XZ = ".xz";
-    public static final String FORMAT_ZSTD = ".zst";
 
     public static final long ALTER_FILMLISTE_SEKUNDEN_FUER_AUTOUPDATE = TimeUnit.SECONDS.convert(3, TimeUnit.HOURS);
     public static final String TIME_MAX_AGE_FOR_DIFF = "09"; // Uhrzeit ab der die Diffliste alle Änderungen abdeckt, die Filmliste darf also nicht vor xx erstellt worden sein
