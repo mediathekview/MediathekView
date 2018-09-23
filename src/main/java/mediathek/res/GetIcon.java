e4da05f0b0ed0e0221d2a1d32bf175f09a02f4dc
Christian F <crystalpalace1977@googlemail.com>
2018-09-23 17:41:19 +0200

    code cleanup

--- src/main/java/mediathek/res/GetIcon.java
+++ src/main/java/mediathek/res/GetIcon.java
@@ -19,12 +19,8 @@
  */
 package mediathek.res;
 
-import java.awt.Image;
-import java.io.File;
-import javax.swing.ImageIcon;
-import mSearch.tool.Log;
-import mediathek.config.MVConfig;
-import mediathek.tool.GuiFunktionen;
+import javax.swing.*;
+import java.awt.*;
 
 public class GetIcon {
 
