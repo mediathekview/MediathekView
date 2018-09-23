61cdb61b72deb4a6138803cf4b3096ef03ae4a76
Christian F <crystalpalace1977@googlemail.com>
2018-09-23 17:27:50 +0200

    - implement anti-throttling strategy for filmlist servers

--- src/main/java/mediathek/config/Konstanten.java
+++ src/main/java/mediathek/config/Konstanten.java
@@ -23,6 +23,7 @@ import mSearch.tool.Version;
 
 public class Konstanten {
     public static final String ROUTER_BASE_ADDRESS = "https://rtr.mdthk.de/";
+    public static final String SERVER_ANTI_THROTTLING_HEADER = "MV-AntiThrottlingID";
 
     public static final Version MVVERSION = new Version(13, 3, 0);
     public static final String PROGRAMMNAME = "MediathekView";
