eeecd4926cdc49ba8b8beedea6d7f31546ebed09
Christian F <crystalpalace1977@googlemail.com>
2018-09-26 15:03:11 +0200

    increase startup performance

--- src/main/java/mSearch/daten/DatenFilm.java
+++ src/main/java/mSearch/daten/DatenFilm.java
@@ -104,10 +104,6 @@ public class DatenFilm implements AutoCloseable, Comparable<DatenFilm> {
         COLUMN_NAMES[FILM_DATUM_LONG] = "DatumL";
     }
 
-    static {
-        Database.initializeDatabase();
-    }
-
     /**
      * The magic arr array.
      * Here all the film information with some minor exceptions.
@@ -510,7 +506,7 @@ public class DatenFilm implements AutoCloseable, Comparable<DatenFilm> {
             logger.debug("Finished creating SQL indices");
         }
 
-        private static void initializeDatabase() {
+        public static void initializeDatabase() {
             logger.debug("initializeDatabase()");
             SqlClosure.sqlExecute(connection -> {
                 Statement statement = connection.createStatement();
