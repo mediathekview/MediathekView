636778856b3b2dd649630b482efca70ef5fbffd9
Christian F <crystalpalace1977@googlemail.com>
2018-09-27 14:32:02 +0200

    add description editing back to description panel
    Former-commit-id: d33feb7cf92680ef7f31c5686f767333f988011e

--- src/main/java/mediathek/javafx/descriptionPanel/DescriptionPanelController.java
+++ src/main/java/mediathek/javafx/descriptionPanel/DescriptionPanelController.java
@@ -3,14 +3,18 @@ package mediathek.javafx.descriptionPanel;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Tab;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import mSearch.daten.DatenFilm;
+import mediathek.MediathekGui;
+import mediathek.config.Daten;
+import mediathek.gui.dialog.DialogFilmBeschreibung;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
 import java.awt.*;
 import java.net.URI;
 import java.util.Optional;
@@ -30,22 +34,27 @@ public class DescriptionPanelController {
 
     private WebEngine webEngine;
     private EventHandler<Event> closeHandler;
+    private DatenFilm currentFilm = null;
+
+    private void setupWebsiteLink() {
+        websiteLink.setOnAction(e -> {
+            try {
+                final Desktop desktop = Desktop.getDesktop();
+                if (desktop.isSupported(Desktop.Action.BROWSE)) {
+                    desktop.browse(new URI(currentFilm.getWebsiteLink()));
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
 
     public void showFilmDescription(@NotNull Optional<DatenFilm> optFilm) {
         optFilm.ifPresentOrElse(film -> {
+            currentFilm = film;
             websiteLink.setVisible(true);
             websiteLink.setVisited(false);
             websiteLink.setTooltip(new Tooltip(film.getWebsiteLink()));
-            websiteLink.setOnAction(e -> {
-                try {
-                    final Desktop desktop = Desktop.getDesktop();
-                    if (desktop.isSupported(Desktop.Action.BROWSE)) {
-                        desktop.browse(new URI(film.getWebsiteLink()));
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            });
 
             webEngine.setUserStyleSheetLocation(getClass().getResource("/mediathek/res/css/description.css").toString());
             webEngine.loadContent(
@@ -58,6 +67,34 @@ public class DescriptionPanelController {
             webEngine.loadContent("");
             websiteLink.setTooltip(null);
             websiteLink.setVisible(false);
+            currentFilm = null;
+        });
+    }
+
+    private void createContextMenu() {
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem edit = new MenuItem("Beschreibung ändern");
+        edit.setOnAction(e -> {
+            SwingUtilities.invokeLater(() -> {
+                DialogFilmBeschreibung dialog = new DialogFilmBeschreibung(MediathekGui.ui(), Daten.getInstance(), currentFilm);
+                dialog.setVisible(true);
+            });
+        });
+
+        contextMenu.getItems().add(edit);
+        contextMenu.setOnShowing(e -> {
+            //if there is no film do not offer edit capability
+            if (currentFilm == null)
+                edit.setDisable(true);
+            else
+                edit.setDisable(false);
+        });
+
+        webView.setOnMousePressed(e -> {
+            if (e.getButton() == MouseButton.SECONDARY)
+                contextMenu.show(webView, e.getScreenX(), e.getScreenY());
+            else
+                contextMenu.hide();
         });
     }
 
@@ -68,7 +105,10 @@ public class DescriptionPanelController {
 
     public void initialize() {
         websiteLink.setVisible(false);
+        setupWebsiteLink();
+
         webView.setContextMenuEnabled(false);
+        createContextMenu();
 
         webEngine = webView.getEngine();
         webEngine.loadContent("<html></html>");
