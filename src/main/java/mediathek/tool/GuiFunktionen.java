c35548733536e86289dd238ad09479fb29a417f5
Christian F <crystalpalace1977@googlemail.com>
2018-12-21 16:42:54 +0100

    initial value of sender selction combo in toolbar
    Former-commit-id: fec8efe2af1db874ef1cc77658b089f8c3cf19a5

--- src/main/java/mediathek/tool/GuiFunktionen.java
+++ src/main/java/mediathek/tool/GuiFunktionen.java
@@ -19,9 +19,6 @@
  */
 package mediathek.tool;
 
-import ca.odell.glazedlists.EventList;
-import ca.odell.glazedlists.TransformedList;
-import ca.odell.glazedlists.event.ListEvent;
 import ca.odell.glazedlists.swing.DefaultEventComboBoxModel;
 import mSearch.daten.ListeFilme;
 import mSearch.tool.Functions.OperatingSystemType;
@@ -396,35 +393,6 @@ public class GuiFunktionen extends MVFunctionSys {
                 enableComponents((Container) component, enable);
             }
         }
-    }
-
-    private static class SenderList extends TransformedList<String, String> {
-
-        public SenderList(EventList<String> source) {
-            super(source);
-        }
-
-        @Override
-        protected boolean isWritable() {
-            return false;
-        }
-
-        @Override
-        public void listChanged(ListEvent<String> listEvent) {
-        }
-
-        @Override
-        public String get(int index) {
-            if (index == 0)
-                return "";
-            else
-                return source.get(index - 1);
-        }
-
-        public int size() {
-            return this.source.size() + 1;
-        }
-
     }
 
 }
