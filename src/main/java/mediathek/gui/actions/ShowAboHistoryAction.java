df176b8a07ac4eecffef06ddd7e9f90bae6dcbc5
Christian F <crystalpalace1977@googlemail.com>
2018-12-21 01:35:14 +0100

    - create abo action classes - code cleanup

---
+++ src/main/java/mediathek/gui/actions/ShowAboHistoryAction.java
@@ -0,0 +1,28 @@
+package mediathek.gui.actions;
+
+import mediathek.config.Daten;
+import mediathek.gui.history.AboHistoryDialog;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class ShowAboHistoryAction extends AbstractAction {
+    private final Frame owner;
+    private final Daten daten;
+
+    public ShowAboHistoryAction(Frame owner, Daten daten) {
+        this.owner = owner;
+        this.daten = daten;
+
+        putValue(Action.NAME, "Erledigte Abos anzeigen...");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        AboHistoryDialog dialog = new AboHistoryDialog(owner, daten);
+        dialog.pack();
+        dialog.setVisible(true);
+    }
+
+}
