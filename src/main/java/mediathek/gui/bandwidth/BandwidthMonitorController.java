d317455fe1ebcd6e3a574ec386c4ccd39b7ef66f
Christian F <crystalpalace1977@googlemail.com>
2019-01-11 13:56:28 +0100

    - use JavaFX 12 EA 7 to fix crash on macOS Mojave - rewrite bandwidth usage info dialog to show more modern content

--- src/main/java/mediathek/gui/bandwidth/BandwidthMonitorController.java
+++ src/main/java/mediathek/gui/bandwidth/BandwidthMonitorController.java
@@ -1,14 +1,15 @@
 package mediathek.gui.bandwidth;
 
+import eu.hansolo.tilesfx.Tile;
+import eu.hansolo.tilesfx.TileBuilder;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
-import javafx.scene.chart.AreaChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Stop;
 import javafx.util.Duration;
 import mediathek.config.Daten;
 import mediathek.config.MVConfig;
@@ -23,7 +24,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 /**
  * This class will manage and display the download bandwidth chart display.
@@ -32,12 +34,10 @@ public class BandwidthMonitorController {
 
     private static final int DEFAULT_WIDTH = 300;
     private static final int DEFAULT_HEIGHT = 150;
-    private static final int TIMELINE_SIZE = 120;
-    private final AtomicInteger time = new AtomicInteger();
     private final ListeDownloads listeDownloads;
-    private XYChart.Series<Number, Number> series;
     private JDialog hudDialog = null;
     private Timeline updateMemoryTimer;
+    private Tile bandwidthTile;
 
     public BandwidthMonitorController(JFrame parent) {
         listeDownloads = Daten.getInstance().getListeDownloads();
@@ -58,7 +58,7 @@ public class BandwidthMonitorController {
         hudDialog.setLayout(new BorderLayout(0, 0));
         final JFXPanel fxPanel = new JFXPanel();
         hudDialog.getContentPane().add(fxPanel, BorderLayout.CENTER);
-        Platform.runLater(() -> fxPanel.setScene(new Scene(createChart())));
+        Platform.runLater(() -> fxPanel.setScene(new Scene(createTile())));
     }
 
     public void close() {
@@ -105,8 +105,8 @@ public class BandwidthMonitorController {
      *
      * @return Used bandwidth in Megabits per second.
      */
-    private int calculateBandwidthUsage() {
-        int bandwidth = 0; // bytes per second
+    private double calculateBandwidthUsage() {
+        double bandwidth = 0d; // bytes per second
         //only count running/active downloads and calc accumulated progress..
         var activeDownloadList = listeDownloads.getListOfStartsNotFinished(DatenDownload.QUELLE_ALLE);
         for (DatenDownload download : activeDownloadList) {
@@ -117,7 +117,7 @@ public class BandwidthMonitorController {
         activeDownloadList.clear();
 
         //convert to MBits per second
-        bandwidth = bandwidth * 8 / 1000 / 1000;
+        bandwidth = bandwidth * 8d / 1000d / 1000d;
 
         return bandwidth;
     }
@@ -125,36 +125,44 @@ public class BandwidthMonitorController {
     private void createUpdateTimer() {
         Platform.runLater(() -> {
             updateMemoryTimer = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-                series.getData().add(new XYChart.Data<>(time.incrementAndGet(), calculateBandwidthUsage()));
-                if (series.getData().size() > TIMELINE_SIZE) {
-                    series.getData().subList(0, series.getData().size() - TIMELINE_SIZE).clear();
-                }
+                bandwidthTile.setValue(calculateBandwidthUsage());
             }));
             updateMemoryTimer.setCycleCount(Animation.INDEFINITE);
         });
     }
 
-    private AreaChart createChart() {
-        series = new XYChart.Series<>();
+    private Tile createTile() {
+        bandwidthTile = TileBuilder.create()
+                .skinType(Tile.SkinType.SPARK_LINE)
+                .prefSize(400, 400)
+                .unit("MBit/s")
+                .minValue(0)
+                .maxValue(2 * 1024)
+                .decimals(0)
+                .tickLabelDecimals(0)
+                .time(ZonedDateTime.now(ZoneId.of("Europe/Berlin")))
+                .gradientStops(new Stop(0, Color.web("#1CAF4D")),
+                        new Stop(0.0075, Color.web("#1CAF4D")),
+                        new Stop(0.00751, Color.web("#91CA40")),
+                        new Stop(0.01166, Color.web("#91CA40")),
+                        new Stop(0.01167, Color.web("#F8C610")),
+                        new Stop(0.01666, Color.web("#F8C610")),
+                        new Stop(0.01667, Color.web("#F29222")),
+                        new Stop(0.025, Color.web("#F29222")),
+                        new Stop(0.02501, Color.web("#EC1D24")),
+                        new Stop(1.0, Color.web("#EC1D24")))
+                .strokeWithGradient(true)
+                .averagingPeriod(96)
+                .averageVisible(true)
+                .averagingEnabled(true)
+                .smoothing(true)
+                .build();
 
-        NumberAxis xAxis = new NumberAxis();
-        xAxis.setForceZeroInRange(false);
-        xAxis.setTickUnit(5d);
-        xAxis.setTickLabelsVisible(false);
-
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setForceZeroInRange(false);
-        yAxis.setLabel("MBit/s");
-
-        AreaChart<Number, Number> areaChart = new AreaChart<>(xAxis, yAxis);
-        areaChart.createSymbolsProperty().setValue(false);
-        areaChart.setLegendVisible(false);
-        areaChart.setAnimated(false);
-        areaChart.getData().add(series);
+        bandwidthTile.setValue(0d);
 
         createUpdateTimer();
 
-        return areaChart;
+        return bandwidthTile;
     }
 
     private void updateListeners() {
