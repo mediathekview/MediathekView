c18efedbd881610873033be3094f243198248478
Christian F <crystalpalace1977@googlemail.com>
2019-01-13 20:03:25 +0100

    Convert film description panel from web view to TextFlow.
    Former-commit-id: def98e7d0bad425e20e03e2962f6c469096a9a18

--- src/main/java/mediathek/javafx/descriptionPanel/DescriptionPanelController.java
+++ src/main/java/mediathek/javafx/descriptionPanel/DescriptionPanelController.java
@@ -3,11 +3,12 @@ package mediathek.javafx.descriptionPanel;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseButton;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.*;
 import mSearch.daten.DatenFilm;
 import mediathek.MediathekGui;
 import mediathek.gui.dialog.DialogFilmBeschreibung;
@@ -24,16 +25,17 @@ import java.util.Optional;
 public class DescriptionPanelController {
     @FXML
     private Hyperlink websiteLink;
-
     @FXML
-    private WebView webView;
-
+    private TextFlow textField;
     @FXML
     private Tab descriptionTab;
 
-    private WebEngine webEngine;
+    @FXML
+    private ScrollPane scrollPane;
+
     private EventHandler<Event> closeHandler;
     private DatenFilm currentFilm = null;
+    private ContextMenu contextMenu;
 
     private void setupWebsiteLink() {
         websiteLink.setOnAction(e -> {
@@ -49,36 +51,40 @@ public class DescriptionPanelController {
     }
 
     public void showFilmDescription(@NotNull Optional<DatenFilm> optFilm) {
+        textField.getChildren().clear();
+
         optFilm.ifPresentOrElse(film -> {
             currentFilm = film;
             websiteLink.setVisible(true);
             websiteLink.setVisited(false);
             websiteLink.setTooltip(new Tooltip(film.getWebsiteLink()));
 
-            webEngine.setUserStyleSheetLocation(getClass().getResource("/mediathek/res/css/description.css").toString());
-            webEngine.loadContent(
-                    "<html>"
-                            + "<span class=\'headline\'>" + (film.getSender().isEmpty() ? "" : film.getSender() + "  -  ")
-                            + film.getTitle() + "</span><br/><br/>"
-                            + "<span class=\'description\'>" + film.getDescription() + "</span>"
-                            + "</html>");
+            Font defaultFont = Font.getDefault();
+            Text headLine = new Text((film.getSender().isEmpty() ? "" : film.getSender() + "  -  ") + film.getTitle());
+            headLine.setFont(Font.font(defaultFont.getName(), FontWeight.BOLD, FontPosture.REGULAR, defaultFont.getSize()));
+            headLine.setFill(Color.RED);
+
+            Text description = new Text(film.getDescription());
+            description.setFont(Font.font(defaultFont.getName(), FontWeight.NORMAL, FontPosture.REGULAR, defaultFont.getSize()));
+
+            textField.getChildren().addAll(headLine,
+                    new Text("\n"),
+                    new Text("\n"),
+                    description);
         }, () -> {
-            webEngine.loadContent("");
             websiteLink.setTooltip(null);
             websiteLink.setVisible(false);
             currentFilm = null;
         });
     }
 
-    private void createContextMenu() {
+    private ContextMenu createContextMenu() {
         ContextMenu contextMenu = new ContextMenu();
         MenuItem edit = new MenuItem("Beschreibung ändern");
-        edit.setOnAction(e -> {
-            SwingUtilities.invokeLater(() -> {
-                DialogFilmBeschreibung dialog = new DialogFilmBeschreibung(MediathekGui.ui(), currentFilm);
-                dialog.setVisible(true);
-            });
-        });
+        edit.setOnAction(e -> SwingUtilities.invokeLater(() -> {
+            DialogFilmBeschreibung dialog = new DialogFilmBeschreibung(MediathekGui.ui(), currentFilm);
+            dialog.setVisible(true);
+        }));
 
         contextMenu.getItems().add(edit);
         contextMenu.setOnShowing(e -> {
@@ -89,12 +95,7 @@ public class DescriptionPanelController {
                 edit.setDisable(false);
         });
 
-        webView.setOnMousePressed(e -> {
-            if (e.getButton() == MouseButton.SECONDARY)
-                contextMenu.show(webView, e.getScreenX(), e.getScreenY());
-            else
-                contextMenu.hide();
-        });
+        return contextMenu;
     }
 
     public void setOnCloseRequest(EventHandler<Event> e) {
@@ -106,10 +107,7 @@ public class DescriptionPanelController {
         websiteLink.setVisible(false);
         setupWebsiteLink();
 
-        webView.setContextMenuEnabled(false);
-        createContextMenu();
-
-        webEngine = webView.getEngine();
-        webEngine.loadContent("<html></html>");
+        contextMenu = createContextMenu();
+        scrollPane.setOnContextMenuRequested(event -> contextMenu.show(textField, event.getScreenX(), event.getScreenY()));
     }
 }
