56dff60259d8753703e7a4c93855bdc4dce82d3f
Christian F <crystalpalace1977@googlemail.com>
2019-03-21 22:34:38 +0100

    add initial thrift notification
    Former-commit-id: f98f6fc5a7dfad57e36774281b7ecba9843b03e1

--- src/main/java/mediathek/tool/notification/NotificationMessage.java
+++ src/main/java/mediathek/tool/notification/NotificationMessage.java
@@ -1,5 +1,7 @@
 package mediathek.tool.notification;
 
+import mediathek.tool.notification.thrift.MessageType;
+
 public class NotificationMessage {
     public String message;
     public String title;
