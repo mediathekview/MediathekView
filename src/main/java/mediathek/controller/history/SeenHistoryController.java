640222bc87bd0374db726e03771122233812ab20
Christian F <crystalpalace1977@googlemail.com>
2019-04-03 22:13:20 +0200

    - add more logging to history controller - due to a f**cking bug in java disable native notifications until further notice
    Former-commit-id: a1b791528a9b390cf4b56c118d83eeb2f80bcaaf

--- src/main/java/mediathek/controller/history/SeenHistoryController.java
+++ src/main/java/mediathek/controller/history/SeenHistoryController.java
@@ -3,15 +3,32 @@ package mediathek.controller.history;
 import mSearch.daten.DatenFilm;
 import mediathek.config.Daten;
 import mediathek.gui.messages.history.DownloadHistoryChangedEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 
 public class SeenHistoryController extends MVUsedUrls<DownloadHistoryChangedEvent> {
+    private static final Logger logger = LogManager.getLogger(SeenHistoryController.class);
+
     public SeenHistoryController() {
         super("history.txt", Daten.getSettingsDirectory_String(), DownloadHistoryChangedEvent.class);
     }
 
+    @Override
+    public synchronized void zeileSchreiben(String thema, String titel, String url) {
+        logger.trace("zeileSchreiben(thema: {}, titel: {}, url: {})", thema, titel, url);
+        super.zeileSchreiben(thema, titel, url);
+    }
+
+    @Override
+    public synchronized void zeileSchreiben(ArrayList<DatenFilm> arrayFilms) {
+        logger.trace("zeileSchreiben(thema: {})", arrayFilms);
+        super.zeileSchreiben(arrayFilms);
+    }
+
     public synchronized void setGesehen(boolean gesehen, ArrayList<DatenFilm> arrayFilms) {
+        logger.trace("setGesehen({})", gesehen);
         if (arrayFilms.isEmpty()) {
             return;
         }
