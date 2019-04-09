ae459dd11d2dea311a05eeec16270d8cafe51b6f
Christian F <crystalpalace1977@googlemail.com>
2019-04-09 20:14:46 +0200

    - add automatic notification service discovery for macOS - final implementation for native download notifications
    Former-commit-id: 2b01a4eeadfd4a31058aee2bd415f8c55312aa84

--- src/main/java/mediathek/tool/notification/NativeNotificationCenter.java
+++ src/main/java/mediathek/tool/notification/NativeNotificationCenter.java
@@ -5,19 +5,49 @@ import mSearch.tool.javafx.FXErrorDialog;
 import mediathek.config.Konstanten;
 import mediathek.tool.notification.thrift.NotificationMessage;
 import mediathek.tool.notification.thrift.ThriftNotificationCenter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
-public class NativeNotificationCenter implements INotificationCenter {
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceListener;
+import java.net.InetAddress;
+
+public class NativeNotificationCenter implements INotificationCenter, ServiceListener {
+    private int serverPort;
+    private InetAddress serverAddress;
+    private static final Logger logger = LogManager.getLogger(NativeNotificationCenter.class);
+
+    public NativeNotificationCenter() {
+        setupJmdnsListener();
+    }
+
+    private void setupJmdnsListener() {
+        try {
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+            jmdns.addServiceListener("_mv-notification._tcp.local.", this);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     @Override
     public void displayNotification(NotificationMessage msg) {
-        if (!ApplicationConfiguration.getConfiguration().getBoolean(ApplicationConfiguration.APPLICATION_SHOW_NOTIFICATIONS,true))
+        if (!ApplicationConfiguration.getConfiguration().getBoolean(ApplicationConfiguration.APPLICATION_SHOW_NOTIFICATIONS, true))
             return;
 
-        try (TTransport transport = new TSocket("localhost", 9090)){
+        if (serverPort == -1 || serverAddress == null) {
+            showErrorDialog(new IllegalArgumentException("server port or server address not set"));
+            return;
+        }
+
+        logger.trace("Sending native notification to {} on serverPort {}", serverAddress, serverPort);
+        try (TTransport transport = new TSocket(serverAddress.getHostAddress(), serverPort)) {
             transport.open();
 
             TProtocol protocol = new TBinaryProtocol(transport);
@@ -25,8 +55,32 @@ public class NativeNotificationCenter implements INotificationCenter {
             ThriftNotificationCenter.Client client = new ThriftNotificationCenter.Client(protocol);
             client.displayNotification(msg);
         } catch (TException e) {
-            FXErrorDialog.showErrorDialog(Konstanten.PROGRAMMNAME,"Native Benachrichtigungen können nicht angezeigt werden",
-                    "Bitte stellen Sie sicher das das Hilfsprogramm gestartet ist.", e);
+            showErrorDialog(e);
         }
+    }
+
+    private void showErrorDialog(Exception ex) {
+        FXErrorDialog.showErrorDialog(Konstanten.PROGRAMMNAME, "Native Benachrichtigungen können nicht angezeigt werden",
+                "Bitte stellen Sie sicher das das Hilfsprogramm gestartet ist.", ex);
+    }
+
+    @Override
+    public void serviceAdded(ServiceEvent serviceEvent) {
+        var info = serviceEvent.getInfo();
+        serverAddress = info.getInetAddress();
+        serverPort = info.getPort();
+    }
+
+    @Override
+    public void serviceRemoved(ServiceEvent serviceEvent) {
+        serverPort = -1;
+        serverAddress = null;
+    }
+
+    @Override
+    public void serviceResolved(ServiceEvent serviceEvent) {
+        var info = serviceEvent.getInfo();
+        serverAddress = info.getInetAddress();
+        serverPort = info.getPort();
     }
 }
