2b01a4eeadfd4a31058aee2bd415f8c55312aa84
Christian F <crystalpalace1977@googlemail.com>
2019-04-09 20:14:46 +0200

    - add automatic notification service discovery for macOS - final implementation for native download notifications

--- src/main/java/mediathek/tool/notification/NotificationFactory.java
+++ src/main/java/mediathek/tool/notification/NotificationFactory.java
@@ -1,13 +1,13 @@
 package mediathek.tool.notification;
 
 import mSearch.tool.ApplicationConfiguration;
+import org.apache.commons.lang3.SystemUtils;
 
 public class NotificationFactory {
     private final static boolean bNativeNotifications;
 
     static {
-        //TODO disabled until Java InetAddress.getLocalHost() fix is available :-(
-        bNativeNotifications = false;
+        bNativeNotifications = SystemUtils.IS_OS_MAC_OSX;
     }
 
     /**
@@ -22,7 +22,8 @@ public class NotificationFactory {
     public static INotificationCenter createNotificationCenter() {
         INotificationCenter notificationCenter;
 
-        if (ApplicationConfiguration.getConfiguration().getBoolean(ApplicationConfiguration.APPLICATION_SHOW_NATIVE_NOTIFICATIONS, false))
+        if (ApplicationConfiguration.getConfiguration().getBoolean(ApplicationConfiguration.APPLICATION_SHOW_NATIVE_NOTIFICATIONS, false)
+        && hasNativeNotifications())
             notificationCenter = new NativeNotificationCenter();
         else
             notificationCenter = new GenericNotificationCenter();
