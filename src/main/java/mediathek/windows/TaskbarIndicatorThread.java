10d2f7ff671525a015d39ebc0dc6ae9849393e8e
Christian F <crystalpalace1977@googlemail.com>
2019-04-22 22:41:35 +0200

    - prevent system standby on windows during downloads
    Former-commit-id: ce42a40a64fdf868d8e0e23c934e86daa9aaf34c

--- src/main/java/mediathek/windows/TaskbarIndicatorThread.java
+++ src/main/java/mediathek/windows/TaskbarIndicatorThread.java
@@ -1,6 +1,10 @@
 package mediathek.windows;
 
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.WinBase;
 import mediathek.tool.threads.IndicatorThread;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,6 +13,8 @@ import java.util.concurrent.TimeUnit;
 public class TaskbarIndicatorThread extends IndicatorThread {
     private final Taskbar taskbar;
     private final JFrame parent;
+    private static final Logger logger = LogManager.getLogger(TaskbarIndicatorThread.class);
+
 
     public TaskbarIndicatorThread(MediathekGuiWindows parent) {
         super();
@@ -18,8 +24,22 @@ public class TaskbarIndicatorThread extends IndicatorThread {
 
     }
 
+    private void disableStandby() {
+        int success = Kernel32.INSTANCE.SetThreadExecutionState(WinBase.ES_CONTINUOUS | WinBase.ES_SYSTEM_REQUIRED);
+        if (success == 0)
+            logger.error("disableStandby() failed!");
+    }
+
+    private void enableStandby() {
+        int success = Kernel32.INSTANCE.SetThreadExecutionState(WinBase.ES_CONTINUOUS);
+        if (success == 0)
+            logger.error("enableStandby() failed!");
+    }
+
     @Override
     public void run() {
+        disableStandby();
+
         try {
             while (!isInterrupted()) {
                 final int percentage = (int) calculateOverallPercentage();
@@ -32,6 +52,7 @@ public class TaskbarIndicatorThread extends IndicatorThread {
         } finally {
             //when we are finished, stop progress
             taskbar.setWindowProgressState(parent, Taskbar.State.OFF);
+            enableStandby();
         }
     }
 }
