3ab6146500519802386733e34a74857440a784d1
Christian F <crystalpalace1977@googlemail.com>
2019-05-21 17:48:02 +0200

    - optimize date handling and reduce allocation of SimpleDateFormat objects
    Former-commit-id: 96cb1d17073cd4f3e7ea0882040835a156ec837a

--- src/main/java/mSearch/tool/DatumFilm.java
+++ src/main/java/mSearch/tool/DatumFilm.java
@@ -19,36 +19,26 @@
  */
 package mSearch.tool;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.apache.commons.lang3.time.FastDateFormat;
+
 import java.util.TimeZone;
 
 @SuppressWarnings("serial")
 public class DatumFilm extends Datum {
     // die Filme werden immer in der Zeitzone "Europe/Berlin" gesucht
-
-    private final static SDF dateFormatter1 = new SDF("dd.MM.yyyy");
-    private final static SDF dateFormatter2 = new SDF("yyyy.MM.dd");
+    protected static final FastDateFormat dateFormatter1 = FastDateFormat.getInstance("dd.MM.yyyy", TimeZone.getTimeZone("Europe/Berlin"));
 
     public DatumFilm(long l) {
         super(l);
+
     }
 
     @Override
     public String toString() {
-        if (this.getTime() == 0) {
+        if (getTime() == 0) {
             return "";
         } else {
             return dateFormatter1.format(this);
-        }
-    }
-
-    private static class SDF extends SimpleDateFormat {
-        private final static TimeZone tz = TimeZone.getTimeZone("Europe/Berlin");
-
-        SDF(String str) {
-            super(str);
-            this.setTimeZone(tz);
         }
     }
 }
