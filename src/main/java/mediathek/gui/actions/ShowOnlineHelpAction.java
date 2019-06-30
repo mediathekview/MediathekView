0de9b107f034b83a6261ab270e537b1e03a42013
Christian F <crystalpalace1977@googlemail.com>
2019-06-30 08:51:31 +0200

    - rework browser opening due to bugs on macOS not opening correctly the default browser
    Former-commit-id: 83a8928ca1310c899f747f49854063af41c0be19

--- src/main/java/mediathek/gui/actions/ShowOnlineHelpAction.java
+++ src/main/java/mediathek/gui/actions/ShowOnlineHelpAction.java
@@ -3,12 +3,12 @@ package mediathek.gui.actions;
 import jiconfont.icons.FontAwesome;
 import jiconfont.swing.IconFontSwing;
 import mSearch.tool.javafx.FXErrorDialog;
+import mediathek.MediathekGui;
 import mediathek.config.Konstanten;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.net.URI;
+import java.net.URISyntaxException;
 
 public class ShowOnlineHelpAction extends AbstractAction {
     public ShowOnlineHelpAction() {
@@ -19,19 +19,13 @@ public class ShowOnlineHelpAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (Desktop.isDesktopSupported()) {
-            Desktop d = Desktop.getDesktop();
-            try {
-                if (d.isSupported(Desktop.Action.BROWSE)) {
-                    d.browse(new URI(Konstanten.ADRESSE_ONLINE_HELP));
-                }
-            } catch (Exception ex) {
-                FXErrorDialog.showErrorDialog("Online-Hilfe",
-                        "Fehler beim Öffnen der Online-Hilfe",
-                        "Es trat ein Fehler beim Öffnen der Online-Hilfe auf.\nSollte dies häufiger auftreten kontaktieren Sie bitte das Entwicklerteam.",
-                        ex);
-            }
+        try {
+            UrlHyperlinkAction.openURL(MediathekGui.ui(),Konstanten.ADRESSE_ONLINE_HELP);
+        } catch (URISyntaxException ex) {
+            FXErrorDialog.showErrorDialog("Online-Hilfe",
+                    "Fehler beim Öffnen der Online-Hilfe",
+                    "Es trat ein Fehler beim Öffnen der Online-Hilfe auf.\nSollte dies häufiger auftreten kontaktieren Sie bitte das Entwicklerteam.",
+                    ex);
         }
-
     }
 }
