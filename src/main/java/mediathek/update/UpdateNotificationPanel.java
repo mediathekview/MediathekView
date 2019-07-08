fd21b9e52ba573c84be45834dc678a27df6bb155
Christian F <crystalpalace1977@googlemail.com>
2019-07-08 20:45:09 +0200

    - update UI dialog code - fix missing icon bug in update dialog due to incorrect path

--- src/main/java/mediathek/update/UpdateNotificationPanel.java
+++ src/main/java/mediathek/update/UpdateNotificationPanel.java
@@ -1,6 +1,9 @@
 package mediathek.update;
 
 import javafx.embed.swing.JFXPanel;
+import net.miginfocom.layout.AC;
+import net.miginfocom.layout.CC;
+import net.miginfocom.layout.LC;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
@@ -22,46 +25,48 @@ public class UpdateNotificationPanel extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner non-commercial license
-        JLabel label1 = new JLabel();
-        JLabel lblAppIcon = new JLabel();
+        var label1 = new JLabel();
+        var lblAppIcon = new JLabel();
         lblReleaseInfo = new JLabel();
-        JLabel label4 = new JLabel();
+        var label4 = new JLabel();
         fxPanel = new JFXPanel();
 
         //======== this ========
         setLayout(new MigLayout(
-                "hidemode 3",
-                // columns
-                "[fill]" +
-                        "[grow,fill]",
-                // rows
-                "[]" +
-                        "[]" +
-                        "[]" +
-                        "[grow]"));
+            new LC().hideMode(3),
+            // columns
+            new AC()
+                .fill().gap()
+                .grow().fill(),
+            // rows
+            new AC()
+                .gap()
+                .gap()
+                .gap()
+                .grow()));
 
         //---- label1 ----
-        label1.setText("Eine neue Version von MediathekView ist verf\u00fcgbar!");
+        label1.setText("Eine neue Version von MediathekView ist verf\u00fcgbar!"); //NON-NLS
         label1.setFont(label1.getFont().deriveFont(label1.getFont().getStyle() | Font.BOLD, label1.getFont().getSize() + 5f));
-        add(label1, "cell 1 0,aligny bottom,growy 0");
+        add(label1, new CC().cell(1, 0).alignY("bottom").growY(0)); //NON-NLS
 
         //---- lblAppIcon ----
-        lblAppIcon.setIcon(new ImageIcon("/Users/christianfranzke/development/IntelliJ Projekte/MediathekView/src/main/resources/mediathek/res/MediathekView_k.png"));
-        add(lblAppIcon, "cell 0 0 1 2");
+        lblAppIcon.setIcon(new ImageIcon(getClass().getResource("/mediathek/res/MediathekView_k.png"))); //NON-NLS
+        add(lblAppIcon, new CC().cell(0, 0, 1, 2));
 
         //---- lblReleaseInfo ----
-        lblReleaseInfo.setText("text");
+        lblReleaseInfo.setText("text"); //NON-NLS
         lblReleaseInfo.setFont(lblReleaseInfo.getFont().deriveFont(lblReleaseInfo.getFont().getSize() - 1f));
-        add(lblReleaseInfo, "cell 1 1,aligny top,growy 0");
+        add(lblReleaseInfo, new CC().cell(1, 1).alignY("top").growY(0)); //NON-NLS
 
         //---- label4 ----
-        label4.setText("Release Notes:");
+        label4.setText("Release Notes:"); //NON-NLS
         label4.setFont(label4.getFont().deriveFont(label4.getFont().getStyle() | Font.BOLD, label4.getFont().getSize() - 2f));
-        add(label4, "cell 1 2");
+        add(label4, new CC().cell(1, 2));
 
         //---- fxPanel ----
         fxPanel.setPreferredSize(new Dimension(480, 240));
-        add(fxPanel, "cell 1 3,grow");
+        add(fxPanel, new CC().cell(1, 3).grow());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
