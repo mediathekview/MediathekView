782fc26fcb473d028ea751474bd4b1bbc23b046e
Christian F <crystalpalace1977@googlemail.com>
2019-07-11 18:26:55 +0200

    - remove unused classes - remove copyright info with outdated info - we will still remain GPL

--- src/main/java/mSearch/tool/InputStreamProgressMonitor.java
+++ src/main/java/mSearch/tool/InputStreamProgressMonitor.java
@@ -1,10 +1,5 @@
 package mSearch.tool;
 
-/**
- * User: Christian F.
- * Date: 15.06.16
- * Time: 14:05
- */
 public interface InputStreamProgressMonitor {
     void progress(long bytesRead, long size);
 }
