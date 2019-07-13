0e36054861ed8fb600c4298ce7e0ee2da9d2a1fc
Christian F <crystalpalace1977@googlemail.com>
2019-07-13 10:06:31 +0200

    - cleanup logging - display protocol file location on success

--- src/main/java/mediathek/tool/Functions.java
+++ src/main/java/mediathek/tool/Functions.java
@@ -1,21 +1,9 @@
 package mediathek.tool;
 
-import mediathek.config.Konstanten;
 import org.apache.commons.lang3.ArchUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.File;
-import java.security.CodeSource;
-import java.util.ResourceBundle;
 
 public class Functions {
-
-    private static final String RBVERSION = "version";
-    private static final Logger logger = LogManager.getLogger(Functions.class);
-
     public static String textLaenge(int max, String text, boolean mitte, boolean addVorne) {
         if (text.length() > max) {
             if (mitte) {
@@ -61,33 +49,6 @@ public class Functions {
         return getOs().toString();
     }
 
-    public static String getPathJar() {
-        //FIXME get rid of version.properties
-        // liefert den Pfad der Programmdatei mit File.separator am Schluss
-        String pFilePath = "version.properties";
-        File propFile = new File(pFilePath);
-        if (!propFile.exists()) {
-            try {
-                CodeSource cS = Konstanten.class.getProtectionDomain().getCodeSource();
-                File jarFile = new File(cS.getLocation().toURI().getPath());
-                String jarDir = jarFile.getParentFile().getPath();
-                propFile = new File(jarDir + File.separator + pFilePath);
-            } catch (Exception ignored) {
-            }
-        } else {
-            logger.debug("getPathJar() propFile does exist");
-        }
-        String s = StringUtils.replace(propFile.getAbsolutePath(), pFilePath, "");
-        if (!s.endsWith(File.separator)) {
-            s += File.separator;
-        }
-        if (s.endsWith("/lib/")) {
-            // dann sind wir in der msearch-lib
-            s = StringUtils.replace(s, "/lib/", "");
-        }
-        return s;
-    }
-
     public static String[] getJavaVersion() {
         String[] ret = new String[4];
         ret[0] = "Vendor: " + System.getProperty("java.vendor");
@@ -95,36 +56,6 @@ public class Functions {
         ret[2] = "Version: " + System.getProperty("java.version");
         ret[3] = "Runtimeversion: " + System.getProperty("java.runtime.version");
         return ret;
-    }
-
-    public static String getCompileDate() {
-        String propToken = "DATE";
-        String msg = "";
-        try {
-            ResourceBundle.clearCache();
-            ResourceBundle rb = ResourceBundle.getBundle(RBVERSION);
-            if (rb.containsKey(propToken)) {
-                msg = rb.getString(propToken);
-            }
-        } catch (Exception e) {
-            Log.errorLog(807293847, e);
-        }
-        return msg;
-    }
-
-    @Deprecated
-    public static String getBuildNr() {
-        String TOKEN_VERSION = "VERSION";
-        try {
-            ResourceBundle.clearCache();
-            ResourceBundle rb = ResourceBundle.getBundle(RBVERSION);
-            if (rb.containsKey(TOKEN_VERSION)) {
-                return new Version(rb.getString(TOKEN_VERSION)).toString();
-            }
-        } catch (Exception e) {
-            Log.errorLog(134679898, e);
-        }
-        return new Version("").toString();
     }
 
     public enum OperatingSystemType {
--- src/main/java/mediathek/tool/Logfile.java
+++ src/main/java/mediathek/tool/Logfile.java
@@ -19,55 +19,11 @@ public class Logfile {
         try (OutputStream os = Files.newOutputStream(logFilePath);
              OutputStreamWriter osw = new OutputStreamWriter(os);
              BufferedWriter bw = new BufferedWriter(osw)) {
-            bw.write("");
-            bw.newLine();
-
-            //  ___  ___         _ _       _   _          _    _   _ _               
-            //  |  \/  |        | (_)     | | | |        | |  | | | (_)              
-            //  | .  . | ___  __| |_  __ _| |_| |__   ___| | _| | | |_  _____      __
-            //  | |\/| |/ _ \/ _` | |/ _` | __| '_ \ / _ \ |/ / | | | |/ _ \ \ /\ / /
-            //  | |  | |  __/ (_| | | (_| | |_| | | |  __/   <\ \_/ / |  __/\ V  V / 
-            //  \_|  |_/\___|\__,_|_|\__,_|\__|_| |_|\___|_|\_\\___/|_|\___| \_/\_/  
-            bw.write("___  ___         _ _       _   _          _    _   _ _               ");
-            bw.newLine();
-            bw.write("|  \\/  |        | (_)     | | | |        | |  | | | (_)              ");
-            bw.newLine();
-            bw.write("| .  . | ___  __| |_  __ _| |_| |__   ___| | _| | | |_  _____      __");
-            bw.newLine();
-            bw.write("| |\\/| |/ _ \\/ _` | |/ _` | __| '_ \\ / _ \\ |/ / | | | |/ _ \\ \\ /\\ / /");
-            bw.newLine();
-            bw.write("| |  | |  __/ (_| | | (_| | |_| | | |  __/   <\\ \\_/ / |  __/\\ V  V / ");
-            bw.newLine();
-            bw.write("\\_|  |_/\\___|\\__,_|_|\\__,_|\\__|_| |_|\\___|_|\\_\\\\___/|_|\\___| \\_/\\_/  ");
-            bw.newLine();
-            bw.write("");
-            bw.newLine();
-            bw.write("");
-            bw.newLine();
-
-            bw.write("#####################################################");
-            bw.newLine();
             bw.write("Erstellt: " + new SimpleDateFormat("dd.MM.yyyy, HH:mm").format(new Date()));
             bw.newLine();
             bw.write("#####################################################");
             bw.newLine();
-            bw.newLine();
             bw.write(progVersion);
-            bw.newLine();
-            bw.write("Compiled: " + Functions.getCompileDate());
-            bw.newLine();
-            bw.newLine();
-            bw.write("=====================================================");
-            bw.newLine();
-            bw.write("Java");
-            bw.newLine();
-            String[] java = Functions.getJavaVersion();
-            for (String ja : java) {
-                bw.write(ja);
-                bw.newLine();
-            }
-            bw.newLine();
-            bw.write("=====================================================");
             bw.newLine();
             bw.write("Betriebssystem: " + System.getProperty("os.name"));
             bw.newLine();
@@ -78,26 +34,12 @@ public class Logfile {
             bw.newLine();
             bw.write("=====================================================");
             bw.newLine();
-            bw.write("Programmpfad: " + Functions.getPathJar());
+            bw.write("Programmpfad: " + MVFunctionSys.getPathJar());
             bw.newLine();
             bw.write("Verzeichnis Einstellungen: " + settingsDir);
             bw.newLine();
-            bw.newLine();
             bw.write("=====================================================");
             bw.newLine();
-            long totalMem = Runtime.getRuntime().totalMemory();
-            bw.write("totalMemory: " + totalMem / (1000L * 1000L) + " MB");
-            bw.newLine();
-            long maxMem = Runtime.getRuntime().maxMemory();
-            bw.write("maxMemory: " + maxMem / (1000L * 1000L) + " MB");
-            bw.newLine();
-            long freeMem = Runtime.getRuntime().freeMemory();
-            bw.write("freeMemory: " + freeMem / (1000L * 1000L) + " MB");
-            bw.newLine();
-            bw.newLine();
-            bw.newLine();
-
-            //
             bw.write("#####################################################");
             bw.newLine();
             bw.write("## Programmeinstellungen ##########################");
