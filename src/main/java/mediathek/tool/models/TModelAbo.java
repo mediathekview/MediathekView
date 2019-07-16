0695ffad29136df341275288da87be3c2b0e6b4d
Christian F <crystalpalace1977@googlemail.com>
2019-07-16 06:37:12 +0200

    - start cleanup of abo model
    Former-commit-id: 7fdead08b3f4494696e809bbe6fcbc07ae8019ef

--- src/main/java/mediathek/tool/models/TModelAbo.java
+++ src/main/java/mediathek/tool/models/TModelAbo.java
@@ -28,4 +28,68 @@ public class TModelAbo extends TModel {
         }
         return result;
     }
+
+    @Override
+    public String getColumnName(int column) {
+        String result;
+
+        switch (column) {
+            case DatenAbo.ABO_NR:
+                result = "Nr";
+                break;
+
+            case DatenAbo.ABO_EINGESCHALTET:
+                result = "aktiv";
+                break;
+
+            case DatenAbo.ABO_NAME:
+                result = "Name";
+                break;
+
+            case DatenAbo.ABO_SENDER:
+                result = "Sender";
+                break;
+
+            case DatenAbo.ABO_THEMA:
+                result = "Thema";
+                break;
+
+            case DatenAbo.ABO_TITEL:
+                result = "Titel";
+                break;
+
+            case DatenAbo.ABO_THEMA_TITEL:
+                result = "Thema-Titel";
+                break;
+
+            case DatenAbo.ABO_IRGENDWO:
+                result = "Irgendwo";
+                break;
+
+            case DatenAbo.ABO_MINDESTDAUER:
+                result = "Dauer";
+                break;
+
+            case DatenAbo.ABO_MIN:
+                result = "min/max";
+                break;
+
+            case DatenAbo.ABO_ZIELPFAD:
+                result = "Zielpfad";
+                break;
+
+            case DatenAbo.ABO_DOWN_DATUM:
+                result = "letztes Abo";
+                break;
+
+            case DatenAbo.ABO_PSET:
+                result = "Programmset";
+                break;
+
+            default:
+                throw new IndexOutOfBoundsException("UNKNOWN COLUMN NAME: " + column);
+        }
+
+        return result;
+    }
 }
