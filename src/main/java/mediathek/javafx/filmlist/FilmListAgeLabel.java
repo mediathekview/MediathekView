ad1aae347fb68f6b04f6b948755fc4c7bfa836c1
Christian F <crystalpalace1977@googlemail.com>
2019-08-18 00:22:42 +0200

    - code cleanup

--- src/main/java/mediathek/javafx/filmlist/FilmListAgeLabel.java
+++ src/main/java/mediathek/javafx/filmlist/FilmListAgeLabel.java
@@ -1,14 +1,18 @@
 package mediathek.javafx.filmlist;
 
+import javafx.application.Platform;
 import mediathek.config.Daten;
 import mediathek.daten.ListeFilme;
+import mediathek.gui.messages.TimerEvent;
 import mediathek.javafx.tool.ComputedLabel;
+import net.engio.mbassy.listener.Handler;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 
 import java.time.Duration;
 
 /**
  * Label which will compute the age of the filmlist when updated.
+ * Update cycle one second.
  */
 class FilmListAgeLabel extends ComputedLabel {
     private final Daten daten;
@@ -16,15 +20,22 @@ class FilmListAgeLabel extends ComputedLabel {
     FilmListAgeLabel(Daten daten) {
         super();
         this.daten = daten;
+
+        daten.getMessageBus().subscribe(this);
     }
 
-    public void setAgeToLabel() {
+    private void setAgeToLabel() {
       final ListeFilme listeFilme = daten.getListeFilme();
 
       setComputedText(computeAge(listeFilme.getAge()));
     }
 
-    public String computeAge(long seconds) {
+    @Handler
+    private void handleTimerEvent(TimerEvent e) {
+        Platform.runLater(this::setAgeToLabel);
+    }
+
+    private String computeAge(long seconds) {
       Duration duration = Duration.ofSeconds(seconds);
       return String.format("Alter: %s", DurationFormatUtils.formatDuration(duration.toMillis(), "HH:mm:ss"));
     }
