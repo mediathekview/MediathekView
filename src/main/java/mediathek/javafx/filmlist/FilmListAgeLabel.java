a0ad914cbf153881e4e262a6a6d141aea31e7092
Christian F <crystalpalace1977@googlemail.com>
2019-08-28 20:29:42 +0200

    initial test code for WM management
    Former-commit-id: 3792997c7817fefe6dabcfdf25a46165e929aa06

--- src/main/java/mediathek/javafx/filmlist/FilmListAgeLabel.java
+++ src/main/java/mediathek/javafx/filmlist/FilmListAgeLabel.java
@@ -6,7 +6,11 @@ import javafx.animation.Timeline;
 import javafx.util.Duration;
 import mediathek.config.Daten;
 import mediathek.javafx.tool.ComputedLabel;
+import mediathek.mainwindow.MediathekGui;
 import org.apache.commons.lang3.time.DurationFormatUtils;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 /**
@@ -14,14 +18,51 @@ import org.apache.commons.lang3.time.DurationFormatUtils;
  * Update cycle one second.
  */
 class FilmListAgeLabel extends ComputedLabel {
-    FilmListAgeLabel() {
-        super();
+    private Timeline timeline;
 
+    FilmListAgeLabel() {
         setupTimer();
+        installWindowListener();
+    }
+
+    private void installWindowListener() {
+        MediathekGui.ui().addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowIconified(WindowEvent e) {
+                System.out.println("ICONIFIED");
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+                System.out.println("DEICONIFIED");
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+                System.out.println("ACTIVATED");
+                timeline.play();
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                System.out.println("DEACTIVATED");
+                timeline.pause();
+            }
+
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                System.out.println("GAINED FOCUS");
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                System.out.println("LOST FOCUS");
+            }
+        });
     }
 
     private void setupTimer() {
-        var timeline = new Timeline(new KeyFrame(Duration.millis(1000d), ae -> setAgeToLabel()));
+        timeline = new Timeline(new KeyFrame(Duration.millis(1000d), ae -> setAgeToLabel()));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
     }
