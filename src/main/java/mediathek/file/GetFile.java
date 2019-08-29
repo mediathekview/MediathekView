26e64efd635e49d6d28ffed8aa6a09706f245c46
Christian F <crystalpalace1977@googlemail.com>
2019-08-29 12:50:27 +0200

    remove unused files

--- src/main/java/mediathek/file/GetFile.java
+++ src/main/java/mediathek/file/GetFile.java
@@ -17,13 +17,11 @@ public class GetFile {
     public static final String PFAD_PSET_WINDOWS = "/mediathek/file/pset_windows.xml";
     public static final String PFAD_PSET_MAC = "/mediathek/file/pset_mac.xml";
     public static final String PFAD_HILFETEXT_GEO = "/mediathek/file/hilfetext_geo.txt";
-    public static final String PFAD_HILFETEXT_FILTER = "/mediathek/file/hilfetext_filter.txt";
     public static final String PFAD_HILFETEXT_BLACKLIST = "/mediathek/file/hilfetext_blacklist.txt";
     public static final String PFAD_HILFETEXT_BEENDEN = "/mediathek/file/hilfetext_beenden.txt";
     public static final String PFAD_HILFETEXT_PRGRAMME = "/mediathek/file/hilfetext_pset.txt";
     public static final String PFAD_HILFETEXT_STANDARD_PSET = "hilfetext_standardPset.txt";
     public static final String PFAD_HILFETEXT_EDIT_DOWNLOAD_PROG = "hilfetext_editDownloadProg.txt";
-    public static final String PFAD_HILFETEXT_UNICODE = "hilfetext_unicode.txt";
     public static final String PFAD_HILFETEXT_RESET = "hilfetext_reset.txt";
     public static final String PFAD_HILFETEXT_RESET_SET = "hilfetext_reset_set.txt";
     public static final String PFAD_HILFETEXT_DIALOG_MEDIA_DB = "hilfetext_dialog_mediaDb.txt";
