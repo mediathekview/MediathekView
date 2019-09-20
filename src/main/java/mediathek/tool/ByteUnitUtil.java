7e33b2122fa0f078beabf827b2e7d4ccd0598c46
Me Like Cookies <me.like.cookies@gmx.de>
2019-09-20 22:54:51 +0200

    use BigDecimal.compareTo(100) rather than Bigdecimal.divide(100).compareTo(1) use BigDecimal.compareTo(10) rather than Bigdecimal.divide(10).compareTo(1) refactoring: new class name ByteUnitUtil

--- src/main/java/mediathek/tool/ByteUnitUtil.java
+++ src/main/java/mediathek/tool/ByteUnitUtil.java
@@ -1,10 +1,15 @@
 package mediathek.tool;
 
+import static org.apache.commons.io.FileUtils.ONE_EB_BI;
+import static org.apache.commons.io.FileUtils.ONE_GB_BI;
+import static org.apache.commons.io.FileUtils.ONE_KB_BI;
+import static org.apache.commons.io.FileUtils.ONE_MB_BI;
+import static org.apache.commons.io.FileUtils.ONE_PB_BI;
+import static org.apache.commons.io.FileUtils.ONE_TB_BI;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
-
-import static org.apache.commons.io.FileUtils.*;
 
 /**
  * Custom Implementation of FileUtils.byteCountToDisplaySize to fix rounding bug
@@ -16,7 +21,7 @@ import static org.apache.commons.io.FileUtils.*;
  * With this version, the displayed value is always set at least 3 numericals so: ####, ###, ##.# or #.##.
  * If anyone sees a serious issue, please give me a call.
  */
-public class FileUtil {
+public class ByteUnitUtil {
 
     private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
 
@@ -29,15 +34,15 @@ public class FileUtil {
         KILOBYTE("KB", ONE_KB_BI),
         BYTE("bytes", BigInteger.ONE);
 
-        private final String unit;
+        private final String unitName;
         private final BigInteger byteCount;
 
-        FileSize(String unit, BigInteger byteCount) {
-            this.unit = unit;
+        FileSize(String unitName, BigInteger byteCount) {
+            this.unitName = unitName;
             this.byteCount = byteCount;
         }
-        private String unit() {
-            return unit;
+        private String unitName() {
+            return unitName;
         }
         private BigInteger byteCount() {
             return byteCount;
@@ -53,25 +58,28 @@ public class FileUtil {
      */
     public static String byteCountToDisplaySize(final BigInteger fileSize) {
 
-        String unit = FileSize.BYTE.unit;
+        String unit = FileSize.BYTE.unitName;
         BigDecimal fileSizeInUnit = BigDecimal.ZERO;
         String val;
 
         for (FileSize fs : FileSize.values()) {
             BigDecimal size_bd = new BigDecimal(fileSize);
-            fileSizeInUnit = size_bd.divide(new BigDecimal(fs.byteCount), 5, ROUNDING_MODE);
+            fileSizeInUnit = size_bd.divide(new BigDecimal(fs.byteCount()), 5, ROUNDING_MODE);
             if (fileSizeInUnit.compareTo(BigDecimal.ONE) >= 0) {
-                unit = fs.unit;
+                unit = fs.unitName();
                 break;
             }
         }
-
+        
         // always round so that at least 3 numerics are displayed (###, ##.#, #.##)
-        if (fileSizeInUnit.divide(BigDecimal.valueOf(100.0), BigDecimal.ROUND_DOWN).compareTo(BigDecimal.ONE) >= 0) {
+        if (fileSizeInUnit.compareTo(BigDecimal.valueOf(100.0)) >= 0) {
+        	// with fileSizeInUnit with 3 or 4 digits (100, 101, ..., 999, 1000, ..., 1023) do not display any decimal places
             val = fileSizeInUnit.setScale(0, ROUNDING_MODE).toString();
-        } else if (fileSizeInUnit.divide(BigDecimal.valueOf(10.0), BigDecimal.ROUND_DOWN).compareTo(BigDecimal.ONE) >= 0) {
+        } else if (fileSizeInUnit.compareTo(BigDecimal.valueOf(10.0)) >= 0) {
+        	// with fileSizeInUnit with 2 digits (10, 11, ..., 99.999) display the first decimal place
             val = fileSizeInUnit.setScale(1, ROUNDING_MODE).toString();
         } else {
+        	// with fileSizeInUnit with 1 digit (0, 1, ..., 9.9999) display the first two decimal places
             val = fileSizeInUnit.setScale(2, ROUNDING_MODE).toString();
         }
 
