0aedb42294a201848122f212a7ac30ac802242ba
Christian F <crystalpalace1977@googlemail.com>
2019-12-05 08:19:36 +0100

    - convert download toolbar to FXML

--- src/main/java/mediathek/gui/toolbar/FXDownloadToolBar.java
+++ src/main/java/mediathek/gui/toolbar/FXDownloadToolBar.java
@@ -1,65 +1,45 @@
 package mediathek.gui.toolbar;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
-import javafx.scene.control.Tooltip;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-import org.controlsfx.glyphfont.FontAwesome;
-import org.controlsfx.glyphfont.GlyphFont;
-import org.controlsfx.glyphfont.GlyphFontRegistry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class FXDownloadToolBar extends ToolBar {
-    public final Button btnUpdateDownloads;
-    public final Button btnFilmInfo;
-    public final Button btnStartAllDownloads;
-    public final Button btnPlayFilm;
-    public final Button btnZurueckstellen;
-    public final Button btnRemoveDownload;
-    public final Button btnCleanup;
-    public final Button btnFilter;
+    private static final Logger logger = LogManager.getLogger(FXDownloadToolBar.class);
+    @FXML
+    public Button btnUpdateDownloads;
+    @FXML
+    public Button btnFilmInfo;
+    @FXML
+    public Button btnStartAllDownloads;
+    @FXML
+    public Button btnPlayFilm;
+    @FXML
+    public Button btnZurueckstellen;
+    @FXML
+    public Button btnRemoveDownload;
+    @FXML
+    public Button btnCleanup;
+    @FXML
+    public Button btnFilter;
 
     public FXDownloadToolBar() {
         super();
 
-        GlyphFont fontAwesome = GlyphFontRegistry.font("FontAwesome");
-
-        btnFilmInfo = new Button("", fontAwesome.create(FontAwesome.Glyph.INFO_CIRCLE).size(16d));
-        btnFilmInfo.setTooltip(new Tooltip("Filminformation anzeigen"));
-
-        btnUpdateDownloads = new Button("",fontAwesome.create(FontAwesome.Glyph.REFRESH).size(16d));
-        btnUpdateDownloads.setTooltip(new Tooltip("Downloadliste aktualisieren"));
-
-        btnStartAllDownloads = new Button("",fontAwesome.create(FontAwesome.Glyph.ANGLE_DOUBLE_DOWN).size(16d));
-        btnStartAllDownloads.setTooltip(new Tooltip("Alle Downloads starten"));
-
-        btnPlayFilm = new Button("",fontAwesome.create(FontAwesome.Glyph.PLAY).size(16d));
-        btnPlayFilm.setTooltip(new Tooltip("Film abspielen"));
-
-        btnZurueckstellen = new Button("",fontAwesome.create(FontAwesome.Glyph.CLOCK_ALT).size(16d));
-        btnZurueckstellen.setTooltip(new Tooltip("Downloads zurückstellen"));
-
-        btnRemoveDownload = new Button("",fontAwesome.create(FontAwesome.Glyph.TRASH_ALT).size(16d));
-        btnRemoveDownload.setTooltip(new Tooltip("Downloads entfernen"));
-
-        btnCleanup = new Button("",fontAwesome.create(FontAwesome.Glyph.ERASER).size(16d));
-        btnCleanup.setTooltip(new Tooltip("Liste säubern"));
-
-        btnFilter = new Button("", fontAwesome.create(FontAwesome.Glyph.FILTER).size(16d));
-        btnFilter.setTooltip(new Tooltip("Filter anzeigen/ausblenden"));
-
-        Region spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-
-        getItems().addAll(btnFilmInfo,
-                btnUpdateDownloads,
-                btnStartAllDownloads,
-                btnPlayFilm,
-                btnZurueckstellen,
-                btnRemoveDownload,
-                btnCleanup,
-                spacer,
-                btnFilter);
+        try {
+            URL url = getClass().getResource("/mediathek/res/programm/fxml/download_toolbar.fxml");
+            FXMLLoader fxmlLoader = new FXMLLoader(url);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.setController(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            logger.error("Failed to load FXML!");
+        }
     }
 }
