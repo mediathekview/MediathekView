aa715cf446bb9a3ea998e6a438c59822110bb3f3
Christian F <crystalpalace1977@googlemail.com>
2019-12-05 13:36:22 +0100

    - code refactor

---
+++ src/main/java/mediathek/javafx/filterpanel/FXFilmToolBar.java
@@ -0,0 +1,52 @@
+package mediathek.javafx.filterpanel;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToolBar;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class FXFilmToolBar extends ToolBar {
+    @FXML
+    public Button btnDownloadFilmList;
+
+    @FXML
+    public Button btnFilmInfo;
+
+    @FXML
+    Button btnPlay;
+
+    @FXML
+    Button btnRecord;
+
+    @FXML
+    Button btnManageAbos;
+
+    @FXML
+    Button btnShowFilter;
+
+    @FXML
+    JFXSearchPanel jfxSearchField;
+
+    @FXML
+    ToggleButton btnSearchThroughDescription;
+
+    public FXFilmToolBar() {
+        super();
+        try {
+            URL url = getClass().getResource("/mediathek/res/programm/fxml/film_toolbar.fxml");
+            FXMLLoader fxmlLoader = new FXMLLoader(url);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.setController(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            Logger logger = LogManager.getLogger(FXFilmToolBar.class);
+            logger.error("Failed to load FXML!");
+        }
+    }
+}
