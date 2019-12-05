d14a9e570ff14614c08c686c19879792073c8492
Christian F <crystalpalace1977@googlemail.com>
2019-12-05 09:55:09 +0100

    - convert abo dialog javafx parts to FXML

--- src/main/java/mediathek/gui/abo/FXAboToolBar.java
+++ src/main/java/mediathek/gui/abo/FXAboToolBar.java
@@ -1,81 +1,55 @@
 package mediathek.gui.abo;
 
 import ca.odell.glazedlists.javafx.EventObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ToolBar;
 import mediathek.config.Daten;
-import mediathek.gui.actions.CreateNewAboAction;
-import mediathek.javafx.CenteredBorderPane;
-import mediathek.javafx.VerticalSeparator;
 import mediathek.tool.SenderList;
-import org.controlsfx.glyphfont.FontAwesome;
-import org.controlsfx.glyphfont.GlyphFont;
-import org.controlsfx.glyphfont.GlyphFontRegistry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class FXAboToolBar extends ToolBar implements Initializable {
-
+    private static final Logger logger = LogManager.getLogger(FXAboToolBar.class);
+    @FXML
     public ComboBox<String> cbSender;
+    @FXML
     public Button btnOn;
+    @FXML
     public Button btnOff;
+    @FXML
     public Button btnDelete;
+    @FXML
     public Button btnEdit;
+    @FXML
     public Button btnNewAbo;
 
-    public FXAboToolBar(ManageAboPanel tabAbo) {
+    public FXAboToolBar() {
         super();
 
-        GlyphFont fontAwesome = GlyphFontRegistry.font("FontAwesome");
-
-        btnOn = new Button("", fontAwesome.create(FontAwesome.Glyph.CHECK).size(16d));
-        btnOn.setTooltip(new Tooltip("Abos einschalten"));
-        btnOn.setOnAction(e -> SwingUtilities.invokeLater(() -> tabAbo.einAus(true)));
-
-        btnOff = new Button("", fontAwesome.create(FontAwesome.Glyph.REMOVE).size(16d));
-        btnOff.setTooltip(new Tooltip("Abos ausschalten"));
-        btnOff.setOnAction(e -> SwingUtilities.invokeLater(() -> tabAbo.einAus(false)));
-
-        btnDelete = new Button("", fontAwesome.create(FontAwesome.Glyph.MINUS).size(16d));
-        btnDelete.setTooltip(new Tooltip("Abos löschen"));
-        btnDelete.setOnAction(e -> SwingUtilities.invokeLater(tabAbo::loeschen));
-
-        btnEdit = new Button("", fontAwesome.create(FontAwesome.Glyph.EDIT).size(16d));
-        btnEdit.setTooltip(new Tooltip("Abo ändern"));
-        btnEdit.setOnAction(e -> SwingUtilities.invokeLater(tabAbo::editAbo));
-
-        btnNewAbo = new Button("", fontAwesome.create(FontAwesome.Glyph.PLUS).size(16d));
-        btnNewAbo.setTooltip(new Tooltip("Abo anlegen"));
-        CreateNewAboAction newAboAction = new CreateNewAboAction(Daten.getInstance().getListeAbo());
-        btnNewAbo.setOnAction(e -> SwingUtilities.invokeLater(() -> newAboAction.actionPerformed(null)));
-
-        var senderList = new SenderList(Daten.getInstance().getListeFilme().getBaseSenderList());
-        cbSender = new ComboBox<>(new EventObservableList<>(senderList));
-        cbSender.getSelectionModel().select(0);
-
-        HBox hBox = new HBox();
-        hBox.getChildren().addAll(new CenteredBorderPane(new Label("Abos für Sender:")), cbSender);
-
-        getItems().addAll(btnOn,
-                btnOff,
-                new VerticalSeparator(),
-                btnNewAbo,
-                btnDelete,
-                btnEdit,
-                new VerticalSeparator(),
-                hBox);
-    }
-
-    public ComboBox<String> getSenderComboBox() {
-        return cbSender;
+        try {
+            URL url = getClass().getResource("/mediathek/res/programm/fxml/abo_toolbar.fxml");
+            FXMLLoader fxmlLoader = new FXMLLoader(url);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.setController(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            logger.error("Failed to load FXML!", e);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        var senderList = new SenderList(Daten.getInstance().getListeFilme().getBaseSenderList());
+        cbSender.setItems(new EventObservableList<>(senderList));
+        cbSender.getSelectionModel().select(0);
     }
 }
