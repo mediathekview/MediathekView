e246f8f24524db38a6382872d306cfb7e7f153d0
Christian F <crystalpalace1977@googlemail.com>
2019-12-05 21:06:27 +0100

    - optimize filmlist length slider layout

--- src/main/java/mediathek/javafx/filterpanel/FilmLenghtSliderNode.java
+++ src/main/java/mediathek/javafx/filterpanel/FilmLenghtSliderNode.java
@@ -1,7 +1,10 @@
 package mediathek.javafx.filterpanel;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import org.controlsfx.tools.Borders;
 
@@ -12,19 +15,21 @@ public class FilmLenghtSliderNode extends VBox {
         Label lblMin = new Label("min");
         Label lblMax = new Label("max");
 
+        var spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
         HBox hb = new HBox();
-        hb.getChildren().addAll(new Label("Mindestlänge:"), lblMin);
-
-        HBox hb2 = new HBox();
-        hb2.getChildren().addAll(new Label("Maximallänge:"), lblMax);
-
-        VBox vb2 = new VBox();
-        vb2.getChildren().addAll(hb, hb2);
+        hb.setPadding(new Insets(0,5,0,5));
+        hb.getChildren().addAll(new Label("Mindestlänge: "), lblMin,
+                spacer,
+                new Label("Maximallänge: "), lblMax);
 
         _filmLengthSlider = new FilmLengthSlider();
         _filmLengthSlider.lowValueProperty().addListener((observable, oldValue, newValue) -> lblMin.setText(String.valueOf(newValue.intValue())));
         _filmLengthSlider.highValueProperty().addListener((observable, oldValue, newValue) -> lblMax.setText(_filmLengthSlider.getLabelFormatter().toString(newValue)));
-        vb2.getChildren().add(_filmLengthSlider);
+
+        VBox vb2 = new VBox();
+        vb2.getChildren().addAll(hb, _filmLengthSlider);
 
         lblMin.setText(String.valueOf((int) _filmLengthSlider.getLowValue()));
         lblMax.setText(_filmLengthSlider.getLabelFormatter().toString(_filmLengthSlider.getHighValue()));
