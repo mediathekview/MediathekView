f84e310d8ea1a7339212bc62f2cc46e1549c0c14
Christian F <crystalpalace1977@googlemail.com>
2019-12-20 07:25:49 +0100

    - code cleanup
    Former-commit-id: 193196bb0aca64f6c3bb1020823292b85209d386

--- src/main/java/mediathek/javafx/filterpanel/FilmLenghtSliderNode.java
+++ src/main/java/mediathek/javafx/filterpanel/FilmLenghtSliderNode.java
@@ -14,27 +14,15 @@ public class FilmLenghtSliderNode extends VBox {
     private final Label lblMax = new Label("max");
 
     public FilmLenghtSliderNode() {
-        var spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-
-        HBox hb = new HBox();
-        hb.setPadding(new Insets(0, 5, 0, 5));
-        Label lblMaximalLaenge = new Label("Maximallänge: ");
-        Label lblMindestlaenge = new Label("Mindestlänge: ");
-        hb.getChildren().addAll(lblMindestlaenge,
-                lblMin,
-                spacer,
-                lblMaximalLaenge,
-                lblMax);
-
         _filmLengthSlider.lowValueProperty().addListener((observable, oldValue, newValue) -> lblMin.setText(String.valueOf(newValue.intValue())));
         _filmLengthSlider.highValueProperty().addListener((observable, oldValue, newValue) -> lblMax.setText(_filmLengthSlider.getLabelFormatter().toString(newValue)));
 
-        VBox vb2 = new VBox();
-        vb2.getChildren().addAll(hb, _filmLengthSlider);
-
         lblMin.setText(String.valueOf((int) _filmLengthSlider.getLowValue()));
         lblMax.setText(_filmLengthSlider.getLabelFormatter().toString(_filmLengthSlider.getHighValue()));
+
+        var vb2 = new VBox();
+        vb2.getChildren().addAll(createHeader(),
+                _filmLengthSlider);
 
         var result = Borders.wrap(vb2)
                 .lineBorder()
@@ -43,5 +31,23 @@ public class FilmLenghtSliderNode extends VBox {
                 .buildAll();
 
         getChildren().add(result);
+    }
+
+    private HBox createHeader() {
+        var spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        var header = new HBox();
+        header.setPadding(new Insets(0, 5, 0, 5));
+
+        var lblMaximalLaenge = new Label("Maximallänge: ");
+        var lblMindestlaenge = new Label("Mindestlänge: ");
+        header.getChildren().addAll(lblMindestlaenge,
+                lblMin,
+                spacer,
+                lblMaximalLaenge,
+                lblMax);
+
+        return header;
     }
 }
