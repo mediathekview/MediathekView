693441bda963478a37018856b823497b7219f190
Christian F <crystalpalace1977@googlemail.com>
2020-01-13 22:55:20 +0100

    wire downloads tab

--- src/main/java/mediathek/gui/GuiDownloads.java
+++ src/main/java/mediathek/gui/GuiDownloads.java
@@ -1,8 +1,9 @@
 package mediathek.gui;
 
 import com.thizzer.jtouchbar.JTouchBar;
+import com.thizzer.jtouchbar.common.ImageName;
 import com.thizzer.jtouchbar.item.TouchBarItem;
-import com.thizzer.jtouchbar.item.view.TouchBarTextField;
+import com.thizzer.jtouchbar.item.view.TouchBarButton;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
@@ -182,10 +183,41 @@ public class GuiDownloads extends AGuiTabPanel {
         touchBar = new JTouchBar();
         touchBar.setCustomizationIdentifier("tabDownloads");
 
-        TouchBarTextField touchBarTextField = new TouchBarTextField();
-        touchBarTextField.setStringValue("TextField 1");
+        TouchBarButton btnFilmInformation = new TouchBarButton();
+        btnFilmInformation.setAction(view -> SwingUtilities.invokeLater(MediathekGui.ui().getFilmInfoDialog()::showInfo));
+        btnFilmInformation.setImage(TouchBarUtils.touchBarImageFromIcon(TouchBarUtils.iconFromFontAwesome(FontAwesome.INFO_CIRCLE)));
+        touchBar.addItem(new TouchBarItem("btnFilmInformation", btnFilmInformation,false));
 
-        touchBar.addItem(new TouchBarItem("id1",touchBarTextField,false));
+        TouchBarButton btnUpdateDownloads = new TouchBarButton();
+        btnUpdateDownloads.setImage(TouchBarUtils.touchBarImageFromFontAwesome(FontAwesome.REFRESH));
+        btnUpdateDownloads.setAction(f -> SwingUtilities.invokeLater(this::updateDownloads));
+        touchBar.addItem(new TouchBarItem("btnUpdateDownloads", btnUpdateDownloads, false));
+
+        TouchBarButton btnStartAllDownloads = new TouchBarButton();
+        btnStartAllDownloads.setImage(TouchBarUtils.touchBarImageFromFontAwesome(FontAwesome.ANGLE_DOUBLE_DOWN));
+        btnStartAllDownloads.setAction(f -> SwingUtilities.invokeLater(() -> starten(true)));
+        touchBar.addItem(new TouchBarItem("btnStartAllDownloads", btnStartAllDownloads,false));
+
+        TouchBarButton btnPlayFilm = new TouchBarButton();
+        var playImage = new com.thizzer.jtouchbar.common.Image(ImageName.NSImageNameTouchBarPlayTemplate, false);
+        btnPlayFilm.setImage(playImage);
+        btnPlayFilm.setAction(f -> SwingUtilities.invokeLater(this::filmAbspielen));
+        touchBar.addItem(new TouchBarItem("btnPlayFilm", btnPlayFilm,false));
+
+        TouchBarButton btnZurueckstellen = new TouchBarButton();
+        btnZurueckstellen.setImage(TouchBarUtils.touchBarImageFromFontAwesome(FontAwesome.CLOCK_O));
+        btnZurueckstellen.setAction(f -> SwingUtilities.invokeLater(() -> downloadLoeschen(false)));
+        touchBar.addItem(new TouchBarItem("btnZurueckstellen", btnZurueckstellen, false));
+
+        TouchBarButton btnRemoveDownload = new TouchBarButton();
+        btnRemoveDownload.setImage(TouchBarUtils.touchBarImageFromFontAwesome(FontAwesome.TRASH_O));
+        btnRemoveDownload.setAction(f -> SwingUtilities.invokeLater(() -> downloadLoeschen(true)));
+        touchBar.addItem(new TouchBarItem("btnRemoveDownload", btnRemoveDownload,false));
+
+        TouchBarButton btnCleanup = new TouchBarButton();
+        btnCleanup.setImage(TouchBarUtils.touchBarImageFromFontAwesome(FontAwesome.ERASER));
+        btnCleanup.setAction(f -> SwingUtilities.invokeLater(this::cleanupDownloads));
+        touchBar.addItem(new TouchBarItem("btnCleanup", btnCleanup,false));
     }
 
     private void setupToolBar() {
