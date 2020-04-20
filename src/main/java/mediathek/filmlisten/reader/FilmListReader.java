ee9a9c669e9860a2d80f444345a3c50d45f64b1b
Christian F <crystalpalace1977@googlemail.com>
2020-04-20 14:24:01 +0200

    - code cleanup
    Former-commit-id: 8562275df521c27de54e20c075db81dbdbf1cf7d

--- src/main/java/mediathek/filmlisten/reader/FilmListReader.java
+++ src/main/java/mediathek/filmlisten/reader/FilmListReader.java
@@ -486,7 +486,7 @@ public class FilmListReader implements AutoCloseable {
 
     private void notifyFertig(String url, ListeFilme liste) {
         logger.info("Liste Filme gelesen am: {}", FastDateFormat.getInstance("dd.MM.yyyy, HH:mm").format(new Date()));
-        logger.info("  erstellt am: {}", liste.metaData().getGenerationDateTime());
+        logger.info("  erstellt am: {}", liste.metaData().getGenerationDateTimeAsString());
         logger.info("  Anzahl Filme: {}", liste.size());
         for (ListenerFilmeLaden l : listeners.getListeners(ListenerFilmeLaden.class)) {
             progressEvent.senderUrl = url;
