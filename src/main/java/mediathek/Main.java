62bfe73ddc15ae0a3003858fa7720da0f387e30a
Christian F <crystalpalace1977@googlemail.com>
2020-05-12 09:43:52 +0200

    - disable unsafe reflection usage by default
    Former-commit-id: 423c4adaae7a069eebe611ff5cc895d0914891d3

--- src/main/java/mediathek/Main.java
+++ src/main/java/mediathek/Main.java
@@ -319,6 +319,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(final String... args) {
+        Functions.disableAccessWarnings();
         setupEnvironmentProperties();
 
         if (GraphicsEnvironment.isHeadless()) {
