d40a609e2f3ec96e70898f41e4c7e9ed16f73281
Christian F <crystalpalace1977@googlemail.com>
2020-05-12 09:20:56 +0200

    - replace jmdns with a faster and more reliable discovery service
    Former-commit-id: 0a6d301974acda619a7d9b00fedbc64ace269bbb

--- src/main/java/mediathek/tool/notification/MacNotificationCenter.java
+++ src/main/java/mediathek/tool/notification/MacNotificationCenter.java
@@ -5,6 +5,10 @@ import mediathek.config.Konstanten;
 import mediathek.tool.javafx.FXErrorDialog;
 import mediathek.tool.notification.thrift.NotificationMessage;
 import mediathek.tool.notification.thrift.ThriftNotificationCenter;
+import net.posick.mDNS.Browse;
+import net.posick.mDNS.DNSSDListener;
+import net.posick.mDNS.MulticastDNSService;
+import net.posick.mDNS.ServiceInstance;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.thrift.TException;
@@ -13,25 +17,22 @@ import org.apache.thrift.protocol.TMultiplexedProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.jetbrains.annotations.NotNull;
+import org.xbill.DNS.Message;
 
-import javax.jmdns.JmDNS;
-import javax.jmdns.ServiceEvent;
-import javax.jmdns.ServiceListener;
-import javax.jmdns.impl.JmDNSImpl;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.util.Arrays;
 
-public class MacNotificationCenter implements INotificationCenter, ServiceListener, Closeable {
+public class MacNotificationCenter implements INotificationCenter, Closeable {
     private static final Logger logger = LogManager.getLogger();
     private static final String NOTIFICATION_SERVICE_NAME = "NotificationService";
-    private JmDNS jmdns;
+    private static final String MV_NOTIFICATION_MDNS_NAME = "_mv-notification._tcp.local.";
     private TSocket transport;
     private TProtocol protocol;
     private TMultiplexedProtocol mp;
     private ThriftNotificationCenter.Client client;
+    private MulticastDNSService service;
 
     public MacNotificationCenter() {
         setupJmdnsListener();
@@ -39,37 +40,39 @@ public class MacNotificationCenter implements INotificationCenter, ServiceListen
 
     private void setupJmdnsListener() {
         try {
-            jmdns = JmDNS.create(InetAddress.getLocalHost());
-            jmdns.addServiceListener("_mv-notification._tcp.local.", this);
+            service = new MulticastDNSService();
+            service.startServiceDiscovery(new Browse(MV_NOTIFICATION_MDNS_NAME), new DNSSDListener() {
+                public void serviceDiscovered(Object id, ServiceInstance service) {
+                    logger.trace("Notification Service Discovered");
+                    Arrays.stream(service.getAddresses())
+                            .filter(a -> a instanceof Inet4Address).findAny()
+                            .ifPresent(a -> {
+                                cleanupThrift();
+                                createThriftClient(a.getHostAddress(), service.getPort());
+                            });
+                }
+
+
+                public void serviceRemoved(Object id, ServiceInstance service) {
+                    logger.trace("Notification Service Removed");
+                    cleanupThrift();
+                }
+
+
+                public void handleException(Object id, Exception e) {
+                    logger.error("mDNS exception occured", e);
+                }
+
+
+                public void receiveMessage(Object id, Message message) {
+                }
+            });
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    @Override
-    public void displayNotification(NotificationMessage msg) {
-            try {
-                logger.trace("Sending native notification to {} on serverPort {}", transport.getSocket().getInetAddress(), transport.getSocket().getPort());
-                client.displayNotification(msg);
-            }
-            catch (Exception e) {
-                showErrorDialog(e);
-            }
-    }
-
-    private void showErrorDialog(Exception ex) {
-        Platform.runLater(() -> FXErrorDialog.showErrorDialog(Konstanten.PROGRAMMNAME, "Native Benachrichtigungen können nicht angezeigt werden",
-                "Bitte stellen Sie sicher das das Hilfsprogramm gestartet ist.", ex));
-    }
-
-    @Override
-    public void serviceAdded(ServiceEvent serviceEvent) {
-        // do nothing as we don´t have useful information in here
-    }
-
-    @Override
-    public void serviceRemoved(ServiceEvent serviceEvent) {
-        System.out.println("REMOVING SERVICE DELETING TRANSPORT");
+    private void cleanupThrift() {
         if (transport != null) {
             if (transport.isOpen())
                 transport.close();
@@ -78,56 +81,26 @@ public class MacNotificationCenter implements INotificationCenter, ServiceListen
         mp = null;
         client = null;
         protocol = null;
-
-        //invalidate cache...maybe this prevents exceptions
-        JmDNSImpl impl = (JmDNSImpl)jmdns;
-        impl.getDns().cleanCache();
-        impl.getCache().clear();
-        impl.cleanCache();
     }
 
     @Override
-    public void serviceResolved(ServiceEvent serviceEvent) {
-        var info = serviceEvent.getInfo();
-        System.out.println("SERVICE RESOLVED: " + serviceEvent.getType());
+    public void displayNotification(NotificationMessage msg) {
+        try {
+            logger.trace("Sending native notification to {} on serverPort {}", transport.getSocket().getInetAddress(), transport.getSocket().getPort());
+            client.displayNotification(msg);
+        } catch (Exception e) {
+            showErrorDialog(e);
+        }
+    }
 
-        var clientAddress = Arrays.stream(info.getInetAddresses()).filter(addr -> addr instanceof Inet4Address).findFirst();
-        clientAddress.ifPresent(addr -> {
-            System.out.println("SERVICE RESOLVED IPV4 TRANSPORT");
-            System.out.println("Address: " + addr.getHostAddress());
-            System.out.println("Port: " + info.getPort());
-
-            if (transport == null) {
-                System.out.println("TRANSPORT IS NULL");
-                createThriftClient(addr.getHostAddress(),info.getPort());
-            }
-            else {
-                //we already have a transport
-                var socket = transport.getSocket();
-                var boundAddr = socket.getInetAddress();
-                var boundPort = socket.getPort();
-
-                //check if transport is bound to resolved entry
-                if (boundPort == info.getPort() && boundAddr.equals(addr)) {
-                    // this transport is already assigned to the advertisement -> no action
-                    System.out.println("PORT AN ADDR EQUAL -> NO ACTION");
-                }
-                else {
-                    System.out.println("PORT AND ADDR NOTEQUAL -> RECREATE TRANSPORT");
-                    //reassign transport
-                    if (transport.isOpen())
-                        transport.close();
-
-                    createThriftClient(addr.getHostAddress(),info.getPort());
-                }
-            }
-        });
-
+    private void showErrorDialog(Exception ex) {
+        Platform.runLater(() -> FXErrorDialog.showErrorDialog(Konstanten.PROGRAMMNAME, "Native Benachrichtigungen können nicht angezeigt werden",
+                "Bitte stellen Sie sicher das das Hilfsprogramm gestartet ist.", ex));
     }
 
     private void createThriftClient(@NotNull String addr, int port) {
         try {
-            System.out.println("CREATING THRIFT CLIENT");
+            logger.trace("creating thrift client");
             transport = new TSocket(addr, port);
             transport.open();
             protocol = new TBinaryProtocol(transport);
@@ -135,7 +108,7 @@ public class MacNotificationCenter implements INotificationCenter, ServiceListen
 
             client = new ThriftNotificationCenter.Client(mp);
         } catch (TException e) {
-            logger.error("Client creation failed!",e);
+            logger.error("thrift client creation failed!", e);
             transport = null;
             mp = null;
             client = null;
@@ -145,11 +118,12 @@ public class MacNotificationCenter implements INotificationCenter, ServiceListen
 
     @Override
     public void close() throws IOException {
+        if (service != null)
+            service.close();
+
         if (transport != null) {
             if (transport.isOpen())
                 transport.close();
         }
-        if (jmdns != null)
-            jmdns.close();
     }
 }
