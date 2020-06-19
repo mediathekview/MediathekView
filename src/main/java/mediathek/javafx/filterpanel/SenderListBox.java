d63fd7035485a49c76331b34892bcd5ca156a6d7
Christian F <crystalpalace1977@googlemail.com>
2020-06-19 12:31:37 +0200

    - fixes #449
    Former-commit-id: a259bebc1138d1531ab4d5b947b2ec044cfeb96e

--- src/main/java/mediathek/javafx/filterpanel/SenderListBox.java
+++ src/main/java/mediathek/javafx/filterpanel/SenderListBox.java
@@ -1,5 +1,6 @@
 package mediathek.javafx.filterpanel;
 
+import javafx.collections.ListChangeListener;
 import mediathek.config.Daten;
 import org.controlsfx.control.CheckListView;
 
@@ -8,5 +9,10 @@ public class SenderListBox extends CheckListView<String> {
         super(Daten.getInstance().getListeFilmeNachBlackList().getSenders());
         setPrefHeight(150d);
         setMinHeight(100d);
+
+        Daten.getInstance()
+                .getListeFilmeNachBlackList().
+                getSenders()
+                .addListener((ListChangeListener<String>) change -> getCheckModel().clearChecks());
     }
 }
