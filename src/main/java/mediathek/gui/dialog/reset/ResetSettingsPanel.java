23c3e99381c689d4f7a12a6805eade8fc7300ef6
Christian F <crystalpalace1977@googlemail.com>
2020-06-22 09:48:57 +0200

    - convert swing dialog to javafx alert
    Former-commit-id: f81e36522f0f1bcd4ddef17787477b5e234bc510

--- src/main/java/mediathek/gui/dialog/reset/ResetSettingsPanel.java
+++ src/main/java/mediathek/gui/dialog/reset/ResetSettingsPanel.java
@@ -33,7 +33,7 @@ public class ResetSettingsPanel extends JPanel {
             if (ret == JOptionPane.OK_OPTION) {
                 // damit wird vor dem Beenden das Konfig-Verzeichnis umbenannt und so startet das
                 // Programm wie beim ersten Start
-                Daten.setReset(true);
+                Daten.setRestConfigurationData(true);
                 MediathekGui.ui().beenden(false, false);
             }
         });
