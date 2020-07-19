a55cd9b9e502691468fdc2707b3057c9509ca183
Nicklas Wiegandt <Nicklas2751@users.noreply.github.com>
2020-07-19 14:28:18 +0200

    Feature #523 (#524)
    * Changed to own filter config and multiple filters
    Extracted filter configurations to a new own class with easier access and changed to load for current filter so there can be many filters. This enables to store different filter sets
    
    * Working multi filter configuration with tests
    
    * Implemented filter migration
    
    * Changed logging language to german
    
    * Fixed filter loading and saving
    
    * Begon to add UI for filter switch
    
    * Implemented filter deletion
    
    * Added UI for deleting filter
    
    * Added method to get current filter
    
    * Changed to save filter DTOs instead of a id and a name list
    
    * Changed UI a bit and fixed filter deleting
    
    * Fixed filter deletion and disabling of its delete button
    
    * Added add new filter button and its listener
    
    * Fixed filter renaming and adding
    
    * Added observers for available filters and current filter to add second selection in tool bar
    
    * Refactorings
    
    * - fix incorrect rename of FXML variables
    
    * - remove unused graphics
    - optimize visual rep of filter panel
    
    * - remove unused graphics
    
    * - add tooltip text to filter combo
    
    * - fix typo
    - suppress warning
    
    * - code cleanup
    
    * - add vsep to toolbar
    
    Co-authored-by: Christian F <crystalpalace1977@googlemail.com>
    Former-commit-id: b1fb39a7899d37df730176e4ec5e548f7b505242

--- src/main/java/mediathek/javafx/filterpanel/FXFilmToolBar.java
+++ src/main/java/mediathek/javafx/filterpanel/FXFilmToolBar.java
@@ -1,10 +1,12 @@
 package mediathek.javafx.filterpanel;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
+import mediathek.tool.FilterConfiguration;
+import mediathek.tool.FilterDTO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,50 +14,66 @@ import java.io.IOException;
 import java.net.URL;
 
 public class FXFilmToolBar extends ToolBar {
-    @FXML
-    public Button btnDownloadFilmList;
+  @FXML public Button btnDownloadFilmList;
 
-    @FXML
-    public Button btnFilmInfo;
+  @FXML public Button btnFilmInfo;
 
-    @FXML
-    Button btnPlay;
+  @FXML Button btnPlay;
 
-    @FXML
-    Button btnRecord;
+  @FXML Button btnRecord;
 
-    @FXML
-    Button btnBookmark;
-    
-    @FXML
-    Button btnManageAbos;
-    
-    @FXML
-    ToggleButton btnShowBookmarkedMovies;
-    
-    @FXML
-    Button btnManageBookMarks;
+  @FXML Button btnBookmark;
 
-    @FXML
-    Button btnShowFilter;
+  @FXML Button btnManageAbos;
 
-    @FXML
-    JFXSearchPanel jfxSearchField;
+  @FXML ToggleButton btnShowBookmarkedMovies;
 
-    @FXML
-    ToggleButton btnSearchThroughDescription;
+  @FXML Button btnManageBookMarks;
 
-    public FXFilmToolBar() {
-        super();
-        try {
-            URL url = getClass().getResource("/mediathek/res/programm/fxml/film_toolbar.fxml");
-            FXMLLoader fxmlLoader = new FXMLLoader(url);
-            fxmlLoader.setRoot(this);
-            fxmlLoader.setController(this);
-            fxmlLoader.load();
-        } catch (IOException e) {
-            Logger logger = LogManager.getLogger(FXFilmToolBar.class);
-            logger.error("Failed to load FXML!");
-        }
+  @FXML Button btnShowFilter;
+
+  @FXML JFXSearchPanel jfxSearchField;
+
+  @FXML ToggleButton btnSearchThroughDescription;
+
+  @FXML ComboBox<FilterDTO> filterSelect;
+
+  public FXFilmToolBar() {
+    super();
+    try {
+      URL url = getClass().getResource("/mediathek/res/programm/fxml/film_toolbar.fxml");
+      FXMLLoader fxmlLoader = new FXMLLoader(url);
+      fxmlLoader.setRoot(this);
+      fxmlLoader.setController(this);
+      fxmlLoader.load();
+      setUpFilterSelect();
+    } catch (IOException e) {
+      Logger logger = LogManager.getLogger(FXFilmToolBar.class);
+      logger.error("Failed to load FXML!");
     }
+  }
+
+  private void setUpFilterSelect() {
+    FilterConfiguration filterConfig = new FilterConfiguration();
+    ObservableList<FilterDTO> availableFilters =
+        FXCollections.observableArrayList(filterConfig.getAvailableFilters());
+    FilterConfiguration.addAvailableFiltersObserver(
+        () -> {
+          availableFilters.clear();
+          availableFilters.addAll(filterConfig.getAvailableFilters());
+        });
+
+    SingleSelectionModel<FilterDTO> selectionModel = filterSelect.getSelectionModel();
+    FilterConfiguration.addCurrentFiltersObserver(selectionModel::select);
+    filterSelect.setItems(availableFilters);
+    selectionModel.select(filterConfig.getCurrentFilter());
+    selectionModel
+        .selectedItemProperty()
+        .addListener(
+            (observableValue, oldValue, newValue) -> {
+              if (newValue != null && !newValue.equals(filterConfig.getCurrentFilter())) {
+                filterConfig.setCurrentFilter(newValue);
+              }
+            });
+  }
 }
