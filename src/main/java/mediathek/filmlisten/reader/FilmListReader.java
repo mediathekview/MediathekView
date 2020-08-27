cbfd30ec0f31cd51fd5dac96942c71860cba5103
Christian F <crystalpalace1977@googlemail.com>
2020-08-27 08:16:49 +0200

    - fixes #536 - restructure code

--- src/main/java/mediathek/filmlisten/reader/FilmListReader.java
+++ src/main/java/mediathek/filmlisten/reader/FilmListReader.java
@@ -243,7 +243,8 @@ public class FilmListReader implements AutoCloseable {
     private void parseAudioVersion(String title, DatenFilm film) {
         if (title.contains("Hörfassung") || title.contains("Audiodeskription")
                 || title.contains("AD |") || title.endsWith("(AD)")
-                || title.contains("Hörspiel") || title.contains("Hörfilm"))
+                || title.contains("Hörspiel") || title.contains("Hörfilm")
+                || title.contains("mit gesprochenen Untertiteln"))
             film.setAudioVersion(true);
     }
 
