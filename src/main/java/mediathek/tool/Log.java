74fba3427eca3275e81a722295b35cf46112520a
derreisende77 <derreisende77@users.noreply.github.com>
2020-10-26 14:05:59 +0100

    - refactor deprecated logging

--- src/main/java/mediathek/tool/Log.java
+++ src/main/java/mediathek/tool/Log.java
@@ -1,12 +1,8 @@
 package mediathek.tool;
 
-import mediathek.config.Config;
-import mediathek.config.Konstanten;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -15,7 +11,6 @@ public class Log {
 
     public final static String LINE = "################################################################################";
     public static final Instant startZeit = Instant.now();
-    private final static String FEHLER = "Fehler(" + Konstanten.PROGRAMMNAME + "): ";
     private static final ArrayList<String> logList = new ArrayList<>();
     private static final Logger logger = LogManager.getLogger();
     private static boolean progress;
@@ -38,32 +33,6 @@ public class Log {
     }
 
     // Fehlermeldung mit Exceptions
-    @Deprecated
-    public static synchronized void errorLog(int fehlerNummer, Exception ex) {
-        fehlermeldung_(fehlerNummer, ex, new String[]{});
-    }
-
-    @Deprecated
-    public static synchronized void errorLog(int fehlerNummer, Exception ex, String text) {
-        fehlermeldung_(fehlerNummer, ex, new String[]{text});
-    }
-
-    @Deprecated
-    public static synchronized void errorLog(int fehlerNummer, Exception ex, String[] text) {
-        fehlermeldung_(fehlerNummer, ex, text);
-    }
-
-    // Fehlermeldungen
-    @Deprecated
-    public static synchronized void errorLog(int fehlerNummer, String text) {
-        fehlermeldung_(fehlerNummer, null, new String[]{text});
-    }
-
-    @Deprecated
-    public static synchronized void errorLog(int fehlerNummer, String[] text) {
-        fehlermeldung_(fehlerNummer, null, text);
-    }
-
     public static synchronized void sysLog(String text) {
         systemmeldung_(new String[]{text});
     }
@@ -80,63 +49,6 @@ public class Log {
         if (progress) {
             System.out.print("                                                                                                             \r");
             progress = false;
-        }
-    }
-
-    private static void fehlermeldung_(int fehlerNummer, Exception ex, String[] texte) {
-        final Throwable t = new Throwable();
-        final StackTraceElement methodCaller = t.getStackTrace()[2];
-        final String klasse = methodCaller.getClassName() + '.' + methodCaller.getMethodName();
-        String kl;
-        try {
-            kl = klasse;
-            while (kl.contains(".")) {
-                if (Character.isUpperCase(kl.charAt(0))) {
-                    break;
-                } else {
-                    kl = kl.substring(kl.indexOf('.') + 1);
-                }
-            }
-        } catch (Exception ignored) {
-            kl = klasse;
-        }
-
-        if (ex != null || Config.isDebugModeEnabled()) {
-            // Exceptions immer ausgeben
-            resetProgress();
-            String x, z;
-            if (ex != null) {
-                x = "!";
-            } else {
-                x = "=";
-            }
-            z = "*";
-            logList.add(x + x + x + x + x + x + x + x + x + x
-                    + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x + x);
-
-            try {
-                // Stacktrace
-                try (StringWriter sw = new StringWriter(); PrintWriter pw = new PrintWriter(sw)) {
-                    if (ex != null) {
-                        ex.printStackTrace(pw);
-                    }
-                    pw.flush();
-                    sw.flush();
-                    logList.add(sw.toString());
-                }
-            } catch (Exception ignored) {
-            }
-
-            logList.add(z + " Fehlernr: " + fehlerNummer);
-            if (ex != null) {
-                logList.add(z + " Exception: " + ex.getMessage());
-            }
-            logList.add(z + ' ' + FEHLER + kl);
-            for (String aTexte : texte) {
-                logList.add(z + "           " + aTexte);
-            }
-            logList.add("");
-            printLog();
         }
     }
 
