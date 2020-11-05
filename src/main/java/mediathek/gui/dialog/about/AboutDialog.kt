5d3f1e1c4e09d5f91ccf5b3f1b09ff842363a73b
Christian F <derreisende77@users.noreply.github.com>
2020-11-05 12:03:07 +0100

    - Yeah, finally no more Jide in here!
    Former-commit-id: 254e2c1d2d2c5c8687f2208af7676f3cdefd88c9

--- src/main/java/mediathek/gui/dialog/about/AboutDialog.kt
+++ src/main/java/mediathek/gui/dialog/about/AboutDialog.kt
@@ -1,31 +1,16 @@
-package mediathek.gui.dialog.about;
+package mediathek.gui.dialog.about
 
-import com.jidesoft.dialog.ButtonPanel;
-import com.jidesoft.dialog.StandardDialog;
-import mediathek.tool.EscapeKeyHandler;
+import mediathek.tool.EscapeKeyHandler
+import java.awt.BorderLayout
+import java.awt.Frame
+import javax.swing.JDialog
 
-import javax.swing.*;
-import java.awt.*;
-
-public class AboutDialog extends StandardDialog {
-    public AboutDialog(Frame owner) {
-        super(owner,"Über dieses Programm", true);
-        EscapeKeyHandler.installHandler(this, this::dispose);
-        pack();
-    }
-
-    @Override
-    public JComponent createBannerPanel() {
-        return null;
-    }
-
-    @Override
-    public JComponent createContentPanel() {
-        return new AboutDialogPanel();
-    }
-
-    @Override
-    public ButtonPanel createButtonPanel() {
-        return null;
+class AboutDialog(owner: Frame?) : JDialog(owner, "Über dieses Programm", true) {
+    init {
+        defaultCloseOperation = DISPOSE_ON_CLOSE
+        EscapeKeyHandler.installHandler(this) { dispose() }
+        contentPane.layout = BorderLayout()
+        contentPane.add(AboutDialogPanel(), BorderLayout.CENTER)
+        pack()
     }
 }
