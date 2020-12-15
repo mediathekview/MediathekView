c80421d9e6be3b5c606b336e53c75670bb60366f
Christian F <crystalpalace1977@googlemail.com>
2020-12-15 19:53:03 +0100

    - don´t run helper everytime we query for touchbar
    Former-commit-id: 08dbb98aa24b57040ca0c5b01faed955e46d7366

--- src/main/java/mediathek/mac/touchbar/TouchBarUtils.java
+++ src/main/java/mediathek/mac/touchbar/TouchBarUtils.java
@@ -17,6 +17,11 @@ import java.util.concurrent.TimeUnit;
 public class TouchBarUtils {
     private static final float TOUCHBAR_BUTTON_SIZE = 64.0f;
     private static final Logger logger = LogManager.getLogger();
+    private static boolean TOUCHBAR_SUPPORTED;
+
+    static {
+        checkTouchBarSupport();
+    }
 
     public static byte[] getImgBytes(@NotNull BufferedImage image) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -53,18 +58,25 @@ public class TouchBarUtils {
         return image;
     }
 
-    public static boolean isTouchBarSupported() {
-        boolean supported = false;
+    /**
+     * Check if touch bar support is available in macOS.
+     * Needs to use external helper for verification.
+     */
+    private static void checkTouchBarSupport() {
+        logger.trace("checkTouchBarSupport");
+        TOUCHBAR_SUPPORTED = false;
         try {
             var process = Runtime.getRuntime().exec("bin/mv_touchbar_support");
             if (process.waitFor(2, TimeUnit.SECONDS)) {
                 // no timeout
-                supported = process.exitValue() == 0;
+                TOUCHBAR_SUPPORTED = process.exitValue() == 0;
             }
         } catch (Exception e) {
             logger.error("Failed to check touchbar support", e);
         }
+    }
 
-        return supported;
+    public static boolean isTouchBarSupported() {
+        return TOUCHBAR_SUPPORTED;
     }
 }
