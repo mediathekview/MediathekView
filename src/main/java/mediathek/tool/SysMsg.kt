d0e1af39327ecf70c0a058fffcdf3e82eecbe61a
Christian F <crystalpalace1977@googlemail.com>
2021-01-05 20:02:37 +0100

    -cleanup and remove unused code

--- src/main/java/mediathek/tool/SysMsg.kt
+++ src/main/java/mediathek/tool/SysMsg.kt
@@ -1,46 +1,17 @@
 package mediathek.tool
 
-import javafx.collections.FXCollections
-
+/**
+ * Leftover only used in RuntimeExec class.
+ */
 object SysMsg {
-    private val outputList = FXCollections.observableArrayList<String>()
-    private const val MAX_STELLEN = 5
-    private const val FUELL_ZEICHEN = "0"
-    private var zeilenNrProgramm = 0
-
-    @Synchronized
     @JvmStatic
     fun playerMsg(text: String) {
         playermeldung(arrayOf(text))
     }
 
     private fun playermeldung(texte: Array<String>) {
-        val z = "  >>"
-        println(z + " " + texte[0])
-        notify(texte[0])
-        for (i in 1 until texte.size) {
-            println(z + " " + texte[i])
-            notify(texte[i])
-        }
-    }
-
-    private fun notify(zeile: String) {
-        addText("[" + getNr(zeilenNrProgramm++) + "]   " + zeile)
-    }
-
-    private fun getNr(nr: Int): String {
-        var str = nr.toString()
-        while (str.length < MAX_STELLEN) {
-            str = FUELL_ZEICHEN + str
-        }
-        return str
-    }
-
-    @Synchronized
-    private fun addText(texte: String) {
-        if (outputList.size > 50000) {
-            outputList.remove(0, 30000)
-        }
-        outputList.add(texte + System.lineSeparator())
+        println("  >> ${texte[0]}")
+        for (i in 1 until texte.size)
+            println("  >> ${texte[i]}")
     }
 }
