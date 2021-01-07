194b0cfcdde65a2ece64dad25822562c2b5d0832
Christian F <crystalpalace1977@googlemail.com>
2021-01-07 13:11:35 +0100

    - cleanup
    Former-commit-id: cbc6fcafea258715ed86b5f964e1ce1fe212f690

--- src/main/java/mediathek/gui/abo/AboInformationController.kt
+++ src/main/java/mediathek/gui/abo/AboInformationController.kt
@@ -20,35 +20,17 @@ class AboInformationController : Initializable {
     @FXML
     private lateinit var inactiveAbos: Label
 
-    private var oldSize = -1
-    private var oldActive: Long = -1
-    private var oldInactive: Long = -1
-
-    private fun updateTotalDisplay(gesamt: Int) {
-        if (gesamt == 1) {
-            totalAbos.text = "Gesamt: 1 Abo"
-        } else {
-            totalAbos.text = String.format("Gesamt: %d Abos", gesamt)
-        }
-    }
-
     private fun updateDisplayText() {
         val listeAbo = Daten.getInstance().listeAbo
-        val numActiveAbos = listeAbo.activeAbos()
-        val numInactiveAbos = listeAbo.inactiveAbos()
-        val gesamt = listeAbo.size
-        if (gesamt != oldSize) {
-            updateTotalDisplay(gesamt)
-            oldSize = gesamt
+        val numAbos = listeAbo.size
+
+        if (numAbos == 1) {
+            totalAbos.text = "Gesamt: 1 Abo"
+        } else {
+            totalAbos.text = "Gesamt: $numAbos Abos"
         }
-        if (numActiveAbos != oldActive) {
-            activeAbos.text = String.format("%d eingeschaltet", numActiveAbos)
-            oldActive = numActiveAbos
-        }
-        if (numInactiveAbos != oldInactive) {
-            inactiveAbos.text = String.format("%d ausgeschaltet", numInactiveAbos)
-            oldInactive = numInactiveAbos
-        }
+        activeAbos.text = "${listeAbo.activeAbos()} eingeschaltet"
+        inactiveAbos.text = "${listeAbo.inactiveAbos()} ausgeschaltet"
     }
 
     @Handler
