fe0d93a6b65cdf910a547d4c67e02f2f4a6018e7
Christian F <crystalpalace1977@googlemail.com>
2021-01-07 08:11:26 +0100

    - convert to kotlin
    Former-commit-id: de2ecd7bbf7bd55be550418b5d8b4cb6e88e5d8d

--- src/main/java/mediathek/gui/abo/FXAboToolBar.kt
+++ src/main/java/mediathek/gui/abo/FXAboToolBar.kt
@@ -1,55 +1,51 @@
-package mediathek.gui.abo;
+package mediathek.gui.abo
 
-import ca.odell.glazedlists.javafx.EventObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ToolBar;
-import mediathek.config.Daten;
-import mediathek.tool.SenderList;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import ca.odell.glazedlists.javafx.EventObservableList
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.fxml.Initializable
+import javafx.scene.control.Button
+import javafx.scene.control.ComboBox
+import javafx.scene.control.ToolBar
+import mediathek.config.Daten
+import mediathek.tool.SenderList
+import org.apache.logging.log4j.LogManager
+import java.io.IOException
+import java.net.URL
+import java.util.*
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+class FXAboToolBar : ToolBar(), Initializable {
+    @FXML lateinit var cbSender: ComboBox<String>
 
+    @FXML lateinit var btnOn: Button
 
-public class FXAboToolBar extends ToolBar implements Initializable {
-    private static final Logger logger = LogManager.getLogger(FXAboToolBar.class);
-    @FXML
-    public ComboBox<String> cbSender;
-    @FXML
-    public Button btnOn;
-    @FXML
-    public Button btnOff;
-    @FXML
-    public Button btnDelete;
-    @FXML
-    public Button btnEdit;
-    @FXML
-    public Button btnNewAbo;
+    @FXML lateinit var btnOff: Button
 
-    public FXAboToolBar() {
-        super();
+    @FXML lateinit var btnDelete: Button
 
-        try {
-            URL url = getClass().getResource("/mediathek/res/programm/fxml/abo/abo_toolbar.fxml");
-            FXMLLoader fxmlLoader = new FXMLLoader(url);
-            fxmlLoader.setRoot(this);
-            fxmlLoader.setController(this);
-            fxmlLoader.load();
-        } catch (IOException e) {
-            logger.error("Failed to load FXML!", e);
-        }
+    @FXML lateinit var btnEdit: Button
+
+    @FXML lateinit var btnNewAbo: Button
+
+    override fun initialize(url: URL?, resourceBundle: ResourceBundle?) {
+        val senderList = SenderList(Daten.getInstance().listeFilme.baseSenderList)
+        cbSender.items = EventObservableList(senderList)
+        cbSender.selectionModel.select(0)
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        var senderList = new SenderList(Daten.getInstance().getListeFilme().getBaseSenderList());
-        cbSender.setItems(new EventObservableList<>(senderList));
-        cbSender.getSelectionModel().select(0);
+    companion object {
+        private val logger = LogManager.getLogger()
+    }
+
+    init {
+        try {
+            val url = FXAboToolBar::class.java.getResource("/mediathek/res/programm/fxml/abo/abo_toolbar.fxml")
+            val fxmlLoader = FXMLLoader(url)
+            fxmlLoader.setRoot(this)
+            fxmlLoader.setController(this)
+            fxmlLoader.load()
+        } catch (e: IOException) {
+            logger.error("Failed to load FXML!", e)
+        }
     }
 }
