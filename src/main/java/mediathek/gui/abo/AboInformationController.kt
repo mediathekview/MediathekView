367eae857698a887b64e7f2816fa39409ab83129
Christian F <crystalpalace1977@googlemail.com>
2021-01-07 12:50:04 +0100

    - convert to kotlin - dont use timer but corresponding update event for abo

--- src/main/java/mediathek/gui/abo/AboInformationController.kt
+++ src/main/java/mediathek/gui/abo/AboInformationController.kt
@@ -1,65 +1,63 @@
-package mediathek.gui.abo;
+package mediathek.gui.abo
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import mediathek.config.Daten;
-import mediathek.gui.messages.TimerEvent;
-import mediathek.javafx.tool.JavaFxUtils;
-import net.engio.mbassy.listener.Handler;
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.Label
+import mediathek.config.Daten
+import mediathek.gui.messages.AboListChangedEvent
+import mediathek.javafx.tool.JavaFxUtils
+import net.engio.mbassy.listener.Handler
+import java.net.URL
+import java.util.*
 
-public class AboInformationController {
+class AboInformationController: Initializable {
     @FXML
-    private Label totalAbos;
+    private lateinit var totalAbos: Label
 
     @FXML
-    private Label activeAbos;
+    private lateinit var activeAbos: Label
 
     @FXML
-    private Label inactiveAbos;
+    private lateinit var inactiveAbos: Label
 
-    private int oldSize = -1;
-    private long oldActive = -1;
-    private long oldInactive = -1;
-    private Daten daten;
+    private var oldSize = -1
+    private var oldActive: Long = -1
+    private var oldInactive: Long = -1
 
-    public void startListener() {
-        this.daten = Daten.getInstance();
-        daten.getMessageBus().subscribe(this);
-    }
-
-    private void updateTotalDisplay(final int gesamt) {
+    private fun updateTotalDisplay(gesamt: Int) {
         if (gesamt == 1) {
-            totalAbos.setText("Gesamt: 1 Abo");
+            totalAbos.text = "Gesamt: 1 Abo"
         } else {
-            totalAbos.setText(String.format("Gesamt: %d Abos", gesamt));
+            totalAbos.text = String.format("Gesamt: %d Abos", gesamt)
         }
     }
 
-    private void updateDisplayText() {
-        final var listeAbo = daten.getListeAbo();
-        final long _activeAbos = listeAbo.activeAbos();
-        final long _inactiveAbos = listeAbo.inactiveAbos();
-        final int gesamt = listeAbo.size();
-
+    private fun updateDisplayText() {
+        val listeAbo = Daten.getInstance().listeAbo
+        val _activeAbos = listeAbo.activeAbos()
+        val _inactiveAbos = listeAbo.inactiveAbos()
+        val gesamt = listeAbo.size
         if (gesamt != oldSize) {
-            updateTotalDisplay(gesamt);
-
-            oldSize = gesamt;
+            updateTotalDisplay(gesamt)
+            oldSize = gesamt
         }
-
         if (_activeAbos != oldActive) {
-            activeAbos.setText(String.format("%d eingeschaltet", _activeAbos));
-            oldActive = _activeAbos;
+            activeAbos.text = String.format("%d eingeschaltet", _activeAbos)
+            oldActive = _activeAbos
         }
-
         if (_inactiveAbos != oldInactive) {
-            inactiveAbos.setText(String.format("%d ausgeschaltet", _inactiveAbos));
-            oldInactive = _inactiveAbos;
+            inactiveAbos.text = String.format("%d ausgeschaltet", _inactiveAbos)
+            oldInactive = _inactiveAbos
         }
     }
 
     @Handler
-    private void handleTimerEvent(TimerEvent e) {
-        JavaFxUtils.invokeInFxThreadAndWait(this::updateDisplayText);
+    private fun handleAboChangedEvent(e: AboListChangedEvent) {
+        JavaFxUtils.invokeInFxThreadAndWait { updateDisplayText() }
+    }
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        Daten.getInstance().messageBus.subscribe(this)
+        updateDisplayText()
     }
 }
