543452cd99e69989d3bf9dcf348fcb40990b2a73
Christian F <crystalpalace1977@googlemail.com>
2021-02-17 08:54:33 +0100

    - add missing search providers for #594

--- src/main/java/mediathek/gui/tabs/tab_film/OnlineSearchProviders.kt
+++ src/main/java/mediathek/gui/tabs/tab_film/OnlineSearchProviders.kt
@@ -5,7 +5,10 @@ enum class OnlineSearchProviders(private val displayName: String, val queryUrl: 
     BING("Bing", "https://www.bing.com/search?q="),
     DUCKDUCKGO("DuckDuckGo", "https://duckduckgo.com/?q="),
     GOOGLE("Google", "https://www.google.de/search?q="),
+    IMDB("IMDB","https://www.imdb.com/find?q="),
     STARTPAGE("Startpage", "https://www.startpage.com/do/search?q="),
+    MOVIEDB("The Movie Database", "https://www.themoviedb.org/search?language=de-DE&query="),
+    TVDB("TheTVDB", "https://thetvdb.com/search?query="),
     YOUTUBE("YouTube", "https://www.youtube.com/results?search_query=");
 
     override fun toString(): String {
