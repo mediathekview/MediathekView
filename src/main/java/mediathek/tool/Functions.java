0cd133882fb7cb693b49e10de18eba026a0a95e9
Christian F <crystalpalace1977@googlemail.com>
2021-09-19 11:28:36 +0200

    - code refactor

--- src/main/java/mediathek/tool/Functions.java
+++ src/main/java/mediathek/tool/Functions.java
@@ -59,18 +59,4 @@ public class Functions {
         return getOs().toString();
     }
 
-    public enum OperatingSystemType {
-
-        UNKNOWN(""), WIN32("Windows"), WIN64("Windows"), LINUX("Linux"), MAC("Mac");
-        private final String name;
-
-        OperatingSystemType(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
 }
