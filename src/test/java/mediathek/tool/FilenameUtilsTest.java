504fae2ca22b62572dfb7613db6187d8c681c73c
Christian F <crystalpalace1977@googlemail.com>
2021-12-03 11:58:57 +0100

    - use library function instead of own code - cover with test
    Former-commit-id: bc9b48b1f6857b30697b5cbc4f234746262936bd

--- src/test/java/mediathek/tool/FilenameUtilsTest.java
+++ src/test/java/mediathek/tool/FilenameUtilsTest.java
@@ -1,5 +1,6 @@
 package mediathek.tool;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,21 +9,29 @@ class FilenameUtilsTest {
     void trailing_test_with_leading_whitespace() {
         var testStr = " betrifft: ... ";
         var result = FilenameUtils.removeWindowsTrailingDots(testStr);
-        Assertions.assertEquals(result, " betrifft:");
+        Assertions.assertEquals(" betrifft:", result);
     }
 
     @Test
     void trailing_test_without_leading_whitespace() {
         var testStr = "betrifft: ...";
         var result = FilenameUtils.removeWindowsTrailingDots(testStr);
-        Assertions.assertEquals(result, "betrifft:");
+        Assertions.assertEquals("betrifft:", result);
 
     }
 
     @Test
     void test_remove_starting_dots() {
         var testStr = "....Paula";
-        var result = FilenameUtils.removeStartingDots(testStr);
-        Assertions.assertEquals(result, "Paula");
+        var result = StringUtils.stripStart(testStr, ".");
+        Assertions.assertEquals("Paula", result);
+    }
+
+    @Test
+    void test_remove_starting_dots_with_leading_whitespace() {
+        // this should not modify string as we have whitespace at beginning
+        var testStr = " ....Paula";
+        var result = StringUtils.stripStart(testStr, ".");
+        Assertions.assertEquals(testStr, result);
     }
 }
