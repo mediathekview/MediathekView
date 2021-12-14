5a160255da696aee954636a06aa17074472ee0b1
Christian F <crystalpalace1977@googlemail.com>
2021-12-14 22:01:41 +0100

    - fix warning
    Former-commit-id: f325dca015741f5f49dc04a9f19311518ad5c278

--- src/main/java/mediathek/tool/sender_icon_cache/SenderIconCacheLoader.java
+++ src/main/java/mediathek/tool/sender_icon_cache/SenderIconCacheLoader.java
@@ -83,7 +83,7 @@ class SenderIconCacheLoader extends CacheLoader<String, Optional<ImageIcon>> {
     }
 
     @Override
-    public Optional<ImageIcon> load(@NotNull String sender) {
+    public @NotNull Optional<ImageIcon> load(@NotNull String sender) {
         ImageIcon icon = switch (sender) {
             case "3Sat" -> getIcon(WIKI_BASE_URL + "/f/f2/3sat-Logo.svg/775px-3sat-Logo.svg.png", "/mediathek/res/sender/3sat.png");
             case "ARD", "ARD.Podcast" -> getIcon(WIKI_BASE_URL + "/6/68/ARD_logo.svg/320px-ARD_logo.svg.png", "/mediathek/res/sender/ard.png");
