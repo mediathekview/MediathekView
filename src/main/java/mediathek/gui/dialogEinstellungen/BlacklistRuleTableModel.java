e07566b6a429308f095a0418459b0925c1e15651
Christian F <crystalpalace1977@googlemail.com>
2021-12-16 14:10:20 +0100

    fix proper removal of multiple rules

--- src/main/java/mediathek/gui/dialogEinstellungen/BlacklistRuleTableModel.java
+++ src/main/java/mediathek/gui/dialogEinstellungen/BlacklistRuleTableModel.java
@@ -5,6 +5,7 @@ import mediathek.daten.blacklist.BlacklistRule;
 import mediathek.daten.blacklist.ListeBlacklist;
 
 import javax.swing.table.AbstractTableModel;
+import java.util.List;
 
 public class BlacklistRuleTableModel extends AbstractTableModel {
     private final ListeBlacklist blacklist;
@@ -32,7 +33,7 @@ public class BlacklistRuleTableModel extends AbstractTableModel {
             case BlacklistRule.BLACKLIST_THEMA -> rule.arr[BlacklistRule.BLACKLIST_THEMA];
             case BlacklistRule.BLACKLIST_TITEL -> rule.arr[BlacklistRule.BLACKLIST_TITEL];
             case BlacklistRule.BLACKLIST_THEMA_TITEL -> rule.arr[BlacklistRule.BLACKLIST_THEMA_TITEL];
-            //case 5 -> rule;
+            case 5 -> rule;
             default -> throw new IllegalStateException("Unexpected value: " + columnIndex);
         };
     }
@@ -52,5 +53,24 @@ public class BlacklistRuleTableModel extends AbstractTableModel {
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
         return false;
+    }
+
+    /**
+     * Remove a BlacklistRule from model
+     * @param modelIndex index from blacklist to delete
+     */
+    public void removeRow(int modelIndex) {
+        blacklist.remove(modelIndex);
+        fireTableRowsDeleted(modelIndex,modelIndex);
+    }
+
+    /**
+     * Remove a collection of rules.
+     * Fire update after all rules have been removed.
+     * @param list of objects to be deleted
+     */
+    public void removeRows(List<BlacklistRule> list) {
+        blacklist.remove(list);
+        fireTableDataChanged();
     }
 }
