5c90cb6b4534cb1051373992af81ed5b9591e5d2
Christian F <crystalpalace1977@googlemail.com>
2021-12-27 21:48:23 +0100

    - convert to JFormDesigner
    Former-commit-id: cef5cb25faff791739f9d62de6df8103442366aa

--- src/main/java/mediathek/gui/dialog/DialogStarteinstellungen.java
+++ src/main/java/mediathek/gui/dialog/DialogStarteinstellungen.java
@@ -11,6 +11,7 @@ import mediathek.gui.dialogEinstellungen.PanelPsetLang;
 import mediathek.tool.GuiFunktionenProgramme;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -156,91 +157,107 @@ public class DialogStarteinstellungen extends JDialog {
     }
 
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // Generated using JFormDesigner non-commercial license
     private void initComponents() {
+        var jPanel2 = new JPanel();
+        jButtonStandard = new JButton();
+        jCheckBoxAlleEinstellungen = new JCheckBox();
+        jButtonAnpassen = new JButton();
+        jScrollPane1 = new JScrollPane();
+        jPanelExtra = new JPanel();
 
-        javax.swing.JPanel jPanel2 = new javax.swing.JPanel();
-        jButtonStandard = new javax.swing.JButton();
-        jCheckBoxAlleEinstellungen = new javax.swing.JCheckBox();
-        jButtonAnpassen = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jPanelExtra = new javax.swing.JPanel();
+        //======== this ========
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Erster Start"); //NON-NLS
+        var contentPane = getContentPane();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("Erster Start");
+        //======== jPanel2 ========
+        {
+            jPanel2.setBorder(new LineBorder(new Color(153, 153, 255), 3));
 
-        jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(153, 153, 255), 3));
+            //---- jButtonStandard ----
+            jButtonStandard.setText("Mit Standardeinstellungen starten"); //NON-NLS
 
-        jButtonStandard.setText("Mit Standardeinstellungen starten");
+            //---- jCheckBoxAlleEinstellungen ----
+            jCheckBoxAlleEinstellungen.setText("alle Einstellungen anzeigen"); //NON-NLS
 
-        jCheckBoxAlleEinstellungen.setText("alle Einstellungen anzeigen");
+            //---- jButtonAnpassen ----
+            jButtonAnpassen.setText("Einstellungen anpassen"); //NON-NLS
 
-        jButtonAnpassen.setText("Einstellungen anpassen");
+            GroupLayout jPanel2Layout = new GroupLayout(jPanel2);
+            jPanel2.setLayout(jPanel2Layout);
+            jPanel2Layout.setHorizontalGroup(
+                jPanel2Layout.createParallelGroup()
+                    .addGroup(GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jCheckBoxAlleEinstellungen)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButtonAnpassen)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonStandard)
+                        .addContainerGap())
+            );
+            jPanel2Layout.setVerticalGroup(
+                jPanel2Layout.createParallelGroup()
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel2Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButtonStandard)
+                            .addComponent(jCheckBoxAlleEinstellungen)
+                            .addComponent(jButtonAnpassen))
+                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            );
+        }
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jCheckBoxAlleEinstellungen)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButtonAnpassen)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButtonStandard)
-                .addContainerGap())
+        //======== jScrollPane1 ========
+        {
+
+            //======== jPanelExtra ========
+            {
+
+                GroupLayout jPanelExtraLayout = new GroupLayout(jPanelExtra);
+                jPanelExtra.setLayout(jPanelExtraLayout);
+                jPanelExtraLayout.setHorizontalGroup(
+                    jPanelExtraLayout.createParallelGroup()
+                        .addGap(0, 788, Short.MAX_VALUE)
+                );
+                jPanelExtraLayout.setVerticalGroup(
+                    jPanelExtraLayout.createParallelGroup()
+                        .addGap(0, 510, Short.MAX_VALUE)
+                );
+            }
+            jScrollPane1.setViewportView(jPanelExtra);
+        }
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addGroup(contentPaneLayout.createParallelGroup()
+                        .addComponent(jScrollPane1, GroupLayout.DEFAULT_SIZE, 791, Short.MAX_VALUE)
+                        .addComponent(jPanel2, GroupLayout.Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addContainerGap())
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonStandard)
-                    .addComponent(jCheckBoxAlleEinstellungen)
-                    .addComponent(jButtonAnpassen))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGroup(contentPaneLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jScrollPane1)
+                    .addGap(18, 18, 18)
+                    .addComponent(jPanel2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap())
         );
-
-        javax.swing.GroupLayout jPanelExtraLayout = new javax.swing.GroupLayout(jPanelExtra);
-        jPanelExtra.setLayout(jPanelExtraLayout);
-        jPanelExtraLayout.setHorizontalGroup(
-            jPanelExtraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 788, Short.MAX_VALUE)
-        );
-        jPanelExtraLayout.setVerticalGroup(
-            jPanelExtraLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 510, Short.MAX_VALUE)
-        );
-
-        jScrollPane1.setViewportView(jPanelExtra);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 791, Short.MAX_VALUE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1)
-                .addGap(18, 18, 18)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-
         pack();
+        setLocationRelativeTo(getOwner());
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonAnpassen;
-    private javax.swing.JButton jButtonStandard;
-    private javax.swing.JCheckBox jCheckBoxAlleEinstellungen;
-    private javax.swing.JPanel jPanelExtra;
-    private javax.swing.JScrollPane jScrollPane1;
+    // Generated using JFormDesigner non-commercial license
+    private JButton jButtonStandard;
+    private JCheckBox jCheckBoxAlleEinstellungen;
+    private JButton jButtonAnpassen;
+    private JScrollPane jScrollPane1;
+    private JPanel jPanelExtra;
     // End of variables declaration//GEN-END:variables
 }
