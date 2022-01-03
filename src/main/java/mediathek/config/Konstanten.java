055b4f789cc189fa7aa6ec5f448a8409f07978d7
Christian F <crystalpalace1977@googlemail.com>
2022-01-03 09:53:46 +0100

    - add nightly indicator to builds. Note that Version.kt needs to be manually recompiled as it does not detect changes correctly in IntellIJ IDE
    Former-commit-id: 31b0afbc765c15be36d6ac55ddb9aa6c1fc0668e

--- src/main/java/mediathek/config/Konstanten.java
+++ src/main/java/mediathek/config/Konstanten.java
@@ -29,6 +29,10 @@ import java.util.concurrent.TimeUnit;
 public class Konstanten {
     public static final long MINIMUM_MEMORY_THRESHOLD = 768 * FileUtils.ONE_MB;
     public static final Version MVVERSION = new Version(13,9,0);
+    /**
+     * Is this a nightly or a production build?
+     */
+    public static final boolean APP_IS_NIGHTLY = true;
     public static final String EXTERNAL_UPDATE_PROPERTY = "externalUpdateCheck";
     public static final String MACOS_OFFICIAL_APP = "OSX_OFFICIAL_APP";
     public static final URL FXML_FILM_DESCRIPTION_PANEL_URL = Konstanten.class.getResource("/mediathek/res/programm/fxml/filmdescription.fxml");
