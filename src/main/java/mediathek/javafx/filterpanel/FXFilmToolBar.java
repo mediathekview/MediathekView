88ab86cf8e9bfb2186c6e021d01894e956c83260
Christian F <crystalpalace1977@googlemail.com>
2022-06-22 13:00:17 +0200

    reformat code
    Former-commit-id: 96ee700c5b00218593196412beb420399650f22b

--- src/main/java/mediathek/javafx/filterpanel/FXFilmToolBar.java
+++ src/main/java/mediathek/javafx/filterpanel/FXFilmToolBar.java
@@ -16,43 +16,42 @@ import java.io.IOException;
 import java.net.URL;
 
 public class FXFilmToolBar extends HBox {
-  @FXML ComboBox<FilterDTO> filterSelect;
+    @FXML
+    ComboBox<FilterDTO> filterSelect;
+    private ObservableList<FilterDTO> availableFilters;
 
-  public FXFilmToolBar() {
-    try {
-      URL url = getClass().getResource("/mediathek/res/programm/fxml/film_toolbar.fxml");
-      FXMLLoader fxmlLoader = new FXMLLoader(url);
-      fxmlLoader.setRoot(this);
-      fxmlLoader.setController(this);
-      fxmlLoader.load();
-      setUpFilterSelect();
-    } catch (IOException e) {
-      Logger logger = LogManager.getLogger(FXFilmToolBar.class);
-      logger.error("Failed to load FXML!");
+    public FXFilmToolBar() {
+        try {
+            URL url = getClass().getResource("/mediathek/res/programm/fxml/film_toolbar.fxml");
+            FXMLLoader fxmlLoader = new FXMLLoader(url);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.setController(this);
+            fxmlLoader.load();
+            setUpFilterSelect();
+        } catch (IOException e) {
+            Logger logger = LogManager.getLogger(FXFilmToolBar.class);
+            logger.error("Failed to load FXML!");
+        }
     }
-  }
 
-  private void setUpFilterSelect() {
-    FilterConfiguration filterConfig = new FilterConfiguration();
-    ObservableList<FilterDTO> availableFilters =
-        FXCollections.observableArrayList(filterConfig.getAvailableFilters());
-    FilterConfiguration.addAvailableFiltersObserver(
-        () -> {
-          availableFilters.clear();
-          availableFilters.addAll(filterConfig.getAvailableFilters());
+    private void setUpFilterSelect() {
+        FilterConfiguration filterConfig = new FilterConfiguration();
+        availableFilters = FXCollections.observableArrayList(filterConfig.getAvailableFilters());
+
+        FilterConfiguration.addAvailableFiltersObserver(() -> {
+            availableFilters.clear();
+            availableFilters.addAll(filterConfig.getAvailableFilters());
         });
 
-    SingleSelectionModel<FilterDTO> selectionModel = filterSelect.getSelectionModel();
-    FilterConfiguration.addCurrentFiltersObserver(selectionModel::select);
-    filterSelect.setItems(availableFilters);
-    selectionModel.select(filterConfig.getCurrentFilter());
-    selectionModel
-        .selectedItemProperty()
-        .addListener(
-            (observableValue, oldValue, newValue) -> {
-              if (newValue != null && !newValue.equals(filterConfig.getCurrentFilter())) {
+        SingleSelectionModel<FilterDTO> selectionModel = filterSelect.getSelectionModel();
+        FilterConfiguration.addCurrentFiltersObserver(selectionModel::select);
+        filterSelect.setItems(availableFilters);
+
+        selectionModel.select(filterConfig.getCurrentFilter());
+        selectionModel.selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (newValue != null && !newValue.equals(filterConfig.getCurrentFilter())) {
                 filterConfig.setCurrentFilter(newValue);
-              }
-            });
-  }
+            }
+        });
+    }
 }
