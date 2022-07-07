f01d4530547c87e648c1e9a378033be6f3f847e2
Christian F <crystalpalace1977@googlemail.com>
2022-07-07 07:00:47 +0200

    - use JVM provided way to open browser - no more custom stuff
    Former-commit-id: 929652a70323d2080ed0987dd7c7ca196838efee

--- src/main/java/mediathek/gui/actions/ShowOnlineHelpAction.java
+++ src/main/java/mediathek/gui/actions/ShowOnlineHelpAction.java
@@ -1,13 +1,15 @@
 package mediathek.gui.actions;
 
-import javafx.application.Platform;
 import mediathek.config.Konstanten;
 import mediathek.mainwindow.MediathekGui;
 import mediathek.tool.SVGIconUtilities;
-import mediathek.tool.javafx.FXErrorDialog;
+import mediathek.tool.SwingErrorDialog;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 public class ShowOnlineHelpAction extends AbstractAction {
@@ -19,13 +21,23 @@ public class ShowOnlineHelpAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            UrlHyperlinkAction.openURL(MediathekGui.ui(),Konstanten.ADRESSE_ONLINE_HELP);
-        } catch (URISyntaxException ex) {
-            Platform.runLater(() -> FXErrorDialog.showErrorDialog("Online-Hilfe",
-                    "Fehler beim Öffnen der Online-Hilfe",
-                    "Es trat ein Fehler beim Öffnen der Online-Hilfe auf.\nSollte dies häufiger auftreten kontaktieren Sie bitte das Entwicklerteam.",
-                    ex));
+        if (Desktop.isDesktopSupported()) {
+            Desktop d = Desktop.getDesktop();
+            if (d.isSupported(Desktop.Action.BROWSE)) {
+                try {
+                    d.browse(new URI(Konstanten.ADRESSE_ONLINE_HELP));
+                } catch (IOException | URISyntaxException ex) {
+                    SwingErrorDialog.showExceptionMessage(MediathekGui.ui(),
+                            "Es trat ein Fehler beim Öffnen der Online-Hilfe auf.\nSollte dies häufiger auftreten kontaktieren Sie bitte das Entwicklerteam.",
+                            ex);
+                }
+            }
+            else {
+                JOptionPane.showMessageDialog(MediathekGui.ui(),
+                        "<html>Ihr Betriebssystem unterstützt das Öffnen des Browsers nicht.<br>" +
+                                "Bitte öffnen Sie <b>" + Konstanten.ADRESSE_ONLINE_HELP + "</b> selbst in Ihrem Browser.</html>",
+                        Konstanten.PROGRAMMNAME, JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 }
