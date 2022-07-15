81a4cb62c18cbd3225eedefa3e5cd2c9c0feacd9
Christian F <crystalpalace1977@googlemail.com>
2022-07-15 13:11:31 +0200

    - fix incorrect Film Test
    Former-commit-id: 5751a90396d8c008febf54b2b9c084d62fb4b408

--- src/test/java/mSearch/daten/DatenFilmTest.java
+++ src/test/java/mSearch/daten/DatenFilmTest.java
@@ -1,37 +1,24 @@
 package mSearch.daten;
 
 import mediathek.daten.DatenFilm;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-import static org.mockito.Mockito.spy;
 
 @ExtendWith(MockitoExtension.class)
 class DatenFilmTest {
 
-  @Spy
-  static DatenFilm testEntity;
-
-  @BeforeAll
-  static void initializeTestEntity() {
-    DatenFilm df = new DatenFilm();
-    testEntity = spy(df);
-
-  }
-
   static Stream<Arguments> filmLengthEdgeCases() {
     return Stream.of(
         arguments("01:21:30", 4890L),
-        arguments((String)null, 0L), // Die Methode fängt alle Exceptions
+        arguments(null, 0L), // Die Methode fängt alle Exceptions
         arguments("01:91:65", 9125L), // Minuten und Sekungen > 59 werden auch verarbeitet!
         arguments("01:31", 0L), // Es müssen immer Stunden:Minuten:Sekunden eingegeben werden
         arguments("1:0:0", 3600L), // Es müssen keine führenden Nullen verwendet werden
@@ -42,12 +29,12 @@ class DatenFilmTest {
   @ParameterizedTest
   @MethodSource("filmLengthEdgeCases")
   void testFilmLengthCalculation(String input, long expected) {
+    DatenFilm df = new DatenFilm();
 
-    testEntity.setDauer(input);
-    testEntity.init();
+    df.setDauer(input);
+    df.init();
 
-    assertThat(expected).isEqualTo(testEntity.getFilmLength());
-
+    Assertions.assertEquals(expected, df.getFilmLength());
   }
 
 }
