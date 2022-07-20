e02f9141a2661ea6777dece3fbe8f5fb7ee5319d
Christian F <crystalpalace1977@googlemail.com>
2022-07-20 12:04:25 +0200

    - convert Info Dialog back to swing

--- src/main/java/mediathek/gui/filmInformation/InfoDialog.kt
+++ src/main/java/mediathek/gui/filmInformation/InfoDialog.kt
@@ -1,62 +1,56 @@
 package mediathek.gui.filmInformation
 
-import javafx.application.Platform
-import javafx.embed.swing.JFXPanel
-import javafx.embed.swing.SwingFXUtils
-import javafx.event.EventHandler
-import javafx.geometry.Pos
-import javafx.scene.Scene
-import javafx.scene.control.*
-import javafx.scene.image.ImageView
+import com.formdev.flatlaf.util.ScaledImageIcon
+import com.jidesoft.swing.MultilineLabel
 import mediathek.daten.DatenFilm
 import mediathek.gui.actions.UrlHyperlinkAction
-import mediathek.javafx.tool.JavaFxUtils
+import mediathek.mainwindow.MediathekGui
 import mediathek.tool.ApplicationConfiguration
 import mediathek.tool.GuiFunktionen
+import mediathek.tool.SwingErrorDialog
 import mediathek.tool.sender_icon_cache.MVSenderIconCache
+import net.miginfocom.layout.AC
 import net.miginfocom.layout.CC
+import net.miginfocom.layout.LC
+import net.miginfocom.swing.MigLayout
 import org.apache.commons.configuration2.sync.LockMode
-import org.tbee.javafx.scene.layout.MigPane
-import java.awt.BorderLayout
+import org.jdesktop.swingx.JXHyperlink
+import java.awt.Desktop
+import java.awt.Dimension
 import java.awt.Point
 import java.awt.Window
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
-import java.awt.image.BufferedImage
+import java.net.URI
 import java.net.URISyntaxException
-import javax.swing.ImageIcon
-import javax.swing.JDialog
-import javax.swing.SwingUtilities
+import javax.swing.*
+
 
 class InfoDialog(parent: Window?) : JDialog(parent) {
     private val config = ApplicationConfiguration.getConfiguration()
     private var currentFilm: DatenFilm? = null
-    private val lblSender = Label()
-    private val lblThema = Label()
-    private val lblTitle = Label()
-    private val lblDate = Label()
-    private val lblUhrzeit = Label()
-    private val lblDuration = Label()
-    private val lblSize = Label()
-    private val cbHq = DisabledCheckBox()
-    private val cbSubtitle = DisabledCheckBox()
-    private val lblGeo = Label()
-    private val lblAbo = Label()
-    private val hyperlink = Hyperlink("Link zur Webseite")
-    private val lblDescription = TextArea()
+    private val lblSender = JLabel()
+    private val lblThema = MultilineLabel()
+    private val lblTitel = MultilineLabel()
+    private val lblDate = JLabel()
+    private val lblUhrzeit = JLabel()
+    private val lblDuration = JLabel()
+    private val lblSize = JLabel()
+    private val lblGeo = JLabel()
+    private val cbHq = SwingDisabledCheckBox()
+    private val cbSubtitle = SwingDisabledCheckBox()
+    private val lblAbo = JLabel()
+    private val hyperlink = SwingHyperlink()
+    private val descScrollPane = JScrollPane()
+    private val lblDescription = JTextArea()
 
-    private fun installContextMenu(component: Label) {
-        val ctMenu = ContextMenu()
-        val mi = MenuItem("Text in die Zwischenablage kopieren")
-        mi.onAction = EventHandler {
-            GuiFunktionen.copyToClipboard(component.text)
+    internal class SwingDisabledCheckBox : JCheckBox() {
+        init {
+            isEnabled = false
         }
-        ctMenu.items.add(mi)
-        component.contextMenu = ctMenu
     }
-
     /**
      * Restore window position from config settings.
      */
@@ -95,61 +89,56 @@ class InfoDialog(parent: Window?) : JDialog(parent) {
     }
 
     private fun clearControls() {
-        Platform.runLater {
-            lblDescription.text = ""
-            lblAbo.text = ""
-            lblGeo.text = ""
-            lblSender.text = ""
-            lblSender.graphic = null
-            lblSize.text = ""
-            lblThema.text = ""
-            lblTitle.text = ""
-            lblDate.text = ""
-            lblUhrzeit.text = ""
-            lblDuration.text = ""
-            cbHq.isSelected = false
-            cbSubtitle.isSelected = false
-            hyperlink.tooltip = null
-            hyperlink.isDisable = true
-        }
+        lblThema.text = ""
+        lblSender.text = ""
+        lblSender.icon = null
+        lblTitel.text = ""
+        lblDate.text = ""
+        lblUhrzeit.text = ""
+        lblDuration.text = ""
+        lblSender.text = ""
+        lblGeo.text = ""
+        cbHq.isSelected = false
+        cbSubtitle.isSelected = false
+        lblAbo.text = ""
+        hyperlink.toolTipText = ""
+        hyperlink.isEnabled = false
+        lblDescription.text = ""
+
+        pack()
     }
 
-    /**
-     * A sender icon with a fixed height of 32 pixel while maintaining aspect ratio.
-     */
-    internal class SenderIcon(b_img: BufferedImage) : ImageView() {
-        init {
-            fitHeight = 32.0
-            isPreserveRatio = true
-            image = SwingFXUtils.toFXImage(b_img, null)
-        }
+    companion object {
+        private val DEFAULT_SENDER_DIMENSION = Dimension(64,64)
     }
-
     private fun updateTextFields() {
         if (currentFilm == null) {
             clearControls()
         } else {
-            Platform.runLater {
-                val desc = currentFilm!!.description.trim { it <= ' ' }
-                lblDescription.text = desc
-                lblDescription.scrollTop = 0.0
-                lblDescription.scrollLeft = 0.0
-                MVSenderIconCache[currentFilm!!.sender].ifPresent { icon: ImageIcon? ->
-                    lblSender.text = ""
-                    lblSender.graphic = SenderIcon(JavaFxUtils.toBufferedImage(icon))
-                }
-                lblGeo.text = currentFilm!!.geo.orElse("")
-                lblSize.text = currentFilm!!.fileSize.toString()
-                lblThema.text = currentFilm!!.thema
-                lblTitle.text = currentFilm!!.title
-                lblDate.text = currentFilm!!.sendeDatum
-                lblUhrzeit.text = currentFilm!!.sendeZeit
-                lblDuration.text = currentFilm!!.filmLengthAsString
-                cbHq.isSelected = currentFilm!!.isHighQuality
-                cbSubtitle.isSelected = currentFilm!!.hasSubtitle()
-                hyperlink.tooltip = Tooltip(currentFilm!!.websiteUrl)
-                hyperlink.isDisable = false
-                lblAbo.text = currentFilm!!.abo?.name
+            MVSenderIconCache[currentFilm!!.sender].ifPresent { icon: ImageIcon? ->
+                lblSender.text = ""
+                val imageDim = Dimension(icon!!.iconWidth, icon.iconHeight)
+                val destDim = GuiFunktionen.calculateFittedDimension(imageDim, DEFAULT_SENDER_DIMENSION)
+                lblSender.icon = ScaledImageIcon(icon, destDim.width, destDim.height)
+            }
+            lblThema.text = currentFilm!!.thema
+            lblTitel.text = currentFilm!!.title
+            lblDate.text = currentFilm!!.sendeDatum
+            lblUhrzeit.text = currentFilm!!.sendeZeit
+            lblDuration.text = currentFilm!!.filmLengthAsString
+            lblSize.text = currentFilm!!.fileSize.toString()
+            lblGeo.text = currentFilm!!.geo.orElse("")
+            cbHq.isSelected = currentFilm!!.isHighQuality
+            cbSubtitle.isSelected = currentFilm!!.hasSubtitle()
+            lblAbo.text = currentFilm!!.abo?.name
+            hyperlink.isEnabled = true
+            hyperlink.toolTipText = currentFilm!!.websiteUrl
+            hyperlink.isClicked = false
+
+            val desc = currentFilm!!.description.trim { it <= ' ' }
+            lblDescription.text = desc
+            SwingUtilities.invokeLater {
+                descScrollPane.verticalScrollBar.value = 0
             }
         }
     }
@@ -161,104 +150,107 @@ class InfoDialog(parent: Window?) : JDialog(parent) {
      */
     fun updateCurrentFilm(film: DatenFilm?) {
         currentFilm = film
-        if (isVisible) updateTextFields()
+        if (isVisible) {
+            updateTextFields()
+            pack()
+        }
+    }
+
+    internal class SwingHyperlink : JXHyperlink() {
+        private fun openUrl(url : String) {
+            try {
+                UrlHyperlinkAction.openURL(null, url)
+            } catch (ex: URISyntaxException) {
+                ex.printStackTrace()
+            }
+        }
+
+        init {
+            this.text = "Link zur Webseite"
+
+            addActionListener {
+                if (toolTipText.isNotEmpty()) {
+                    if (Desktop.isDesktopSupported()) {
+                        val d = Desktop.getDesktop()
+                        if (d.isSupported(Desktop.Action.BROWSE)) {
+                            try {
+                                d.browse(URI(toolTipText))
+                            }
+                            catch (ex: Exception) {
+                                SwingErrorDialog.showExceptionMessage(
+                                    MediathekGui.ui(),
+                                    "Es trat ein Fehler beim Öffnen des Links auf.\nSollte dies häufiger auftreten kontaktieren Sie bitte das Entwicklerteam.",
+                                    ex)
+                            }
+                        } else {
+                            openUrl(toolTipText)
+                        }
+                    }
+                    else {
+                        openUrl(toolTipText)
+                    }
+
+                }
+            }
+        }
     }
 
     private fun buildLayout() {
-        contentPane.layout = BorderLayout()
-        val newFxPanel = JFXPanel()
-        contentPane.add(newFxPanel, BorderLayout.CENTER)
-        Platform.runLater {
-            val migPane = MigPane(
-                    "hidemode 3",  //columns
-                    "[fill,shrink 0]" +
-                            "[fill]",  //rows
-                    "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[]" +
-                            "[fill,grow]")
-            migPane.add(RightOrientedLabel("Sender:"), CC().cell(0, 0))
-            migPane.add(lblSender, CC().cell(1, 0))
+        layout = MigLayout(
+            LC().insets("5").hideMode(3),
+            // columns
+            AC()
+                .fill().gap()
+                .size("250!").fill(),
+            // rows
+            AC()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap()
+                .gap())
 
-            migPane.add(RightOrientedLabel("Thema:"), CC().cell(0, 1))
-            lblThema.isWrapText = true
-            installContextMenu(lblThema)
-            migPane.add(lblThema, CC().cell(1, 1).growY())
+        add(JLabel("Sender:"), CC().cell(0, 0))
+        add(lblSender, CC().cell(1, 0))
+        add(JLabel("Thema:"), CC().cell(0, 1))
+        lblThema.lineWrap = true
+        add(lblThema, CC().cell(1, 1).growY())
+        add(JLabel("Titel:"), CC().cell(0, 2))
+        lblTitel.lineWrap = true
+        add(lblTitel, CC().cell(1, 2).growY())
 
-            migPane.add(RightOrientedLabel("Titel:"), CC().cell(0, 2))
-            lblTitle.isWrapText = true
-            installContextMenu(lblTitle)
-            migPane.add(lblTitle, CC().cell(1, 2).growY())
-
-            migPane.add(RightOrientedLabel("Datum:"), CC().cell(0, 3))
-            migPane.add(lblDate, CC().cell(1, 3))
-            migPane.add(RightOrientedLabel("Uhrzeit:"), CC().cell(0, 4))
-            migPane.add(lblUhrzeit, CC().cell(1, 4))
-            migPane.add(RightOrientedLabel("Dauer:"), CC().cell(0, 5))
-            migPane.add(lblDuration, CC().cell(1, 5))
-            migPane.add(RightOrientedLabel("Größe (MB):"), CC().cell(0, 6))
-            migPane.add(lblSize, CC().cell(1, 6))
-            migPane.add(RightOrientedLabel("HQ:"), CC().cell(0, 7))
-            migPane.add(cbHq, CC().cell(1, 7))
-            migPane.add(RightOrientedLabel("Untertitel:"), CC().cell(0, 8))
-            migPane.add(cbSubtitle, CC().cell(1, 8))
-            migPane.add(RightOrientedLabel("Geo:"), CC().cell(0, 9))
-            migPane.add(lblGeo, CC().cell(1, 9))
-            migPane.add(RightOrientedLabel("Abo:"), CC().cell(0, 10))
-            migPane.add(lblAbo, CC().cell(1, 10))
-            migPane.add(RightOrientedLabel("Beschreibung:"), CC().cell(0, 12))
-
-            hyperlink.contextMenu = createCopyUrlContextMenu()
-            hyperlink.isUnderline = true
-            hyperlink.onAction = EventHandler {
-                SwingUtilities.invokeLater {
-                    if (currentFilm != null) {
-                        try {
-                            UrlHyperlinkAction.openURL(null, currentFilm!!.websiteUrl)
-                        } catch (ex: URISyntaxException) {
-                            ex.printStackTrace()
-                        }
-                    }
-                }
-            }
-            migPane.add(hyperlink, CC().cell(0, 11).spanX(2))
-
-            lblDescription.isWrapText = true
-            lblDescription.prefRowCount = 4
-            lblDescription.isEditable = false
-            migPane.add(lblDescription, CC().cell(0, 13).spanX(2).growY().growX().minHeight("60"))
-            newFxPanel.scene = Scene(migPane)
-        }
-    }
-
-    private fun createCopyUrlContextMenu() : ContextMenu {
-        val contextMenu = ContextMenu()
-        val mi = MenuItem("URL kopieren")
-        mi.onAction = EventHandler { SwingUtilities.invokeLater { GuiFunktionen.copyToClipboard(currentFilm!!.websiteUrl) } }
-        contextMenu.items.add(mi)
-        return contextMenu
-    }
-    
-    internal class RightOrientedLabel(label: String?) : Label(label) {
-        init {
-            alignment = Pos.BASELINE_RIGHT
-        }
-    }
-    
-    internal class DisabledCheckBox : CheckBox() {
-        init {
-            isDisable = true
-        }
+        add(JLabel("Datum:"), CC().cell(0, 3))
+        add(lblDate, CC().cell(1, 3))
+        add(JLabel("Uhrzeit:"), CC().cell(0, 4))
+        add(lblUhrzeit, CC().cell(1, 4))
+        add(JLabel("Dauer:"), CC().cell(0, 5))
+        add(lblDuration, CC().cell(1, 5))
+        add(JLabel("Größe (MB):"), CC().cell(0, 6))
+        add(lblSize, CC().cell(1, 6))
+        add(JLabel("HQ:"), CC().cell(0, 7))
+        add(cbHq, CC().cell(1, 7))
+        add(JLabel("Untertitel:"), CC().cell(0, 8))
+        add(cbSubtitle, CC().cell(1, 8))
+        add(JLabel("Geo:"), CC().cell(0, 9))
+        add(lblGeo, CC().cell(1, 9))
+        add(JLabel("Abo:"), CC().cell(0, 10))
+        add(lblAbo, CC().cell(1, 10))
+        add(hyperlink, CC().cell(0, 11).spanX(2))
+        add(JLabel("Beschreibung:"), CC().cell(0, 12))
+        lblDescription.isEditable = false
+        lblDescription.lineWrap = true
+        lblDescription.wrapStyleWord = true
+        descScrollPane.setViewportView(lblDescription)
+        add(descScrollPane, CC().cell(0, 13).spanX(2).growY().growX().height("90!"))
     }
 
     init {
@@ -266,7 +258,7 @@ class InfoDialog(parent: Window?) : JDialog(parent) {
         title = "Filminformation"
         isResizable = false
         //hardcode size as linux hates pack()
-        setSize(325, 520)
+        //setSize(350, 520)
         defaultCloseOperation = DISPOSE_ON_CLOSE
         buildLayout()
         updateTextFields()
