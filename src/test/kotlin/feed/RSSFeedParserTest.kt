a2cb8dd3eee0317cac15609535e59c16a3948128
Christian F. <crystalpalace1977@googlemail.com>
2024-08-11 20:55:30 +0200

    - make rss feed parser test locally. Thanks to reneleonhardt.

--- src/test/kotlin/feed/RSSFeedParserTest.kt
+++ src/test/kotlin/feed/RSSFeedParserTest.kt
@@ -2,23 +2,28 @@ package feed
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.net.URI
+import java.io.File
 
 internal class RSSFeedParserTest {
     @Test
     fun mvwTest() {
         try {
-            URI("https://mediathekviewweb.de/feed").toURL().openStream().use {
+            File("src/test/resources/rss/mediathekviewweb.xml").inputStream().use {
                 val parser = RSSFeedParser()
                 val feed = parser.readFeed(it)
                 Assertions.assertNotNull(feed)
                 //println(feed);
                 Assertions.assertNotNull(feed!!.messages)
-                Assertions.assertFalse(feed.messages.isEmpty())
+                Assertions.assertFalse(feed.messages.isEmpty)
                 for (message in feed.messages) {
                     //println(message);
                     Assertions.assertNotNull(message)
+                    Assertions.assertTrue(message.author.isBlank())
                     Assertions.assertFalse(message.guid.isBlank())
+                    Assertions.assertFalse(message.link.isBlank())
+                    Assertions.assertFalse(message.description.isBlank())
+                    Assertions.assertFalse(message.title.isBlank())
+                    Assertions.assertTrue(message.guid.endsWith('='))
                 }
             }
         } catch (e: Exception) {
