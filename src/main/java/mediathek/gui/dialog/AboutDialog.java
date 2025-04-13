2a764fc1b9bd84da78b803f70e73859df58f0ce1
MarkusJannek <61918131+MarkusJannek@users.noreply.github.com>
2025-04-13 19:03:41 +0200

    fix bug
    setEditable false für beide textPanes, da der Nutzer vorher den Text löscjen konnte

--- src/main/java/mediathek/gui/dialog/AboutDialog.java
+++ src/main/java/mediathek/gui/dialog/AboutDialog.java
@@ -58,7 +58,7 @@ public class AboutDialog extends JDialog {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner non-commercial license
+        // Generated using JFormDesigner Educational license - Markus Jannek
         var label1 = new JLabel();
         var panel1 = new JPanel();
         var label2 = new JLabel();
@@ -84,180 +84,172 @@ public class AboutDialog extends JDialog {
         hyperlinkEjTechnologies = new JXHyperlink();
 
         //======== this ========
-        setTitle("\u00dcber dieses Programm"); //NON-NLS
+        setTitle("\u00dcber dieses Programm");
         setModal(true);
         setMinimumSize(new Dimension(725, 470));
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
         var contentPane = getContentPane();
         contentPane.setLayout(new MigLayout(
-            new LC().fill().insets("5").hideMode(3).gridGap("5", "5"), //NON-NLS
-            // columns
-            new AC()
-                .size("316").fill().gap() //NON-NLS
-                .fill(),
-            // rows
-            new AC()
-                .fill().gap()
-                .fill().gap()
-                .fill()));
+          "fill,insets 5,hidemode 3,gap 5 5",
+          // columns
+          "[316,fill]" +
+          "[fill]",
+          // rows
+          "[fill]" +
+          "[fill]" +
+          "[fill]"));
 
         //---- label1 ----
-        label1.setIcon(new ImageIcon(getClass().getResource("/mediathek/res/MediathekView.png"))); //NON-NLS
-        contentPane.add(label1, new CC().cell(0, 0));
+        label1.setIcon(new ImageIcon(getClass().getResource("/mediathek/res/MediathekView.png")));
+        contentPane.add(label1, "cell 0 0");
 
         //======== panel1 ========
         {
-            panel1.setLayout(new MigLayout(
-                new LC().insets("0").hideMode(3).gridGap("0", "0"), //NON-NLS
-                // columns
-                new AC()
-                    .grow().align("left"), //NON-NLS
-                // rows
-                new AC()
-                    .fill().gap()
-                    .fill().gap("rel") //NON-NLS
-                    .grow().fill()));
+          panel1.setLayout(new MigLayout(
+            "insets 0,hidemode 3,gap 0 0",
+            // columns
+            "[grow,left]",
+            // rows
+            "[fill]" +
+            "[fill]rel" +
+            "[grow,fill]"));
 
-            //---- label2 ----
-            label2.setText("MediathekView"); //NON-NLS
-            label2.setFont(label2.getFont().deriveFont(label2.getFont().getStyle() | Font.BOLD, label2.getFont().getSize() + 35f));
-            panel1.add(label2, new CC().cell(0, 0));
+          //---- label2 ----
+          label2.setText("MediathekView");
+          label2.setFont(label2.getFont().deriveFont(label2.getFont().getStyle() | Font.BOLD, label2.getFont().getSize() + 35f));
+          panel1.add(label2, "cell 0 0");
 
-            //---- lblVersion ----
-            lblVersion.setText("Version"); //NON-NLS
-            panel1.add(lblVersion, new CC().cell(0, 1));
+          //---- lblVersion ----
+          lblVersion.setText("Version");
+          panel1.add(lblVersion, "cell 0 1");
 
-            //======== tabbedPane1 ========
+          //======== tabbedPane1 ========
+          {
+            tabbedPane1.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+
+            //======== panel2 ========
             {
-                tabbedPane1.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+              panel2.setPreferredSize(new Dimension(196, 220));
+              panel2.setLayout(new BorderLayout());
 
-                //======== panel2 ========
-                {
-                    panel2.setPreferredSize(new Dimension(196, 220));
-                    panel2.setLayout(new BorderLayout());
+              //======== scrollPane3 ========
+              {
+                scrollPane3.setOpaque(false);
+                scrollPane3.setMinimumSize(new Dimension(196, 162));
 
-                    //======== scrollPane3 ========
-                    {
-                        scrollPane3.setOpaque(false);
-                        scrollPane3.setMinimumSize(new Dimension(196, 162));
-
-                        //---- textPane1 ----
-                        textPane1.setText("MediathekView-Client:\nChristian Franzke (derreisende77)\n\nMediathekView-Server:\nNicklas Wiegandt (nicklas2751)\nPeter W. (pidoubleyou)\nSascha Wiegandt (thesasch)\n\nServer-Administration:\nAlexander Finkh\u00e4user (alex1702)"); //NON-NLS
-                        scrollPane3.setViewportView(textPane1);
-                    }
-                    panel2.add(scrollPane3, BorderLayout.CENTER);
-                }
-                tabbedPane1.addTab("aktive Entwickler", panel2); //NON-NLS
-
-                //======== panel3 ========
-                {
-                    panel3.setLayout(new BorderLayout());
-
-                    //======== scrollPane1 ========
-                    {
-
-                        //---- textPane2 ----
-                        textPane2.setText("Gr\u00fcnder des Programms:\nXaver W. (xaverW)\n\nWeitere Beteiligte:\nsiedlerchr\nstyrol\nzxsd\napoleon\nhostis\npmshell\nclel\nthausherr\nklauswich"); //NON-NLS
-                        scrollPane1.setViewportView(textPane2);
-                    }
-                    panel3.add(scrollPane1, BorderLayout.CENTER);
-                }
-                tabbedPane1.addTab("ehemalige Mitwirkende", panel3); //NON-NLS
+                //---- textPane1 ----
+                textPane1.setText("MediathekView-Client:\nChristian Franzke (derreisende77)\n\nMediathekView-Server:\nNicklas Wiegandt (nicklas2751)\nPeter W. (pidoubleyou)\nSascha Wiegandt (thesasch)\n\nServer-Administration:\nAlexander Finkh\u00e4user (alex1702)");
+                textPane1.setEditable(false);
+                scrollPane3.setViewportView(textPane1);
+              }
+              panel2.add(scrollPane3, BorderLayout.CENTER);
             }
-            panel1.add(tabbedPane1, new CC().cell(0, 2).grow());
+            tabbedPane1.addTab("aktive Entwickler", panel2);
+
+            //======== panel3 ========
+            {
+              panel3.setLayout(new BorderLayout());
+
+              //======== scrollPane1 ========
+              {
+
+                //---- textPane2 ----
+                textPane2.setText("Gr\u00fcnder des Programms:\nXaver W. (xaverW)\n\nWeitere Beteiligte:\nsiedlerchr\nstyrol\nzxsd\napoleon\nhostis\npmshell\nclel\nthausherr\nklauswich");
+                textPane2.setEditable(false);
+                scrollPane1.setViewportView(textPane2);
+              }
+              panel3.add(scrollPane1, BorderLayout.CENTER);
+            }
+            tabbedPane1.addTab("ehemalige Mitwirkende", panel3);
+          }
+          panel1.add(tabbedPane1, "cell 0 2,grow");
         }
-        contentPane.add(panel1, new CC().cell(1, 0, 1, 2).grow());
+        contentPane.add(panel1, "cell 1 0 1 2,grow");
 
         //======== panel5 ========
         {
-            panel5.setLayout(new MigLayout(
-                new LC().fillX().insets("5").hideMode(3).gridGap("5", "5"), //NON-NLS
-                // columns
-                new AC()
-                    .size("316").fill(), //NON-NLS
-                // rows
-                new AC()
-                    .fill().gap()
-                    .fill().gap()
-                    .fill().gap()
-                    .fill().gap()
-                    .fill().gap()
-                    ));
+          panel5.setLayout(new MigLayout(
+            "fillx,insets 5,hidemode 3,gap 5 5",
+            // columns
+            "[316,fill]",
+            // rows
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[]"));
 
-            //---- hyperlinkHomepage ----
-            hyperlinkHomepage.setText("Homepage"); //NON-NLS
-            panel5.add(hyperlinkHomepage, new CC().cell(0, 0));
+          //---- hyperlinkHomepage ----
+          hyperlinkHomepage.setText("Homepage");
+          panel5.add(hyperlinkHomepage, "cell 0 0");
 
-            //---- hyperlinkGuiDonation ----
-            hyperlinkGuiDonation.setText("Spende an den Entwickler des Programms"); //NON-NLS
-            panel5.add(hyperlinkGuiDonation, new CC().cell(0, 1));
+          //---- hyperlinkGuiDonation ----
+          hyperlinkGuiDonation.setText("Spende an den Entwickler des Programms");
+          panel5.add(hyperlinkGuiDonation, "cell 0 1");
 
-            //---- hyperlinkServerDonation ----
-            hyperlinkServerDonation.setText("Spende f\u00fcr den Server-Betrieb"); //NON-NLS
-            panel5.add(hyperlinkServerDonation, new CC().cell(0, 2));
+          //---- hyperlinkServerDonation ----
+          hyperlinkServerDonation.setText("Spende f\u00fcr den Server-Betrieb");
+          panel5.add(hyperlinkServerDonation, "cell 0 2");
 
-            //---- hyperlinkForum ----
-            hyperlinkForum.setText("Hilfe-Forum"); //NON-NLS
-            panel5.add(hyperlinkForum, new CC().cell(0, 3));
+          //---- hyperlinkForum ----
+          hyperlinkForum.setText("Hilfe-Forum");
+          panel5.add(hyperlinkForum, "cell 0 3");
 
-            //---- hyperlinkOnlineHelp ----
-            hyperlinkOnlineHelp.setText("Online-Anleitung"); //NON-NLS
-            panel5.add(hyperlinkOnlineHelp, new CC().cell(0, 4));
+          //---- hyperlinkOnlineHelp ----
+          hyperlinkOnlineHelp.setText("Online-Anleitung");
+          panel5.add(hyperlinkOnlineHelp, "cell 0 4");
 
-            //---- hyperlinkFaq ----
-            hyperlinkFaq.setText("Frequently Asked Questions (FAQ)"); //NON-NLS
-            panel5.add(hyperlinkFaq, new CC().cell(0, 5));
+          //---- hyperlinkFaq ----
+          hyperlinkFaq.setText("Frequently Asked Questions (FAQ)");
+          panel5.add(hyperlinkFaq, "cell 0 5");
         }
-        contentPane.add(panel5, new CC().cell(0, 1).growX());
+        contentPane.add(panel5, "cell 0 1,growx");
 
         //======== panel6 ========
         {
-            panel6.setLayout(new MigLayout(
-                new LC().fill().insets("0 5 5 5").hideMode(3).gridGap("5", "5"), //NON-NLS
-                // columns
-                new AC()
-                    .size("316").fill(), //NON-NLS
-                // rows
-                new AC()
-                    .align("top"))); //NON-NLS
+          panel6.setLayout(new MigLayout(
+            "fill,insets 0 5 5 5,hidemode 3,gap 5 5",
+            // columns
+            "[316,fill]",
+            // rows
+            "[top]"));
 
-            //---- label4 ----
-            label4.setText("Die Entwicklung wird unterst\u00fctzt von:"); //NON-NLS
-            label4.setVerticalAlignment(SwingConstants.TOP);
-            panel6.add(label4, new CC().cell(0, 0));
+          //---- label4 ----
+          label4.setText("Die Entwicklung wird unterst\u00fctzt von:");
+          label4.setVerticalAlignment(SwingConstants.TOP);
+          panel6.add(label4, "cell 0 0");
         }
-        contentPane.add(panel6, new CC().cell(0, 2));
+        contentPane.add(panel6, "cell 0 2");
 
         //======== panel4 ========
         {
-            panel4.setLayout(new MigLayout(
-                new LC().insets("0").hideMode(3).gridGap("0", "0"), //NON-NLS
-                // columns
-                new AC()
-                    .grow().align("left"), //NON-NLS
-                // rows
-                new AC()
-                    .fill().gap()
-                    .fill()));
+          panel4.setLayout(new MigLayout(
+            "insets 0,hidemode 3,gap 0 0",
+            // columns
+            "[grow,left]",
+            // rows
+            "[fill]" +
+            "[fill]"));
 
-            //---- hyperlinkJetBrains ----
-            hyperlinkJetBrains.setText("JetBrains IntelliJ"); //NON-NLS
-            hyperlinkJetBrains.setVerticalAlignment(SwingConstants.TOP);
-            panel4.add(hyperlinkJetBrains, new CC().cell(0, 0).growX());
+          //---- hyperlinkJetBrains ----
+          hyperlinkJetBrains.setText("JetBrains IntelliJ");
+          hyperlinkJetBrains.setVerticalAlignment(SwingConstants.TOP);
+          panel4.add(hyperlinkJetBrains, "cell 0 0,growx");
 
-            //---- hyperlinkEjTechnologies ----
-            hyperlinkEjTechnologies.setText("ej-technologies install4j & JProfiler"); //NON-NLS
-            panel4.add(hyperlinkEjTechnologies, new CC().cell(0, 1).growX());
+          //---- hyperlinkEjTechnologies ----
+          hyperlinkEjTechnologies.setText("ej-technologies install4j & JProfiler");
+          panel4.add(hyperlinkEjTechnologies, "cell 0 1,growx");
         }
-        contentPane.add(panel4, new CC().cell(1, 2));
+        contentPane.add(panel4, "cell 1 2");
         pack();
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner non-commercial license
+    // Generated using JFormDesigner Educational license - Markus Jannek
     private JLabel lblVersion;
     private JScrollPane scrollPane1;
     private JXHyperlink hyperlinkHomepage;
