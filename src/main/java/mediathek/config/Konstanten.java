9992a7b719d5fcd8b3797f592aba5e00ccf8d3d6
Christian F. <crystalpalace1977@googlemail.com>
2025-06-03 23:05:52 +0200

    - prepare for release

--- src/main/java/mediathek/config/Konstanten.java
+++ src/main/java/mediathek/config/Konstanten.java
@@ -30,7 +30,7 @@ public class Konstanten {
     public static final long MINIMUM_MEMORY_THRESHOLD = 768 * FileUtils.ONE_MB;
     public static final Version MVVERSION = new Version(14, 3, 0);
 
-    public static final ApplicationType APPLICATION_TYPE = ApplicationType.NIGHTLY;
+    public static final ApplicationType APPLICATION_TYPE = ApplicationType.PRODUCTION;
     public static final String MACOS_OFFICIAL_APP = "OSX_OFFICIAL_APP";
 
     public static final String FORMAT_ZIP = ".zip";
