579806216dca0154872b3916e86b19ead81ef9a3
A. Finkhaeuser <alex@elaon.de>
2025-10-31 21:21:13 +0100

    Version in der Konstanten.java aktualisiert

--- src/main/java/mediathek/config/Konstanten.java
+++ src/main/java/mediathek/config/Konstanten.java
@@ -17,11 +17,11 @@
  */
 package mediathek.config;
 
+import java.util.concurrent.TimeUnit;
 import mediathek.tool.FileUtils;
 import mediathek.tool.Version;
 import okhttp3.HttpUrl;
 
-import java.util.concurrent.TimeUnit;
 
 public class Konstanten {
     public static final String JSOUP_USER_AGENT = "Mozilla/5.0";
@@ -29,7 +29,7 @@ public class Konstanten {
     public static final String NEW_SENDER_ACTIVATED_QUESTION_CONFIG_KEY = "newSendersActivated.fourteen.three";
     public static final String NEW_FILMLENGTH_ACTIVATED_QUESTION_CONFIG_KEY = "newFilmlengthActivated.fourteen.three";
     public static final long MINIMUM_MEMORY_THRESHOLD = 768 * FileUtils.ONE_MB;
-    public static final Version MVVERSION = new Version(14, 4, 0);
+    public static final Version MVVERSION = new Version(14, 4, 1);
 
     public static final ApplicationType APPLICATION_TYPE = ApplicationType.PRODUCTION;
     public static final String MACOS_OFFICIAL_APP = "OSX_OFFICIAL_APP";
