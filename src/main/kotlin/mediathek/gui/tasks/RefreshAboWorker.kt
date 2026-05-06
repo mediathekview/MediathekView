6d6ae08a4fb47d849862fb391df1e7a4ae1fe816
Christian F. <crystalpalace1977@googlemail.com>
2026-05-06 08:21:31 +0200

    convert to kotlin

---
+++ src/main/kotlin/mediathek/gui/tasks/RefreshAboWorker.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2026 derreisende77.
+ * This code was developed as part of the MediathekView project https://github.com/mediathekview/MediathekView
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mediathek.gui.tasks
+
+import mediathek.config.Daten
+import javax.swing.JLabel
+import javax.swing.JProgressBar
+import javax.swing.SwingUtilities
+import javax.swing.SwingWorker
+
+class RefreshAboWorker(
+    progLabel: JLabel,
+    progressBar: JProgressBar,
+) : SwingWorker<Void?, Int>() {
+    init {
+        SwingUtilities.invokeLater {
+            progLabel.text = "Abos eintragen"
+            progressBar.isIndeterminate = true
+        }
+    }
+
+    override fun doInBackground(): Void? {
+        val daten = Daten.getInstance()
+        daten.listeAbo.setAboFuerFilm(daten.listeFilme, false)
+        return null
+    }
+}
