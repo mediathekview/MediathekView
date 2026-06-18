e23d7aabed7fb3f51bd6637684a604e90f8e64af
Christian F. <crystalpalace1977@googlemail.com>
2026-06-18 23:13:00 +0200

    Decouple online FAQ action from main window

--- src/main/kotlin/mediathek/gui/actions/ShowOnlineFaqAction.kt
+++ src/main/kotlin/mediathek/gui/actions/ShowOnlineFaqAction.kt
@@ -1,10 +1,10 @@
 package mediathek.gui.actions
 
 import mediathek.config.Konstanten
-import mediathek.mainwindow.MediathekGui
 import mediathek.tool.SVGIconUtilities
 import mediathek.tool.SwingErrorDialog
 import java.awt.Desktop
+import java.awt.Component
 import java.awt.event.ActionEvent
 import java.io.IOException
 import java.net.URI
@@ -12,7 +12,7 @@ import java.net.URISyntaxException
 import javax.swing.AbstractAction
 import javax.swing.JOptionPane
 
-class ShowOnlineFaqAction(private val ui: MediathekGui) : AbstractAction() {
+class ShowOnlineFaqAction(private val parentComponent: Component) : AbstractAction() {
     init {
         putValue(NAME, "Frequently Asked Questions (FAQs) anzeigen...")
         putValue(SMALL_ICON, SVGIconUtilities.createSVGIcon("icons/fontawesome/circle-question.svg"))
@@ -26,20 +26,20 @@ class ShowOnlineFaqAction(private val ui: MediathekGui) : AbstractAction() {
                     d.browse(URI(Konstanten.ADRESSE_ONLINE_FAQ))
                 } catch (ex: IOException) {
                     SwingErrorDialog.showExceptionMessage(
-                        ui,
+                        parentComponent,
                         "Es trat ein Fehler beim Öffnen der Online-Hilfe auf.\nSollte dies häufiger auftreten kontaktieren Sie bitte das Entwicklerteam.",
                         ex
                     )
                 } catch (ex: URISyntaxException) {
                     SwingErrorDialog.showExceptionMessage(
-                        ui,
+                        parentComponent,
                         "Es trat ein Fehler beim Öffnen der Online-Hilfe auf.\nSollte dies häufiger auftreten kontaktieren Sie bitte das Entwicklerteam.",
                         ex
                     )
                 }
             } else {
                 JOptionPane.showMessageDialog(
-                    ui,
+                    parentComponent,
                     "<html>Ihr Betriebssystem unterstützt das Öffnen des Browsers nicht.<br>" +
                             "Bitte öffnen Sie <b>" + Konstanten.ADRESSE_ONLINE_FAQ + "</b> selbst in Ihrem Browser.</html>",
                     Konstanten.PROGRAMMNAME, JOptionPane.ERROR_MESSAGE
